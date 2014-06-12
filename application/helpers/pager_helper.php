389cb3ae6e491203116690e6c1db80114e43b86c
Kovah <mail@kovah.de>
2014-06-12 18:21:20 +0200

    Bootstrap update for pager / pagination

--- application/helpers/pager_helper.php
+++ application/helpers/pager_helper.php
@@ -24,24 +24,24 @@ function pager($base_url, $model)
 
 	if (($previous_page = $CI->$model->previous_offset) >= 0)
 	{
-		$pager .= '<a class="btn" href="' . $base_url . '/0" title="' . lang('first') . '"><i class="icon-fast-backward"></i></a>';
-		$pager .= '<a class="btn" href="' . $base_url . '/' . $CI->$model->previous_offset . '" title="' . lang('prev') . '"><i class="icon-backward"></i></a>';
+		$pager .= '<a class="btn btn-default btn-sm" href="' . $base_url . '/0" title="' . lang('first') . '"><i class="fa fa-fast-backward no-margin"></i></a>';
+		$pager .= '<a class="btn btn-default btn-sm" href="' . $base_url . '/' . $CI->$model->previous_offset . '" title="' . lang('prev') . '"><i class="fa fa-backward no-margin"></i></a>';
 	}
 	else
 	{
-		$pager .= '<a class="btn disabled" href="#" title="' . lang('first') . '"><i class="icon-fast-backward"></i></a>';
-		$pager .= '<a class="btn disabled" href="#" title="' . lang('prev') . '"><i class="icon-backward"></i></a>';
+		$pager .= '<a class="btn btn-default btn-sm disabled" href="#" title="' . lang('first') . '"><i class="fa fa-fast-backward no-margin"></i></a>';
+		$pager .= '<a class="btn btn-default btn-sm disabled" href="#" title="' . lang('prev') . '"><i class="fa fa-backward no-margin"></i></a>';
 	}
 
 	if (($next_page = $CI->$model->next_offset) <= $CI->$model->last_offset)
 	{
-		$pager .= '<a class="btn" href="' . $base_url . '/' . $CI->$model->next_offset . '" title="' . lang('next') . '"><i class="icon-forward"></i></a>';
-		$pager .= '<a class="btn" href="' . $base_url . '/' . $CI->$model->last_offset . '" title="' . lang('last') . '"><i class="icon-fast-forward"></i></a>';
+		$pager .= '<a class="btn btn-default btn-sm" href="' . $base_url . '/' . $CI->$model->next_offset . '" title="' . lang('next') . '"><i class="fa fa-forward no-margin"></i></a>';
+		$pager .= '<a class="btn btn-default btn-sm" href="' . $base_url . '/' . $CI->$model->last_offset . '" title="' . lang('last') . '"><i class="fa fa-fast-forward no-margin"></i></a>';
 	}
 	else
 	{
-		$pager .= '<a class="btn disabled" href="#" title="' . lang('next') . '"><i class="icon-forward"></i></a>';
-		$pager .= '<a class="btn disabled" href="#" title="' . lang('last') . '"><i class="icon-fast-forward"></i></a>';
+		$pager .= '<a class="btn btn-default disabled btn-sm" href="#" title="' . lang('next') . '"><i class="fa fa-forward no-margin"></i></a>';
+		$pager .= '<a class="btn btn-default disabled btn-sm" href="#" title="' . lang('last') . '"><i class="fa fa-fast-forward no-margin"></i></a>';
 	}
 	
 	$pager .= '</div>';
