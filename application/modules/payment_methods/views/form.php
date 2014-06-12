42e6f26a506480effde5629bdb2b1982765467aa
Kovah <mail@kovah.de>
2014-06-12 18:55:53 +0200

    Bootstrap update for module: payment_methods

--- application/modules/payment_methods/views/form.php
+++ application/modules/payment_methods/views/form.php
@@ -1,21 +1,26 @@
 <form method="post" class="form-horizontal">
 
-	<div class="headerbar">
-		<h1><?php echo lang('payment_method_form'); ?></h1>
-		<?php $this->layout->load_view('layout/header_buttons'); ?>
-	</div>
+    <div class="headerbar">
+        <h1><?php echo lang('payment_method_form'); ?></h1>
+        <?php $this->layout->load_view('layout/header_buttons'); ?>
+    </div>
 
-	<div class="content">
+    <div class="content">
 
-		<?php $this->layout->load_view('layout/alerts'); ?>
+        <?php $this->layout->load_view('layout/alerts'); ?>
 
-			<div class="control-group">
-				<label class="control-label"><?php echo lang('payment_method'); ?>: </label>
-				<div class="controls">
-					<input type="text" name="payment_method_name" id="payment_method_name" value="<?php echo $this->mdl_payment_methods->form_value('payment_method_name'); ?>">
-				</div>
-			</div>
+        <div class="form-group">
+            <div class="col-xs-12 col-sm-2 text-right text-left-xs">
+                <label for="payment_method_name" class="control-label">
+                    <?php echo lang('payment_method'); ?>:
+                </label>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <input type="text" name="payment_method_name" id="payment_method_name" class="form-control"
+                       value="<?php echo $this->mdl_payment_methods->form_value('payment_method_name'); ?>">
+            </div>
+        </div>
 
-	</div>
-	
+    </div>
+
 </form>
--- application/modules/payment_methods/views/index.php
+++ application/modules/payment_methods/views/index.php
@@ -1,54 +1,63 @@
 <div class="headerbar">
-	<h1><?php echo lang('payment_methods'); ?></h1>
+    <h1><?php echo lang('payment_methods'); ?></h1>
 
-	<div class="pull-right">
-		<a class="btn btn-primary" href="<?php echo site_url('payment_methods/form'); ?>"><i class="icon-plus icon-white"></i> <?php echo lang('new'); ?></a>
-	</div>
-	
-	<div class="pull-right">
-		<?php echo pager(site_url('payment_methods/index'), 'mdl_payment_methods'); ?>
-	</div>
+    <div class="pull-right">
+        <a class="btn btn-sm btn-primary" href="<?php echo site_url('payment_methods/form'); ?>">
+            <i class="fa fa-plus"></i> <?php echo lang('new'); ?>
+        </a>
+    </div>
+
+    <div class="pull-right">
+        <?php echo pager(site_url('payment_methods/index'), 'mdl_payment_methods'); ?>
+    </div>
 
 </div>
 
 <div class="table-content">
 
-	<?php $this->layout->load_view('layout/alerts'); ?>
+    <?php $this->layout->load_view('layout/alerts'); ?>
 
-	<table class="table table-striped">
+    <div class="table-responsive">
+        <table class="table table-striped">
 
-		<thead>
-			<tr>
-				<th><?php echo lang('payment_method'); ?></th>
-				<th><?php echo lang('options'); ?></th>
-			</tr>
-		</thead>
+            <thead>
+            <tr>
+                <th><?php echo lang('payment_method'); ?></th>
+                <th><?php echo lang('options'); ?></th>
+            </tr>
+            </thead>
 
-		<tbody>
-			<?php foreach ($payment_methods as $payment_method) { ?>
-			<tr>
-				<td><?php echo $payment_method->payment_method_name; ?></td>
-				<td>
-					<div class="options btn-group">
-						<a class="btn btn-small dropdown-toggle" data-toggle="dropdown" href="#"><i class="icon-cog"></i> <?php echo lang('options'); ?></a>
-						<ul class="dropdown-menu">
-							<li>
-								<a href="<?php echo site_url('payment_methods/form/' . $payment_method->payment_method_id); ?>">
-									<i class="icon-pencil"></i> <?php echo lang('edit'); ?>
-								</a>
-							</li>
-							<li>
-								<a href="<?php echo site_url('payment_methods/delete/' . $payment_method->payment_method_id); ?>" onclick="return confirm('<?php echo lang('delete_record_warning'); ?>');">
-									<i class="icon-trash"></i> <?php echo lang('delete'); ?>
-								</a>
-							</li>
-						</ul>
-					</div>
-				</td>
-			</tr>
-			<?php } ?>
-		</tbody>
+            <tbody>
+            <?php foreach ($payment_methods as $payment_method) { ?>
+                <tr>
+                    <td><?php echo $payment_method->payment_method_name; ?></td>
+                    <td>
+                        <div class="options btn-group">
+                            <a class="btn btn-default btn-sm dropdown-toggle"
+                               data-toggle="dropdown" href="#">
+                                <i class="fa fa-cog"></i>
+                                <?php echo lang('options'); ?>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a href="<?php echo site_url('payment_methods/form/' . $payment_method->payment_method_id); ?>">
+                                        <i class="fa fa-edit fa-margin"></i>
+                                        <?php echo lang('edit'); ?>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="<?php echo site_url('payment_methods/delete/' . $payment_method->payment_method_id); ?>" onclick="return confirm('<?php echo lang('delete_record_warning'); ?>');">
+                                        <i class="fa fa-trash-o fa-margin"></i>
+                                        <?php echo lang('delete'); ?>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </td>
+                </tr>
+            <?php } ?>
+            </tbody>
 
-	</table>
-
+        </table>
+    </div>
 </div>
