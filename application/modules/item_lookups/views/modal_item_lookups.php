635bb2504536ab04044102a0d830911308fb79df
Kovah <mail@kovah.de>
2014-06-12 18:54:04 +0200

    Bootstrap update for module: item_lookups

--- application/modules/item_lookups/views/modal_item_lookups.php
+++ application/modules/item_lookups/views/modal_item_lookups.php
@@ -8,12 +8,12 @@
         $('#select-items-confirm').click(function()
         {
             var item_lookup_ids = [];
-            
+
             $("input[name='item_lookup_ids[]']:checked").each(function ()
             {
                 item_lookup_ids.push(parseInt($(this).val()));
             });
-            
+
             $.post("<?php echo site_url('item_lookups/ajax/process_item_selections'); ?>", {
                 item_lookup_ids: item_lookup_ids
             }, function(data) {
@@ -27,7 +27,7 @@
                     $('#item_table tr:last textarea[name=item_description]').val(items[key].item_description);
                     $('#item_table tr:last input[name=item_price]').val(items[key].item_price);
                     $('#item_table tr:last input[name=item_quantity]').val('1');
-                    
+
                     $('#modal-choose-items').modal('hide');
                 }
             });
@@ -36,38 +36,52 @@
 
 </script>
 
-<div id="modal-choose-items" class="modal hide">
-	<form class="form-horizontal">
-		<div class="modal-header">
-			<a data-dismiss="modal" class="close">x</a>
-			<h3><?php echo lang('add_item_from_lookup'); ?></h3>
-		</div>
-		<div class="modal-body">
-			
-            <table class="table table-bordered table-striped">
-                <tr>
-                    <th></th>
-                    <th><?php echo lang('item'); ?></th>
-                    <th><?php echo lang('description'); ?></th>
-                    <th><?php echo lang('price'); ?></th>
-                </tr>
-                <?php foreach ($item_lookups as $item_lookup) { ?>
-                <tr>
-                    <td><input type="checkbox" name="item_lookup_ids[]" value="<?php echo $item_lookup->item_lookup_id; ?>"></td>
-                    <td><?php echo $item_lookup->item_name; ?></td>
-                    <td><?php echo $item_lookup->item_description; ?></td>
-                    <td><?php echo format_currency($item_lookup->item_price); ?></td>
-                </tr>
-                <?php } ?>
-            </table>
+<div id="modal-choose-items" class="modal col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2"
+     role="dialog" aria-labelledby="modal-choose-items" aria-hidden="true">
+    <form class="modal-content">
+        <div class="modal-header">
+            <a data-dismiss="modal" class="close">x</a>
+            <h3><?php echo lang('add_item_from_lookup'); ?></h3>
+        </div>
+        <div class="modal-body">
+            <div class="table-responsive">
+                <table class="table table-bordered table-striped">
+                    <?php foreach ($item_lookups as $item_lookup) { ?>
+                        <tr>
+                            <td class="text-center">
+                                <input type="checkbox" name="item_lookup_ids[]"
+                                       value="<?php echo $item_lookup->item_lookup_id; ?>">
+                            </td>
+                            <td>
+                                <b><?php echo $item_lookup->item_name; ?></b>
+                            </td>
+                            <td class="text-right">
+                                <?php echo format_currency($item_lookup->item_price); ?>
+                            </td>
+                        </tr>
+                        <tr class="bold-border">
+                            <td colspan="3">
+                                <?php echo $item_lookup->item_description; ?>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </table>
+            </div>
+        </div>
 
-		</div>
+        <div class="modal-footer">
+            <div class="btn-group">
+                <button class="btn btn-danger" type="button" data-dismiss="modal">
+                    <i class="fa fa-times"></i>
+                    <?php echo lang('cancel'); ?>
+                </button>
+                <button class="btn btn-success" id="select-items-confirm" type="button">
+                    <i class="fa fa-check"></i>
+                    <?php echo lang('submit'); ?>
+                </button>
+            </div>
+        </div>
 
-		<div class="modal-footer">
-            <button class="btn btn-danger" type="button" data-dismiss="modal"><i class="icon-white icon-remove"></i> <?php echo lang('cancel'); ?></button>
-			<button class="btn btn-primary" id="select-items-confirm" type="button"><i class="icon-white icon-ok"></i> <?php echo lang('submit'); ?></button>
-		</div>
-
-	</form>
+    </form>
 
 </div>
