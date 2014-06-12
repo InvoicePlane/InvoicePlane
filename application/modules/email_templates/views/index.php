a83fdc74b451bb5615269397df252fccd7640ba3
Kovah <mail@kovah.de>
2014-06-12 18:29:55 +0200

    Bootstrap update for module: email_templates

--- application/modules/email_templates/views/index.php
+++ application/modules/email_templates/views/index.php
@@ -1,54 +1,56 @@
 <div class="headerbar">
-	<h1><?php echo lang('email_templates'); ?></h1>
+    <h1><?php echo lang('email_templates'); ?></h1>
 
-	<div class="pull-right">
-		<a class="btn btn-primary" href="<?php echo site_url('email_templates/form'); ?>"><i class="icon-plus icon-white"></i> <?php echo lang('new'); ?></a>
-	</div>
-	
-	<div class="pull-right">
-		<?php echo pager(site_url('email_templates/index'), 'mdl_email_templates'); ?>
-	</div>
+    <div class="pull-right">
+        <a class="btn btn-sm btn-primary" href="<?php echo site_url('email_templates/form'); ?>">
+            <i class="fa fa-plus"></i> <?php echo lang('new'); ?>
+        </a>
+    </div>
+
+    <div class="pull-right">
+        <?php echo pager(site_url('email_templates/index'), 'mdl_email_templates'); ?>
+    </div>
 
 </div>
 
 <div class="table-content">
 
-	<?php echo $this->layout->load_view('layout/alerts'); ?>
+    <?php echo $this->layout->load_view('layout/alerts'); ?>
 
-	<table class="table table-striped">
+    <table class="table table-striped">
 
-		<thead>
-			<tr>
-				<th><?php echo lang('title'); ?></th>
-				<th><?php echo lang('options'); ?></th>
-			</tr>
-		</thead>
+        <thead>
+        <tr>
+            <th><?php echo lang('title'); ?></th>
+            <th><?php echo lang('options'); ?></th>
+        </tr>
+        </thead>
 
-		<tbody>
-			<?php foreach ($email_templates as $email_template) { ?>
-			<tr>
-				<td><?php echo $email_template->email_template_title; ?></td>
-				<td>
-					<div class="options btn-group">
-						<a class="btn btn-small dropdown-toggle" data-toggle="dropdown" href="#"><i class="icon-cog"></i> <?php echo lang('options'); ?></a>
-						<ul class="dropdown-menu">
-							<li>
-								<a href="<?php echo site_url('email_templates/form/' . $email_template->email_template_id); ?>">
-									<i class="icon-pencil"></i> <?php echo lang('edit'); ?>
-								</a>
-							</li>
-							<li>
-								<a href="<?php echo site_url('email_templates/delete/' . $email_template->email_template_id); ?>" onclick="return confirm('<?php echo lang('delete_record_warning'); ?>');">
-									<i class="icon-trash"></i> <?php echo lang('delete'); ?>
-								</a>
-							</li>
-						</ul>
-					</div>
-				</td>
-			</tr>
-			<?php } ?>
-		</tbody>
+        <tbody>
+        <?php foreach ($email_templates as $email_template) { ?>
+            <tr>
+                <td><?php echo $email_template->email_template_title; ?></td>
+                <td>
+                    <div class="options btn-group">
+                        <a class="btn btn-default btn-sm dropdown-toggle" data-toggle="dropdown" href="#"><i class="fa fa-cog"></i> <?php echo lang('options'); ?></a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="<?php echo site_url('email_templates/form/' . $email_template->email_template_id); ?>">
+                                    <i class="fa fa-edit fa-margin"></i> <?php echo lang('edit'); ?>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?php echo site_url('email_templates/delete/' . $email_template->email_template_id); ?>" onclick="return confirm('<?php echo lang('delete_record_warning'); ?>');">
+                                    <i class="fa fa-trash-o fa-margin"></i> <?php echo lang('delete'); ?>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </td>
+            </tr>
+        <?php } ?>
+        </tbody>
 
-	</table>
+    </table>
 
 </div>
