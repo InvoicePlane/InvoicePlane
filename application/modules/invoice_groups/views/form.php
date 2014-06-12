c524079162a591734b4ba7be55f0f8aa336d17e2
Kovah <mail@kovah.de>
2014-06-12 18:53:06 +0200

    Bootstrap update for module: invoice_groups

--- application/modules/invoice_groups/views/form.php
+++ application/modules/invoice_groups/views/form.php
@@ -1,62 +1,78 @@
 <form method="post" class="form-horizontal">
 
-	<div class="headerbar">
-		<h1><?php echo lang('invoice_group_form'); ?></h1>
-		<?php $this->layout->load_view('layout/header_buttons'); ?>
-	</div>
+    <div class="headerbar">
+        <h1><?php echo lang('invoice_group_form'); ?></h1>
+        <?php $this->layout->load_view('layout/header_buttons'); ?>
+    </div>
 
-	<div class="content">
+    <div class="content">
 
-		<?php $this->layout->load_view('layout/alerts'); ?>
+        <?php $this->layout->load_view('layout/alerts'); ?>
 
-			<div class="control-group">
-				<label class="control-label"><?php echo lang('name'); ?>: </label>
-				<div class="controls">
-					<input type="text" name="invoice_group_name" id="invoice_group_name" value="<?php echo $this->mdl_invoice_groups->form_value('invoice_group_name'); ?>">
-				</div>
-			</div>
+        <div class="form-group">
+            <div class="col-xs-12 col-sm-3 col-lg-2 text-right text-left-xs">
+                <label class="control-label"><?php echo lang('name'); ?>: </label>
+            </div>
+            <div class="col-xs-12 col-sm-8 col-lg-8">
+                <input type="text" name="invoice_group_name" id="invoice_group_name" class="form-control"
+                       value="<?php echo $this->mdl_invoice_groups->form_value('invoice_group_name'); ?>">
+            </div>
+        </div>
 
-			<div class="control-group">
-				<label class="control-label"><?php echo lang('prefix'); ?>: </label>
-				<div class="controls">
-					<input type="text" name="invoice_group_prefix" id="invoice_group_prefix" value="<?php echo $this->mdl_invoice_groups->form_value('invoice_group_prefix'); ?>">
-				</div>
-			</div>
+        <div class="form-group">
+            <div class="col-xs-12 col-sm-3 col-lg-2 text-right text-left-xs">
+                <label class="control-label"><?php echo lang('prefix'); ?>: </label>
+            </div>
+            <div class="col-xs-12 col-sm-8 col-lg-8">
+                <input type="text" name="invoice_group_prefix" id="invoice_group_prefix" class="form-control"
+                       value="<?php echo $this->mdl_invoice_groups->form_value('invoice_group_prefix'); ?>">
+            </div>
+        </div>
 
-			<div class="control-group">
-				<label class="control-label"><?php echo lang('next_id'); ?>: </label>
-				<div class="controls">
-					<input type="text" name="invoice_group_next_id" id="invoice_group_next_id" value="<?php echo $this->mdl_invoice_groups->form_value('invoice_group_next_id'); ?>">
-				</div>
-			</div>
+        <div class="form-group">
+            <div class="col-xs-12 col-sm-3 col-lg-2 text-right text-left-xs">
+                <label class="control-label"><?php echo lang('next_id'); ?>: </label>
+            </div>
+            <div class="col-xs-12 col-sm-8 col-lg-8">
+                <input type="text" name="invoice_group_next_id" id="invoice_group_next_id" class="form-control"
+                       value="<?php echo $this->mdl_invoice_groups->form_value('invoice_group_next_id'); ?>">
+            </div>
+        </div>
 
-			<div class="control-group">
-				<label class="control-label"><?php echo lang('left_pad'); ?>: </label>
-				<div class="controls">
-					<input type="text" name="invoice_group_left_pad" id="invoice_group_left_pad" value="<?php echo $this->mdl_invoice_groups->form_value('invoice_group_left_pad'); ?>">
-				</div>
-			</div>
+        <div class="form-group">
+            <div class="col-xs-12 col-sm-3 col-lg-2 text-right text-left-xs">
+                <label class="control-label"><?php echo lang('left_pad'); ?>: </label>
+            </div>
+            <div class="col-xs-12 col-sm-8 col-lg-8">
+                <input type="text" name="invoice_group_left_pad" id="invoice_group_left_pad" class="form-control"
+                       value="<?php echo $this->mdl_invoice_groups->form_value('invoice_group_left_pad'); ?>">
+            </div>
+        </div>
 
-			<div class="control-group">
-				<label class="control-label"><?php echo lang('year_prefix'); ?>: </label>
-				<div class="controls">
-					<select name="invoice_group_prefix_year" id="invoice_group_prefix_year">
-						<option value="0" <?php if ($this->mdl_invoice_groups->form_value('invoice_group_prefix_year') == 0) { ?>selected="selected"<?php } ?>><?php echo lang('no'); ?></option>
-						<option value="1" <?php if ($this->mdl_invoice_groups->form_value('invoice_group_prefix_year') == 1) { ?>selected="selected"<?php } ?>><?php echo lang('yes'); ?></option>
-					</select>
-				</div>
-			</div>
+        <div class="form-group">
+            <div class="col-xs-12 col-sm-3 col-lg-2 text-right text-left-xs">
+                <label class="control-label"><?php echo lang('year_prefix'); ?>: </label>
+            </div>
+            <div class="col-xs-12 col-sm-8 col-lg-8">
+                <select name="invoice_group_prefix_year" id="invoice_group_prefix_year" class="form-control">
+                    <option value="0" <?php if ($this->mdl_invoice_groups->form_value('invoice_group_prefix_year') == 0) { ?>selected="selected"<?php } ?>><?php echo lang('no'); ?></option>
+                    <option value="1" <?php if ($this->mdl_invoice_groups->form_value('invoice_group_prefix_year') == 1) { ?>selected="selected"<?php } ?>><?php echo lang('yes'); ?></option>
+                </select>
+            </div>
+        </div>
 
-			<div class="control-group">
-				<label class="control-label"><?php echo lang('month_prefix'); ?>: </label>
-				<div class="controls">
-					<select name="invoice_group_prefix_month" id="invoice_group_prefix_month">
-						<option value="0" <?php if ($this->mdl_invoice_groups->form_value('invoice_group_prefix_month') == 0) { ?>selected="selected"<?php } ?>><?php echo lang('no'); ?></option>
-						<option value="1" <?php if ($this->mdl_invoice_groups->form_value('invoice_group_prefix_month') == 1) { ?>selected="selected"<?php } ?>><?php echo lang('yes'); ?></option>
-					</select>
-				</div>
-			</div>
+        <div class="form-group">
+            <div class="col-xs-12 col-sm-3 col-lg-2 text-right text-left-xs">
+                <label class="control-label"><?php echo lang('month_prefix'); ?>: </label>
+            </div>
+            <div class="col-xs-12 col-sm-8 col-lg-8">
+                <select name="invoice_group_prefix_month" id="invoice_group_prefix_month" class="form-control">
+                    <option value="0" <?php if ($this->mdl_invoice_groups->form_value('invoice_group_prefix_month') == 0) { ?>selected="selected"<?php } ?>><?php echo lang('no'); ?></option>
+                    <option value="1" <?php if ($this->mdl_invoice_groups->form_value('invoice_group_prefix_month') == 1) { ?>selected="selected"<?php } ?>><?php echo lang('yes'); ?></option>
+                </select>
+            </div>
+        </div>
 
-	</div>
-	
+    </div>
+
 </form>
--- application/modules/invoice_groups/views/index.php
+++ application/modules/invoice_groups/views/index.php
@@ -1,64 +1,68 @@
 <div class="headerbar">
-	<h1><?php echo lang('invoice_groups'); ?></h1>
+    <h1><?php echo lang('invoice_groups'); ?></h1>
 
-	<div class="pull-right">
-		<a class="btn btn-primary" href="<?php echo site_url('invoice_groups/form'); ?>"><i class="icon-plus icon-white"></i> <?php echo lang('new'); ?></a>
-	</div>
-	
-	<div class="pull-right">
-		<?php echo pager(site_url('invoice_groups/index'), 'mdl_invoice_groups'); ?>
-	</div>
+    <div class="pull-right">
+        <a class="btn btn-sm btn-primary" href="<?php echo site_url('invoice_groups/form'); ?>"><i class="fa fa-plus"></i> <?php echo lang('new'); ?></a>
+    </div>
+
+    <div class="pull-right">
+        <?php echo pager(site_url('invoice_groups/index'), 'mdl_invoice_groups'); ?>
+    </div>
 
 </div>
 
 <div class="table-content">
 
-	<?php $this->layout->load_view('layout/alerts'); ?>
+    <?php $this->layout->load_view('layout/alerts'); ?>
 
-	<table class="table table-striped">
+    <div class="table-responsive">
+        <table class="table table-striped">
 
-		<thead>
-			<tr>
-				<th><?php echo lang('name'); ?></th>
-				<th><?php echo lang('prefix'); ?></th>
-				<th><?php echo lang('next_id'); ?></th>
-				<th><?php echo lang('left_pad'); ?></th>
-				<th><?php echo lang('year_prefix'); ?></th>
-				<th><?php echo lang('month_prefix'); ?></th>
-				<th><?php echo lang('options'); ?></th>
-			</tr>
-		</thead>
+            <thead>
+            <tr>
+                <th><?php echo lang('name'); ?></th>
+                <th><?php echo lang('prefix'); ?></th>
+                <th><?php echo lang('next_id'); ?></th>
+                <th><?php echo lang('left_pad'); ?></th>
+                <th><?php echo lang('year_prefix'); ?></th>
+                <th><?php echo lang('month_prefix'); ?></th>
+                <th><?php echo lang('options'); ?></th>
+            </tr>
+            </thead>
 
-		<tbody>
-			<?php foreach ($invoice_groups as $invoice_group) { ?>
-			<tr>
-				<td><?php echo $invoice_group->invoice_group_name; ?></td>
-				<td><?php echo $invoice_group->invoice_group_prefix; ?></td>
-				<td><?php echo $invoice_group->invoice_group_next_id; ?></td>
-				<td><?php echo $invoice_group->invoice_group_left_pad; ?></td>
-				<td><?php echo ($invoice_group->invoice_group_prefix_year) ? lang('yes') : lang('no'); ?></td>
-				<td><?php echo ($invoice_group->invoice_group_prefix_month) ? lang('yes') : lang('no'); ?></td>
-				<td>
-					<div class="options btn-group">
-						<a class="btn btn-small dropdown-toggle" data-toggle="dropdown" href="#"><i class="icon-cog"></i> <?php echo lang('options'); ?></a>
-						<ul class="dropdown-menu">
-							<li>
-								<a href="<?php echo site_url('invoice_groups/form/' . $invoice_group->invoice_group_id); ?>">
-									<i class="icon-pencil"></i> <?php echo lang('edit'); ?>
-								</a>
-							</li>
-							<li>
-								<a href="<?php echo site_url('invoice_groups/delete/' . $invoice_group->invoice_group_id); ?>" onclick="return confirm('<?php echo lang('delete_record_warning'); ?>');">
-									<i class="icon-trash"></i> <?php echo lang('delete'); ?>
-								</a>
-							</li>
-						</ul>
-					</div>
-				</td>
-			</tr>
-			<?php } ?>
-		</tbody>
+            <tbody>
+            <?php foreach ($invoice_groups as $invoice_group) { ?>
+                <tr>
+                    <td><?php echo $invoice_group->invoice_group_name; ?></td>
+                    <td><?php echo $invoice_group->invoice_group_prefix; ?></td>
+                    <td><?php echo $invoice_group->invoice_group_next_id; ?></td>
+                    <td><?php echo $invoice_group->invoice_group_left_pad; ?></td>
+                    <td><?php echo ($invoice_group->invoice_group_prefix_year) ? lang('yes') : lang('no'); ?></td>
+                    <td><?php echo ($invoice_group->invoice_group_prefix_month) ? lang('yes') : lang('no'); ?></td>
+                    <td>
+                        <div class="options btn-group">
+                            <a class="btn btn-default btn-sm dropdown-toggle"
+                               data-toggle="dropdown" href="#">
+                                <i class="fa fa-cog"></i> <?php echo lang('options'); ?>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a href="<?php echo site_url('invoice_groups/form/' . $invoice_group->invoice_group_id); ?>">
+                                        <i class="fa fa-edit fa-margin"></i> <?php echo lang('edit'); ?>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="<?php echo site_url('invoice_groups/delete/' . $invoice_group->invoice_group_id); ?>" onclick="return confirm('<?php echo lang('delete_record_warning'); ?>');">
+                                        <i class="fa fa-trash-o fa-margin"></i> <?php echo lang('delete'); ?>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </td>
+                </tr>
+            <?php } ?>
+            </tbody>
 
-	</table>
-
+        </table>
+    </div>
 </div>
