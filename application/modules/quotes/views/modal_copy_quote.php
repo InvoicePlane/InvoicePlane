1db466abc28b5d0b96a68c1001122f4a44a89b63
Kovah <mail@kovah.de>
2014-06-18 18:20:12 +0200

    Corrected wrong handling of submit buttons

--- application/modules/quotes/views/modal_copy_quote.php
+++ application/modules/quotes/views/modal_copy_quote.php
@@ -96,7 +96,7 @@
                 <button class="btn btn-danger" type="button" data-dismiss="modal">
                     <i class="fa fa-times"></i> <?php echo lang('cancel'); ?>
                 </button>
-                <button class="btn btn-success" id="invoice_create_confirm" type="button">
+                <button class="btn btn-success" id="copy_quote_confirm" type="button">
                     <i class="fa fa-check"></i> <?php echo lang('submit'); ?>
                 </button>
             </div>
