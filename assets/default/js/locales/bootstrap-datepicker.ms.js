183b3e3efc7e3792161cfe261537cb18ebc43815
Piero Steinger <piero@steinger.eu>
2014-08-21 21:23:46 +0200

    Updated Bootstrap Datepicker to 1.3.0

--- assets/default/js/locales/bootstrap-datepicker.ms.js
+++ assets/default/js/locales/bootstrap-datepicker.ms.js
@@ -3,11 +3,12 @@
  * Ateman Faiz <noorulfaiz@gmail.com>
  */
 ;(function($){
-	$.fn.datepicker.dates['de'] = {
+	$.fn.datepicker.dates['ms'] = {
 		days: ["Ahad", "Isnin", "Selasa", "Rabu", "Khamis", "Jumaat", "Sabtu", "Ahad"],
 		daysShort: ["Aha", "Isn", "Sel", "Rab", "Kha", "Jum", "Sab", "Aha"],
 		daysMin: ["Ah", "Is", "Se", "Ra", "Kh", "Ju", "Sa", "Ah"],
 		months: ["Januari", "Februari", "Mac", "April", "Mei", "Jun", "Julai", "Ogos", "September", "Oktober", "November", "Disember"],
-		monthsShort: ["Jan", "Feb", "Mar", "Apr", "Mei", "Jun", "Jul", "Ogo", "Sep", "Okt", "Nov", "Dis"]
+		monthsShort: ["Jan", "Feb", "Mar", "Apr", "Mei", "Jun", "Jul", "Ogo", "Sep", "Okt", "Nov", "Dis"],
+		today: "Hari Ini"
 	};
 }(jQuery));
--- assets/default/js/locales/bootstrap-datepicker.pt.js
+++ assets/default/js/locales/bootstrap-datepicker.pt.js
@@ -1,13 +1,16 @@
 /**
- * Brazilian translation for bootstrap-datepicker
- * Cauan Cabral <cauan@radig.com.br>
+ * Portuguese translation for bootstrap-datepicker
+ * Original code: Cauan Cabral <cauan@radig.com.br>
+ * Tiago Melo <tiago.blackcode@gmail.com>
  */
 ;(function($){
-	$.fn.datepicker.dates['br'] = {
+	$.fn.datepicker.dates['pt'] = {
 		days: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado", "Domingo"],
 		daysShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb", "Dom"],
 		daysMin: ["Do", "Se", "Te", "Qu", "Qu", "Se", "Sa", "Do"],
 		months: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
-		monthsShort: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"]
+		monthsShort: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+		today: "Hoje",
+		clear: "Limpar"
 	};
 }(jQuery));
