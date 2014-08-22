6b1675fbf01f51fbe8d0d2491d2fce46063fd2a8
Piero Steinger <piero@steinger.eu>
2014-08-22 23:28:33 +0200

    merged datepicker CSS to style.css

--- application/modules/layout/views/layout.php
+++ application/modules/layout/views/layout.php
@@ -17,7 +17,6 @@
     <meta name="viewport" content="width=device-width,initial-scale=1">
 
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/default/css/style.css">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/default/css/bootstrap-datepicker.css">
 
     <script src="<?php echo base_url() . 'assets/default/js/libs/modernizr-2.8.2.min.js'; ?>"></script>
     <script src="<?php echo base_url(); ?>assets/default/js/libs/jquery-1.11.1.min.js"></script>
