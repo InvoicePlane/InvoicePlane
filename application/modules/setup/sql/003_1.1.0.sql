f5a4dd44a83fc0a3181eeaa2077d269e1d207662
b3rn475 <b3rn4@hotmail.it>
2014-08-29 16:10:29 +0200

    Added "VAT ID" and "TAX CODE" to user and client

--- application/modules/setup/sql/003_1.1.0.sql
+++ application/modules/setup/sql/003_1.1.0.sql
@@ -6,3 +6,11 @@ ADD COLUMN `email_template_from_email` VARCHAR(255) NULL AFTER `email_template_f
 ADD COLUMN `email_template_cc` VARCHAR(255) NULL AFTER `email_template_from_email`,
 ADD COLUMN `email_template_bcc` VARCHAR(255) NULL AFTER `email_template_cc`,
 ADD COLUMN `email_template_pdf_template` VARCHAR(255) NULL AFTER `email_template_bcc`;
+
+ALTER TABLE `ip_clients`
+ADD COLUMN `client_vat_id` VARCHAR(100) NOT NULL DEFAULT '' AFTER `client_web`,
+ADD COLUMN `client_tax_code` VARCHAR(100) NOT NULL DEFAULT '' AFTER `client_vat_id`;
+
+ALTER TABLE `ip_users`
+ADD COLUMN `user_vat_id` VARCHAR(100) NOT NULL DEFAULT '' AFTER `user_web`,
+ADD COLUMN `user_tax_code` VARCHAR(100) NOT NULL DEFAULT '' AFTER `user_vat_id`;
