4696e884581b3990647e9da9ceff567729d2c65a
Kovah <mail@kovah.de>
2014-10-26 23:00:31 +0100

    Solved #64 and changed some styles of the public quote / invoice templates

--- application/views/quote_templates/public/default.php
+++ application/views/quote_templates/public/default.php
@@ -19,8 +19,9 @@
 
         <style>
             body {
-                color: #000 !important;
-				overflow-y: auto;
+                color: #333 !important;
+                padding: 0 0 25px;
+                height: auto;
             }
             table {
                 width:100%;
@@ -28,27 +29,19 @@
             #header table {
                 width:100%;
                 padding: 0px;
+                margin-bottom: 15px;
             }
             #header table td {
                 vertical-align: text-top;
-                padding: 5px;
-            }
-            #company-name{
-                color:#000;
-                font-size: 18px;
             }
             #invoice-to {
-                /*                display: table;*/
-                /*                content: "";*/
+                margin-bottom: 15px;
             }
             #invoice-to td {
                 text-align: left
             }
-            .seperator {
-                height: 25px
-            }
-            .top-border {
-                border-top: none;
+            #invoice-to h3 {
+                margin-bottom: 10px;
             }
             .no-bottom-border {
                 border:none !important;
@@ -58,21 +51,15 @@
                 text-align: right;
             }
             #invoice-container {
-                margin: auto;
-                margin-top: 25px;
+                margin: 25px auto;
                 width: 900px;
                 padding: 20px;
-                top:10px;
                 background-color: white;
-                box-shadow: 4px 4px 14px rgba(0, 0, 0, 0.8);
-                overflow-y: hidden;
+                box-shadow: 0 5px 10px rgba(0, 0, 0, 0.25);
             }
             #menu-container {
-                margin: auto;
-                margin-top: 25px;
+                margin: 25px auto;
                 width: 900px;
-                top:10px;
-                overflow-y: hidden;
             }
             .flash-message {
                 font-size: 120%;
@@ -106,6 +93,8 @@
                 <?php echo $flash_message; ?>
             </div>
             <?php } ?>
+
+            <div class="clearfix"></div>
         </div>
 
         <div id="invoice-container">
@@ -135,7 +124,7 @@
                 <table style="width: 100%;">
                     <tr>
                         <td>
-                            <h2><?php echo $quote->client_name; ?></h2>
+                            <h3><?php echo $quote->client_name; ?></h3>
                             <p><?php if ($quote->client_vat_id) { echo lang("vat_id_short") . ": " . $quote->client_vat_id . '<br>'; } ?>
                                 <?php if ($quote->client_tax_code) { echo lang("tax_code_short") . ": " . $quote->client_tax_code . '<br>'; } ?>
                                 <?php if ($quote->client_address_1) { echo $quote->client_address_1 . '<br>'; } ?>
@@ -146,21 +135,21 @@
                                 <?php if ($quote->client_phone) { ?><abbr>P:</abbr><?php echo $quote->client_phone; ?><br><?php } ?>
                             </p>
                         </td>
-                        <td style="width:40%;"></td>
-                        <td>
+                        <td style="width:30%;"></td>
+                        <td style="width:25%;">
                             <table>
                                 <tbody>
                                     <tr>
                                         <td><?php echo lang('quote_date'); ?></td>
-                                        <td><?php echo date_from_mysql($quote->quote_date_created); ?></td>
+                                        <td style="text-align:right;"><?php echo date_from_mysql($quote->quote_date_created); ?></td>
                                     </tr>
                                     <tr>
                                         <td><?php echo lang('expires'); ?></td>
-                                        <td><?php echo date_from_mysql($quote->quote_date_expires); ?></td>
+                                        <td style="text-align:right;"><?php echo date_from_mysql($quote->quote_date_expires); ?></td>
                                     </tr>
                                     <tr>
                                         <td><?php echo lang('total'); ?></td>
-                                        <td><?php echo format_currency($quote->quote_total); ?></td>
+                                        <td style="text-align:right;"><?php echo format_currency($quote->quote_total); ?></td>
                                     </tr>
                                 </tbody>
                             </table>
@@ -172,9 +161,9 @@
                 <table class="table table-striped">
                     <thead>
                         <tr>
-                            <th><?php echo lang('qty'); ?></th>
                             <th><?php echo lang('item'); ?></th>
                             <th><?php echo lang('description'); ?></th>
+                            <th><?php echo lang('qty'); ?></th>
                             <th><?php echo lang('price'); ?></th>
                             <th><?php echo lang('total'); ?></th>
                         </tr>
@@ -182,9 +171,9 @@
                     <tbody>
                         <?php foreach ($items as $item) : ?>
                             <tr>
-                                <td><?php echo format_amount($item->item_quantity); ?></td>
                                 <td><?php echo $item->item_name; ?></td>
                                 <td><?php echo nl2br($item->item_description); ?></td>
+                                <td><?php echo format_amount($item->item_quantity); ?></td>
                                 <td><?php echo format_currency($item->item_price); ?></td>
                                 <td><?php echo format_currency($item->item_subtotal); ?></td>
                             </tr>
@@ -210,12 +199,11 @@
                         <?php endforeach ?>
                         <tr>
                             <td class="no-bottom-border" colspan="3"></td>
-                            <td><?php echo lang('total'); ?>:</td>
-                            <td><?php echo format_currency($quote->quote_total); ?></td>
+                            <td><b><?php echo lang('total'); ?>:</b></td>
+                            <td><b><?php echo format_currency($quote->quote_total); ?></b></td>
                         </tr>
                     </tbody>
                 </table>
-                <div class="seperator"></div>
                 
             </div>
 
