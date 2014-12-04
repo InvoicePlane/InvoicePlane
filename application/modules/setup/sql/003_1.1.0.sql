85b2923613e51802e608189ece1f7339f31cce92
Stefano Storti <s.storti@s2software.it>
2014-12-04 15:47:54 +0100

    In query upgrade database, Elimina campi C.F. e P.IVA custom già trasferiti in nuovi campi nativi.

--- application/modules/setup/sql/003_1.1.0.sql
+++ application/modules/setup/sql/003_1.1.0.sql
@@ -34,4 +34,9 @@ ALTER TABLE ip_invoice_amounts MODIFY COLUMN invoice_balance decimal(20,2);
 # Sposta dati C.F. e P.IVA custom nei nuovi campi aggiunti in progetto originale
 UPDATE ip_users SET user_vat_id = user_it_piva, user_tax_code = user_it_codfisc;
 UPDATE ip_clients SET client_vat_id = client_it_piva, client_tax_code = client_it_codfisc;
+# Cancella campi C.F. e P.IVA custom già trasferiti in nuovi campi nativi
+ALTER TABLE `ip_users` DROP `user_it_piva`;
+ALTER TABLE `ip_users` DROP `user_it_codfisc`;
+ALTER TABLE `ip_clients` DROP `client_it_piva`;
+ALTER TABLE `ip_clients` DROP `client_it_codfisc`;
 # ---it---fine
