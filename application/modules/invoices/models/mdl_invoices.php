edcad82ee0ab109e58038f2690593f8151bdd63d
Kovah <mail@kovah.de>
2014-12-23 08:52:27 +0100

    invoices / quotes are now sorted by their ID

--- application/modules/invoices/models/mdl_invoices.php
+++ application/modules/invoices/models/mdl_invoices.php
@@ -85,7 +85,7 @@ class Mdl_Invoices extends Response_Model {
 
     public function default_order_by()
     {
-        $this->db->order_by('ip_invoices.invoice_date_created DESC');
+        $this->db->order_by('ip_invoices.invoice_number DESC');
     }
 
     public function default_join()
