ee89bf6c8c61d5f26e58d53d1f1c27588d41a9e3
egrueda <egrueda@gmail.com>
2015-02-08 09:59:45 +0100

    Families and products
    Added two modules to manage product families and products

---
+++ application/modules/families/views/form.php
@@ -0,0 +1,26 @@
+<form method="post" class="form-horizontal">
+
+    <div class="headerbar">
+        <h1><?php echo lang('families_form'); ?></h1>
+        <?php $this->layout->load_view('layout/header_buttons'); ?>
+    </div>
+
+    <div class="content">
+
+        <?php $this->layout->load_view('layout/alerts'); ?>
+
+        <div class="form-group">
+            <div class="col-xs-12 col-sm-2 text-right text-left-xs">
+                <label for="family_name" class="control-label">
+                    <?php echo lang('family_name'); ?>:
+                </label>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <input type="text" name="family_name" id="family_name" class="form-control"
+                       value="<?php echo $this->mdl_families->form_value('family_name'); ?>">
+            </div>
+        </div>
+
+    </div>
+
+</form>
