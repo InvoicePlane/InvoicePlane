f9fae8788ad0f3ca8b924fbeac83c44fbe8dd69e
sudwebdesign <thomas@sudwebdesign.fr>
2015-02-16 13:37:52 +0100

    fix fonts

---
+++ application/helpers/mpdf/ttfontdata/xbriyaz.mtx.php
@@ -0,0 +1,26 @@
+<?php
+$name='XBRiyaz';
+$type='TTF';
+$desc=array (
+  'Ascent' => 1123,
+  'Descent' => -391,
+  'CapHeight' => 717,
+  'Flags' => 4,
+  'FontBBox' => '[-308 -854 1513 1278]',
+  'ItalicAngle' => 0,
+  'StemV' => 87,
+  'MissingWidth' => 605,
+);
+$up=-278;
+$ut=29;
+$ttffile='/var/www/invoiceplane.dev/application/helpers/mpdf/ttfonts/XB Riyaz.ttf';
+$TTCfontID='0';
+$originalsize=1144740;
+$sip=false;
+$smp=false;
+$BMPselected=false;
+$fontkey='xbriyaz';
+$panose=' 0 0 2 0 5 3 8 0 0 2 0 4';
+$haskerninfo=false;
+$unAGlyphs=true;
+?>
