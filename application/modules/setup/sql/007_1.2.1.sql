99a7bbd347cd292d1e7a69438be253911d51e381
Kovah <mail@kovah.de>
2015-03-04 16:53:39 +0100

    Solves IP-198 - Allow longer tax rate names

--- application/modules/setup/sql/007_1.2.1.sql
+++ application/modules/setup/sql/007_1.2.1.sql
@@ -7,3 +7,6 @@ UPDATE `ip_settings` SET `setting_value` = "this-month"
 WHERE `setting_value` = "month";
 UPDATE `ip_settings` SET `setting_value` = "this-year"
 WHERE `setting_value` = "year";
+
+# Solves IP-198
+ALTER TABLE ip_tax_rates CHANGE tax_rate_name tax_rate_name VARCHAR(60) NOT NULL;
