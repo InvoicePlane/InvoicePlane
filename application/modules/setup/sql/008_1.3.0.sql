770c50f9bb14f27ae22b25eff7a81776d48a0234
Patrick Mainka <patrickmainka@gmail.com>
2015-03-22 11:11:35 +0100

    IP 216 Invoice_created Time added

--- application/modules/setup/sql/008_1.3.0.sql
+++ application/modules/setup/sql/008_1.3.0.sql
@@ -1,2 +1,7 @@
 /* IP-195 */
 ALTER TABLE `ip_quotes` ADD COLUMN `notes` LONGTEXT;
+
+/*Solves IP-216*/
+ALTER TABLE `ip_invoices` 
+ADD COLUMN `invoice_time_created` TIME NOT NULL DEFAULT '00:00:00'
+AFTER `invoice_date_created`;
