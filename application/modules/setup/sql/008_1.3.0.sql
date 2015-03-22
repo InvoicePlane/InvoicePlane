7d4a6f11b06e5035614435d2c238524b9f2065cc
Patrick Mainka <patrickmainka@gmail.com>
2015-03-22 11:14:41 +0100

    IP-213 invoice_password added

--- application/modules/setup/sql/008_1.3.0.sql
+++ application/modules/setup/sql/008_1.3.0.sql
@@ -1,2 +1,10 @@
 /* IP-195 */
 ALTER TABLE `ip_quotes` ADD COLUMN `notes` LONGTEXT;
+
+/*Solves IP-216*/
+ALTER TABLE `ip_invoices` 
+ADD COLUMN `invoice_time_created` TIME NOT NULL DEFAULT '00:00:00'
+AFTER `invoice_date_created`,
+/*Solves IP-213*/
+ADD COLUMN `invoice_password` VARCHAR(60) NULL
+AFTER `is_read_only`;
