8a84c4d8fb35759e7e8743c5523e10d45c85c544
Kovah <mail@kovah.de>
2015-04-30 13:45:27 +0200

    FIXED IP-262 - Resolve MySQL errors for 1.2.0 setup / install

--- application/modules/setup/sql/006_1.2.0.sql
+++ application/modules/setup/sql/006_1.2.0.sql
@@ -1,28 +1,29 @@
 /*  Customize quote / invoice groups #5 */
 ALTER TABLE `ip_invoice_groups`
-ADD COLUMN `invoice_group_identifier_format` VARCHAR(255) NOT NULL
-AFTER `invoice_group_name`;
+  ADD COLUMN `invoice_group_identifier_format` VARCHAR(255) NOT NULL
+  AFTER `invoice_group_name`;
+
 /* convert fields to format string
  * Schema: Prefix{{{YEAR}}}{{{MONTH}}}{{{ID}}}
  * (based on existing schema)
  */
 UPDATE ip_invoice_groups
-SET invoice_group_identifier_format = CONCAT(
+  SET invoice_group_identifier_format = CONCAT(
     invoice_group_prefix,
     CASE invoice_group_prefix_year
-    WHEN 1 THEN '{{{year}}}'
-    ELSE ''
+      WHEN 1 THEN '{{{year}}}'
+      ELSE ''
     END,
     CASE invoice_group_prefix_month
-    WHEN 1 THEN '{{{month}}}'
-    ELSE ''
+      WHEN 1 THEN '{{{month}}}'
+      ELSE ''
     END,
     '{{{id}}}'
 );
 ALTER TABLE `ip_invoice_groups`
-DROP invoice_group_prefix,
-DROP invoice_group_prefix_month,
-DROP invoice_group_prefix_year;
+  DROP invoice_group_prefix,
+  DROP invoice_group_prefix_month,
+  DROP invoice_group_prefix_year;
 
 # Module "families"
 CREATE TABLE IF NOT EXISTS `ip_families` (
@@ -49,17 +50,31 @@ CREATE TABLE IF NOT EXISTS `ip_products` (
   DEFAULT CHARSET = utf8;
 
 # Move lookup items to products
-INSERT INTO ip_products (product_name, product_description, product_price)
+INSERT INTO ip_products (
+  family_id,
+  product_sku,
+  product_name,
+  product_description,
+  product_price,
+  purchase_price,
+  tax_rate_id
+)
   SELECT
+    0 as family_id, -- default to 0 (no family)
+    concat('sku-',item_lookup_id) as product_sku, -- use ip_item_lookup primary key as new SKU
     item_name,
     item_description,
-    item_price
+    item_price,
+    0 as product_purchase_price, -- default purchase price to 0
+    0 as tax_rate_id -- default tax rate ID
   FROM ip_item_lookups;
 
 /* Add the Invoice Sign */
-ALTER TABLE `ip_invoice_amounts` ADD `invoice_sign` ENUM('1', '-1') NOT NULL DEFAULT '1'
-AFTER `invoice_id`;
+ALTER TABLE `ip_invoice_amounts`
+  ADD `invoice_sign` ENUM('1', '-1') NOT NULL DEFAULT '1'
+  AFTER `invoice_id`;
+
 ALTER TABLE `ip_invoices`
-ADD `creditinvoice_parent_id` INT(11),
-ADD `is_read_only` TINYINT(1)
-AFTER `invoice_status_id`;
+  ADD `creditinvoice_parent_id` INT(11),
+  ADD `is_read_only` TINYINT(1)
+  AFTER `invoice_status_id`;
