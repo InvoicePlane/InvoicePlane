1d3bf42d4ec8d325e2cf552834d176dea4c721c7
Kovah <mail@kovah.de>
2015-05-19 08:41:31 +0200

    FIXED IP-205 - Some pages only use the half window size on guest view, updated the invoice / quote views with discounts and an optimized layout

--- application/modules/guest/views/quotes_view.php
+++ application/modules/guest/views/quotes_view.php
@@ -6,28 +6,29 @@
         <?php if (in_array($quote->quote_status_id, array(2, 3))) { ?>
             <a href="<?php echo site_url('guest/quotes/approve/' . $quote->quote_id); ?>"
                class="btn btn-success btn-sm">
-                <i class="glyphicon glyphicon-check"></i>
+                <i class="fa fa-check"></i>
                 <?php echo lang('approve_this_quote'); ?>
             </a>
             <a href="<?php echo site_url('guest/quotes/reject/' . $quote->quote_id); ?>"
                class="btn btn-danger btn-sm">
-                <i class="glyphicon glyphicon-ban-circle"></i>
+                <i class="fa fa-times-circle"></i>
                 <?php echo lang('reject_this_quote'); ?>
             </a>
         <?php } elseif ($quote->quote_status_id == 4) { ?>
             <a href="#" class="btn btn-success btn-sm disabled">
+                <i class="fa fa-check"></i>
                 <?php echo lang('quote_approved'); ?>
             </a>
         <?php } elseif ($quote->quote_status_id == 5) { ?>
             <a href="#" class="btn btn-danger btn-sm disabled">
+                <i class="fa fa-times-circle"></i>
                 <?php echo lang('quote_rejected'); ?>
             </a>
         <?php } ?>
 
         <a href="<?php echo site_url('guest/quotes/generate_pdf/' . $quote_id); ?>"
            class="btn btn-default btn-sm" id="btn_generate_pdf">
-            <i class="icon-print"></i>
-            <?php echo lang('download_pdf'); ?>
+            <i class="fa fa-print"></i> <?php echo lang('download_pdf'); ?>
         </a>
     </div>
 
@@ -35,11 +36,11 @@
 
 <?php echo $this->layout->load_view('layout/alerts'); ?>
 
-<div id="content" class="table-content">
+<div id="content">
 
     <div class="quote">
 
-        <div class="cf">
+        <div class="row">
 
             <div class="col-xs-12 col-md-9">
 
@@ -64,66 +65,78 @@
 
             <div class="col-xs-12 col-md-3">
                 <div class="panel panel-default panel-body text-right">
-                    <p>
-                        <b><?php echo lang('quote'); ?> #</b><br/>
-                        <?php echo $quote->quote_number; ?>
-                    </p>
-
-                    <p>
-                        <b><?php echo lang('date'); ?></b><br/>
-                        <?php echo date_from_mysql($quote->quote_date_created); ?>
-                    </p>
-
-                    <p>
-                        <b><?php echo lang('due_date'); ?></b><br/>
-                        <?php echo date_from_mysql($quote->quote_date_expires); ?>
-                    </p>
-
+                    <table class="table table-condensed">
+                        <tr>
+                            <td><?php echo lang('quote'); ?> #</td>
+                            <td><?php echo $quote->quote_number; ?></td>
+                        </tr>
+                        <tr>
+                            <td><?php echo lang('date'); ?></td>
+                            <td><?php echo date_from_mysql($quote->quote_date_created); ?></td>
+                        </tr>
+                        <tr>
+                            <td><?php echo lang('due_date'); ?></td>
+                            <td><?php echo date_from_mysql($quote->quote_date_expires); ?></td>
+                        </tr>
+                    </table>
                 </div>
             </div>
 
         </div>
 
+        <br/>
         <div class="table-responsive">
             <table id="item_table" class="items table table-striped table-bordered">
                 <thead>
                 <tr>
-                    <th><?php echo lang('item'); ?></th>
-                    <th style="min-width: 300px;"><?php echo lang('description'); ?></th>
-                    <th style="width: 100px;"><?php echo lang('quantity'); ?></th>
-                    <th style="width: 100px;"><?php echo lang('price'); ?></th>
-                    <th><?php echo lang('tax_rate'); ?></th>
-                    <th><?php echo lang('subtotal'); ?></th>
-                    <th><?php echo lang('tax'); ?></th>
-                    <th><?php echo lang('total'); ?></th>
+                    <th></th>
+                    <th><?php echo lang('item'); ?> / <?php echo lang('description'); ?></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
                 </tr>
                 </thead>
-                <tbody>
 
-                <?php foreach ($items as $item) { ?>
-                    <tr class="item">
-                        <td style="vertical-align: top;">
-                            <?php echo $item->item_name; ?></td>
-                        <td><?php echo $item->item_description; ?></td>
-                        <td style="vertical-align: top;">
-                            <?php echo format_amount($item->item_quantity); ?></td>
-                        <td style="vertical-align: top;">
-                            <?php echo format_amount($item->item_price); ?></td>
-                        <td style="vertical-align: top;">
-                            <?php echo format_amount($item->item_tax_total); ?></td>
-                        <td style="vertical-align: top;">
-                            <span name="subtotal"><?php echo format_currency($item->item_subtotal); ?></span>
+                <?php
+                $i = 1;
+                foreach ($items as $item) { ?>
+                    <tbody>
+                    <tr>
+                        <td rowspan="2" style="max-width: 20px;" class="text-center">
+                            <?php echo $i;
+                            $i++; ?>
                         </td>
-                        <td style="vertical-align: top;">
-                            <span name="item_tax_total"><?php echo format_currency($item->item_tax_total); ?></span>
+                        <td><?php echo $item->item_name; ?></td>
+                        <td>
+                            <span class="pull-left"><?php echo lang('quantity'); ?></span>
+                            <span class="pull-right amount"><?php echo $item->item_quantity; ?></span>
                         </td>
-                        <td style="vertical-align: top;">
-                            <span name="item_total"><?php echo format_currency($item->item_total); ?></span>
+                        <td>
+                            <span class="pull-left"><?php echo lang('discount'); ?></span>
+                            <span class="pull-right amount"><?php echo format_currency($item->item_discount); ?></span>
+                        </td>
+                        <td>
+                            <span class="pull-left"><?php echo lang('subtotal'); ?></span>
+                            <span class="pull-right amount"><?php echo format_currency($item->item_subtotal); ?></span>
                         </td>
                     </tr>
+                    <tr>
+                        <td class="text-muted"><?php echo $item->item_description; ?></td>
+                        <td>
+                            <span class="pull-left"><?php echo lang('price'); ?></span>
+                            <span class="pull-right amount"><?php format_amount($item->item_price); ?></span>
+                        </td>
+                        <td>
+                            <span class="pull-left"><?php echo lang('tax'); ?></span>
+                            <span class="pull-right amount"><?php echo format_amount($item->item_tax_total); ?></span>
+                        </td>
+                        <td>
+                            <span class="pull-left"><?php echo lang('total'); ?></span>
+                            <span class="pull-right amount"><?php echo format_currency($item->item_total); ?></span>
+                        </td>
+                    </tr>
+                    </tbody>
                 <?php } ?>
-
-                </tbody>
 
             </table>
         </div>
@@ -132,27 +145,36 @@
             <table class="table table-striped table-bordered">
                 <thead>
                 <tr>
-                    <th><?php echo lang('subtotal'); ?></th>
-                    <th><?php echo lang('item_tax'); ?></th>
-                    <th><?php echo lang('quote_tax'); ?></th>
-                    <th><?php echo lang('total'); ?></th>
+                    <th class="text-right"><?php echo lang('subtotal'); ?></th>
+                    <th class="text-right"><?php echo lang('item_tax'); ?></th>
+                    <th class="text-right"><?php echo lang('quote_tax'); ?></th>
+                    <th class="text-right"><?php echo lang('discount'); ?></th>
+                    <th class="text-right"><?php echo lang('total'); ?></th>
                 </tr>
                 </thead>
                 <tbody>
                 <tr>
-                    <td><?php echo format_currency($quote->quote_item_subtotal); ?></td>
-                    <td><?php echo format_currency($quote->quote_item_tax_total); ?></td>
-                    <td>
+                    <td class="amount"><?php echo format_currency($quote->quote_item_subtotal); ?></td>
+                    <td class="amount"><?php echo format_currency($quote->quote_item_tax_total); ?></td>
+                    <td class="amount">
                         <?php if ($quote_tax_rates) {
                             foreach ($quote_tax_rates as $quote_tax_rate) { ?>
                                 <?php echo $quote_tax_rate->quote_tax_rate_name . ' ' . $quote_tax_rate->quote_tax_rate_percent; ?>%:
-                                <?php echo format_currency($quote_tax_rate->quote_tax_rate_amount); ?><br>
+                                <?php echo format_currency($quote_tax_rate->quote_tax_rate_amount); ?><br/>
                             <?php }
                         } else {
                             echo format_currency('0');
                         } ?>
                     </td>
-                    <td><b><?php echo format_currency($quote->quote_total); ?></b></td>
+                    <td class="amount"><?php
+                        if ($quote->quote_discount_percent == floatval(0)) {
+                            echo $quote->quote_discount_percent.'%';
+                        } else {
+                            echo format_currency($quote->quote_discount_amount);
+                        }
+                        ?>
+                    </td>
+                    <td class="amount"><b><?php echo format_currency($quote->quote_total); ?></b></td>
                 </tr>
                 </tbody>
             </table>
