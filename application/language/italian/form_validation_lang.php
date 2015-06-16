26da7c05c633a11c2f38bdf586897be1074390c3
Stefano Storti <s.storti@s2software.it>
2015-06-16 10:02:37 +0200

    Traduzioni

---
+++ application/language/italian/form_validation_lang.php
@@ -0,0 +1,29 @@
+<?php
+
+$lang['required']			= "Il campo %s è obbligatorio.";
+$lang['isset']				= "Il campo %s deve avere un valore.";
+$lang['valid_email']		= "Il campo %s deve contenere un indirizzo email valido.";
+$lang['valid_emails']		= "Il campo %s deve contenere tutte email valide.";
+$lang['valid_url']			= "Il campo %s deve contenere un URL valido.";
+$lang['valid_ip']			= "Il campo %s deve contenere un IP valido.";
+$lang['min_length']			= "Il campo %s deve essere lungo almeno %s caratteri.";
+$lang['max_length']			= "Il campo %s non può essere più lungo di %s caratteri.";
+$lang['exact_length']		= "Il campo %s deve essere lungo esattamente %s caratteri.";
+$lang['alpha']				= "Il campo %s può contenere solo lettere.";
+$lang['alpha_numeric']		= "Il campo %s può contenere solo caratteri alfanumerici.";
+$lang['alpha_dash']			= "Il campo %s può contenere solo caratteri alfanumerici e trattini.";
+$lang['numeric']			= "Il campo %s deve contenere solo numeri.";
+$lang['is_numeric']			= "Il campo %s deve contenere solo caratteri numerici.";
+$lang['integer']			= "Il campo %s deve contenere un numero intero.";
+$lang['regex_match']		= "Il campo %s non è nel formato corretto.";
+$lang['matches']			= "Il campo %s non corrisponde al campo %s.";
+$lang['is_unique'] 			= "Il campo %s deve contenere un valore unico.";
+$lang['is_natural']			= "Il campo %s deve contenere solo numeri positivi.";
+$lang['is_natural_no_zero']	= "Il campo %s deve contenere un numero maggiore di zero.";
+$lang['decimal']			= "Il campo %s deve contenere un numero decimale.";
+$lang['less_than']			= "Il campo %s deve contenere un numerino minore di %s.";
+$lang['greater_than']		= "Il campo %s deve contenere un numero maggiore di %s.";
+
+
+/* End of file form_validation_lang.php */
+/* Location: ./system/language/english/form_validation_lang.php */
