fc7d6fb01d1563d23ac78883a022fbb3d0a708cd
pauluZ <pawel.szczepanek@gmail.com>
2015-07-31 09:59:20 +0200

    hide custom fields if empty in clients module (view)

--- application/modules/clients/views/view.php
+++ application/modules/clients/views/view.php
@@ -173,20 +173,22 @@
                 </div>
             </div>
 
-            <div class="row">
-                <div class="col-xs-12 col-md-6">
-                    <h4><?php echo lang('custom_fields'); ?></h4>
-                    <br/>
-                    <table class="table table-condensed table-striped">
-                        <?php foreach ($custom_fields as $custom_field) { ?>
-                            <tr>
-                                <td><?php echo $custom_field->custom_field_label ?></td>
-                                <td><?php echo $client->{$custom_field->custom_field_column}; ?></td>
-                            </tr>
-                        <?php } ?>
-                    </table>
+            <?php if ($custom_fields) { ?>
+                <div class="row">
+                    <div class="col-xs-12 col-md-6">
+                        <h4><?php echo lang('custom_fields'); ?></h4>
+                        <br/>
+                        <table class="table table-condensed table-striped">
+                            <?php foreach ($custom_fields as $custom_field) { ?>
+                                <tr>
+                                    <td><?php echo $custom_field->custom_field_label ?></td>
+                                    <td><?php echo $client->{$custom_field->custom_field_column}; ?></td>
+                                </tr>
+                            <?php } ?>
+                        </table>
+                    </div>
                 </div>
-            </div>
+            <?php } ?>
 
             <hr/>
 
