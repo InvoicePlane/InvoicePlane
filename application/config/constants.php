5bbf6c9497d00f2c8acb05ef48ff01f60a8b6eef
WebNinjrDesign <tim@webninjrdesign.com>
2015-08-31 09:54:14 +1000

    Aligned values
    Aligned the permission values for readability and editing.

--- application/config/constants.php
+++ application/config/constants.php
@@ -13,10 +13,10 @@
 | always be used to set the mode correctly.
 |
 */
-define('FILE_READ_MODE', 0644);
+define('FILE_READ_MODE',  0644);
 define('FILE_WRITE_MODE', 0666);
-define('DIR_READ_MODE', 0755);
-define('DIR_WRITE_MODE', 0777);
+define('DIR_READ_MODE',   0755);
+define('DIR_WRITE_MODE',  0777);
 
 /*
 |--------------------------------------------------------------------------
