1c6173e9ef56d57d2c449970f5a1cea0b8b70914
WebNinjrDesign <tim@webninjrdesign.com>
2015-08-31 23:08:44 +1000

    Standardised quotes + Spacing

--- application/core/MY_Loader.php
+++ application/core/MY_Loader.php
@@ -1,7 +1,7 @@
 <?php (defined('BASEPATH')) OR exit('No direct script access allowed');
 
 /* load the MX_Loader class */
-require APPPATH . "third_party/MX/Loader.php";
+require APPPATH . 'third_party/MX/Loader.php';
 
 class MY_Loader extends MX_Loader
 {
