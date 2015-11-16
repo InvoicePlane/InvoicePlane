e2340d57fc327ba607125c4d25ca464c9c8cdbd2
Kovah <mail@kovah.de>
2015-11-17 00:06:15 +0100

    Implemented support for onsite payments with credit card (tested with Stripe), offsite payments are still missing

--- application/modules/guest/controllers/Payment_Information.php
+++ application/modules/guest/controllers/Payment_Information.php
@@ -26,14 +26,23 @@ class Payment_Information extends Guest_Controller
         $this->load->model('invoices/mdl_invoices');
     }
 
-    public function form($invoice_id)
+    public function form($invoice_url_key)
     {
+        $disable_form = false;
+
         // Check if the invoice exists and is billable
-        $invoice = $this->mdl_invoices->where('ip_invoices.invoice_id', $invoice_id)->where_in('ip_invoices.client_id',
+        $invoice = $this->mdl_invoices->where('ip_invoices.invoice_url_key', $invoice_url_key)->where_in('ip_invoices.client_id',
             $this->user_clients)->get()->row();
 
         if (!$invoice) {
             show_404();
+        }
+
+        // Check if the invoice is payable
+        if ($invoice->invoice_balance == 0) {
+            //$this->session->set_flashdata('alert_error', lang('invoice_already_paid'));
+            $this->session->set_userdata('alert_error', lang('invoice_already_paid'));
+            $disable_form = true;
         }
 
         // Get all payment gateways
@@ -45,14 +54,22 @@ class Payment_Information extends Guest_Controller
 
         $available_drivers = array();
         foreach ($gateway_drivers as $driver) {
-            $setting = $this->mdl_settings->setting('gateway_' . strtolower($driver));
+
+            $d = strtolower($driver);
+            $setting = $this->mdl_settings->setting('gateway_' . $d);
+            $invoice_payment_method = $invoice->payment_method;
+            $driver_payment_method = $this->mdl_settings->setting('gateway_payment_method_' . $d);
+
             if ($setting == 1) {
-                array_push($available_drivers, $driver);
+                if ($invoice_payment_method == 0 || $driver_payment_method == $invoice_payment_method) {
+                    array_push($available_drivers, $driver);
+                }
             }
         }
 
         $this->layout->set(
             array(
+                'disable_form' => $disable_form,
                 'invoice' => $invoice,
                 'gateways' => $available_drivers,
             )
--- application/modules/guest/views/payment_information.php
+++ application/modules/guest/views/payment_information.php
@@ -3,45 +3,107 @@
 
     <div class="pull-right">
         <a href="<?php echo site_url('guest/invoices/generate_pdf/'); ?>"
-            class="btn btn-sm btn-default">
+           class="btn btn-sm btn-default">
             <i class="fa fa-print"></i> <?php echo lang('download_pdf'); ?>
         </a>
     </div>
 
 </div>
 
-<?php echo $this->layout->load_view('layout/alerts'); ?>
-
 <div id="content">
 
-    <form action="<?php echo site_url('guest/payment_handler/make_payment/'); ?>"
-          method="post" id="payment-information-form" class="form-horizontal">
+    <?php echo $this->layout->load_view('layout/alerts'); ?>
 
-        <div class="form-group">
+    <?php if ($disable_form === false) { ?>
 
-            <input type="hidden" name="invoice_url_key" value="<?php echo $invoice->invoice_url_key; ?>">
+        <h2><?php echo lang('total') . ': ' . format_currency($invoice->invoice_total); ?></h2>
+        <h2><?php echo lang('balance') . ': ' . format_currency($invoice->invoice_balance); ?></h2>
+        <br/>
 
-            <label for="gateway"><?php echo lang('gateway'); ?></label>
-            <select name="gateway" id="gateway-select" class="form-control">
-                <?php
-                // Display all available gateways
-                foreach ($gateways as $gateway) { ?>
-                    <option value="<?php echo $gateway; ?>"><?php echo ucwords(str_replace('_', ' ', $gateway)); ?></option>
-                <?php } ?>
-            </select>
+        <form action="<?php echo site_url('guest/payment_handler/make_payment/'); ?>"
+              method="post" id="payment-information-form">
 
-            <?php
-            /*
-             * @todo implementation of the credit card form data is missing at the moment
-             */
-            ?>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-md-6">
 
-        </div>
+                    <div class="form-group">
+                        <input type="hidden" name="invoice_url_key" value="<?php echo $invoice->invoice_url_key; ?>">
 
-        <div class="form-group">
-            <button class="btn btn-success" type="submit"><?php echo lang('pay_now'); ?></button>
-        </div>
+                        <label for="gateway"><?php echo lang('online_payment_method'); ?></label>
+                        <select name="gateway" id="gateway-select" class="form-control">
+                            <?php
+                            // Display all available gateways
+                            foreach ($gateways as $gateway) { ?>
+                                <option
+                                    value="<?php echo $gateway; ?>">
+                                    <?php echo ucwords(str_replace('_', ' ', $gateway)); ?>
+                                </option>
+                            <?php } ?>
+                        </select>
+                    </div>
 
-    </form>
+                    <div class="panel panel-default">
+
+                        <div class="panel-heading">
+                            <h3 class="panel-title"><?php echo lang('creditcard_details'); ?></h3>
+                        </div>
+
+                        <div class="panel-body">
+                            <div class="alert alert-info">
+                                <?php echo lang('online_payment_creditcard_hint'); ?>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="control-label">
+                                    <?php echo lang('creditcard_number'); ?>
+                                </label>
+                                <input type="text" name="creditcard_number" class="input-sm form-control">
+                            </div>
+
+                            <div class="row">
+                                <div class="col-xs-4">
+                                    <div class="form-group">
+                                        <label class="control-label">
+                                            <?php echo lang('creditcard_expiry_month'); ?>
+                                        </label>
+                                        <input type="number" name="creditcard_expiry_month"
+                                               class="input-sm form-control"
+                                               min="1" max="12">
+                                    </div>
+                                </div>
+                                <div class="col-xs-4">
+                                    <div class="form-group">
+                                        <label class="control-label">
+                                            <?php echo lang('creditcard_expiry_year'); ?>
+                                        </label>
+                                        <input type="number" name="creditcard_expiry_year" class="input-sm form-control"
+                                               min="<?php echo date('Y'); ?>" max="<?php echo date('Y') + 20; ?>">
+                                    </div>
+                                </div>
+                                <div class="col-xs-4">
+                                    <div class="form-group">
+                                        <label class="control-label">
+                                            <?php echo lang('creditcard_cvv'); ?>
+                                        </label>
+                                        <input type="number" name="creditcard_cvv" class="input-sm form-control">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="form-group">
+                <button class="btn btn-success btn-lg ajax-loader" type="submit">
+                    <i class="fa fa-credit-card fa-margin">&nbsp;</i><?php echo lang('pay_now'); ?>
+                </button>
+            </div>
+
+        </form>
+
+    <?php } ?>
 
 </div>
