cbc71d68f2efb7e6d1523825313bc20c2e5dfcb2
Kovah <mail@kovah.de>
2016-01-30 10:00:46 +0100

    IP-363 - Custom title still missing for the login page

--- application/modules/sessions/views/session_passwordreset.php
+++ application/modules/sessions/views/session_passwordreset.php
@@ -2,7 +2,14 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>InvoicePlane</title>
+    <title>
+        <?php
+        if ($this->mdl_settings->setting('custom_title') != '') {
+            echo $this->mdl_settings->setting('custom_title');
+        } else {
+            echo 'InvoicePlane';
+        } ?>
+    </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link href="<?php echo base_url(); ?>assets/default/css/style.css" rel="stylesheet">
