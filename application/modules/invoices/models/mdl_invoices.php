5c06762800d8985b31390aa0934df930b7df81d3
Fabian Vogler <fabian@equivalence.ch>
2016-02-01 09:31:38 +0100

    Remove old invoice_id argument from mdl_items save

--- application/modules/invoices/models/mdl_invoices.php
+++ application/modules/invoices/models/mdl_invoices.php
@@ -221,7 +221,7 @@ class Mdl_Invoices extends Response_Model
                 'item_order' => $invoice_item->item_order
             );
 
-            $this->mdl_items->save($target_id, NULL, $db_array);
+            $this->mdl_items->save(NULL, $db_array);
         }
 
         $invoice_tax_rates = $this->mdl_invoice_tax_rates->where('invoice_id', $source_id)->get()->result();
@@ -260,7 +260,7 @@ class Mdl_Invoices extends Response_Model
                 'item_order' => $invoice_item->item_order
             );
 
-            $this->mdl_items->save($target_id, NULL, $db_array);
+            $this->mdl_items->save(NULL, $db_array);
         }
 
         $invoice_tax_rates = $this->mdl_invoice_tax_rates->where('invoice_id', $source_id)->get()->result();
