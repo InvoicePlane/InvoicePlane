180b0757a5873dedd27d85b1e99c0add69352adf
Kovah <mail@kovah.de>
2016-05-09 09:45:06 +0200

    Optimization for the mpdf helper

--- application/helpers/mpdf_helper.php
+++ application/helpers/mpdf_helper.php
@@ -16,7 +16,7 @@ if (!defined('BASEPATH'))
  * 
  */
 
-function pdf_create($html, $filename, $stream = TRUE, $password = NULL,$isInvoice = NULL,$isGuest = NULL)
+function pdf_create($html, $filename, $stream = true, $password = null, $isInvoice = null, $isGuest = null)
 {
     require_once(APPPATH . 'helpers/mpdf/mpdf.php');
 
@@ -38,10 +38,13 @@ function pdf_create($html, $filename, $stream = TRUE, $password = NULL,$isInvoic
         $mpdf->setAutoBottomMargin = 'stretch';
         $mpdf->SetHTMLFooter('<div id="footer">' . $CI->mdl_settings->settings['pdf_invoice_footer'] . '</div>');
     }
+
     $invoice_array = array();
     $mpdf->WriteHTML($html);
 
+    // If $stream is true (default) the PDF will be displayed directly in the browser, otherwise will be returned as a download
     if ($stream) {
+
         if (!$isInvoice) {
             return $mpdf->Output($filename . '.pdf', 'I');
         }
@@ -50,48 +53,51 @@ function pdf_create($html, $filename, $stream = TRUE, $password = NULL,$isInvoic
             array_push($invoice_array, $file);
         }
 
-        if (!empty($invoice_array) AND $isGuest) {
+        if (!empty($invoice_array) && $isGuest) {
             rsort($invoice_array);
             header('Content-type: application/pdf');
             return readfile($invoice_array[0]);
         } else
-            if ($isGuest){
-            //todo flashdata is deleted between requests
-            //$CI->session->flashdata('alert_error', 'sorry no Invoice found!');
-            redirect('guest/view/invoice/' . end($CI->uri->segment_array()));
-        }
+            if ($isGuest) {
+                // @TODO flashdata is deleted between requests
+                //$CI->session->flashdata('alert_error', 'sorry no Invoice found!');
+                redirect('guest/view/invoice/' . end($CI->uri->segment_array()));
+            }
+
         $mpdf->Output('./uploads/archive/' . date('Y-m-d') . '_' . $filename . '.pdf', 'F');
-        return $mpdf->Output( $filename . '.pdf', 'I');
-    }
+        
+        return $mpdf->Output($filename . '.pdf', 'I');
 
-    else {
+    } else {
 
-        if($isInvoice) {
+        if ($isInvoice) {
 
-            foreach (glob('./uploads/archive/*' .  $filename . '.pdf') as $file) {
+            foreach (glob('./uploads/archive/*' . $filename . '.pdf') as $file) {
                 array_push($invoice_array, $file);
             }
             if (!empty($invoice_array) && !is_null($isGuest)) {
                 rsort($invoice_array);
                 return $invoice_array[0];
             }
-            $mpdf->Output('./uploads/archive/' . date('Y-m-d') .'_'. $filename . '.pdf', 'F');
-            return './uploads/archive/'.date('Y-m-d').'_'. $filename . '.pdf';
+            $mpdf->Output('./uploads/archive/' . date('Y-m-d') . '_' . $filename . '.pdf', 'F');
+            return './uploads/archive/' . date('Y-m-d') . '_' . $filename . '.pdf';
         }
+
         $mpdf->Output('./uploads/temp/' . $filename . '.pdf', 'F');
 
-        // DELETE OLD TEMP FILES - Housekeeping
+        // Housekeeping
         // Delete any files in temp/ directory that are >1 hrs old
         $interval = 3600;
-        if ($handle = @opendir(preg_replace('/\/$/','','./uploads/temp/'))) {
+        if ($handle = @opendir(preg_replace('/\/$/', '', './uploads/temp/'))) {
             while (false !== ($file = readdir($handle))) {
-                if (($file != '..') && ($file != '.') && !is_dir($file) && ((filemtime('./uploads/temp/'.$file)+$interval) < time()) && (substr($file, 0, 1) !== '.') && ($file !='remove.txt')) { // mPDF 5.7.3
-                    unlink('./uploads/temp/'.$file);
+                if (($file != '..') && ($file != '.') && !is_dir($file) && ((filemtime('./uploads/temp/' . $file) + $interval) < time()) && (substr($file, 0, 1) !== '.') && ($file != 'remove.txt')) { // mPDF 5.7.3
+                    unlink('./uploads/temp/' . $file);
                 }
             }
             closedir($handle);
         }
-        //==============================================================================================================
+
+        // Return the pdf itself
         return './uploads/temp/' . $filename . '.pdf';
     }
 }
