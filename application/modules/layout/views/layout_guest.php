7bbe91364277254c629e81661ac63ae27681fe7c
Kovah <mail@kovah.de>
2016-05-10 13:16:39 +0200

    Updates all assets

--- application/modules/layout/views/layout_guest.php
+++ application/modules/layout/views/layout_guest.php
@@ -38,7 +38,7 @@
     <script src="<?php echo base_url(); ?>assets/default/js/libs/respond-1.4.2.min.js"></script>
     <![endif]-->
     
-    <script src="<?php echo base_url(); ?>assets/default/js/libs/jquery-1.12.0.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/default/js/libs/jquery-1.12.3.min.js"></script>
     <script src="<?php echo base_url(); ?>assets/default/js/libs/bootstrap-3.3.6.min.js"></script>
     <script src="<?php echo base_url(); ?>assets/default/js/libs/jquery-ui-1.11.4.min.js"></script>
 
