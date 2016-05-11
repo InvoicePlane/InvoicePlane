f380d2002a26d24077d12430792a4978022ecda8
Kovah <mail@kovah.de>
2016-05-11 14:35:12 +0200

    Corrects SQL statements

--- application/modules/setup/sql/019_1.4.7.sql
+++ application/modules/setup/sql/019_1.4.7.sql
@@ -10,7 +10,7 @@ WHERE setting_key='public_quote_template' AND
       setting_value='default';
 
 # IP-408 - Add reference to products to items
-ALTER TABLE `ip_quote_items`
+ALTER TABLE `ip_invoice_items`
 ADD COLUMN `item_product_id` INT(11) DEFAULT NULL
 AFTER `item_tax_rate_id`;
 
