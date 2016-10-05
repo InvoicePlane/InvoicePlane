7f7d7b9b4638ae25190e10394f5ca03a78da54cf
Kovah <mail@kovah.de>
2016-10-05 12:13:28 +0200

    IP-449 - Moves the debug mode into the index.php file The log threshold in the configuration is not updated properly thus resulting in broken logging if debug mode was turned on

--- application/core/Base_Controller.php
+++ application/core/Base_Controller.php
@@ -56,14 +56,6 @@ class Base_Controller extends MX_Controller
             $this->load->model('settings/mdl_settings');
             $this->mdl_settings->load_settings();
 
-            // Debug Mode
-            if ($this->mdl_settings->setting('enable_debug')) {
-                $this->config->set_item('log_threshold', 2);
-                defined('IP_DEBUG') ?: define('IP_DEBUG', true);
-            } else {
-                defined('IP_DEBUG') ?: define('IP_DEBUG', false);
-            }
-
             $this->lang->load('ip', $this->mdl_settings->setting('default_language'));
             $this->lang->load('form_validation', $this->mdl_settings->setting('default_language'));
             $this->lang->load('custom', $this->mdl_settings->setting('default_language'));
