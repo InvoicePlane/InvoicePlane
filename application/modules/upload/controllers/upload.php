69b0ce6aa9aa026119601c8ed853ba61eb89d5cf
Kovah <mail@kovah.de>
2016-12-12 10:10:23 +0100

    Provide more secure downloads of customer-uploaded files (IP-459)

--- application/modules/upload/controllers/upload.php
+++ application/modules/upload/controllers/upload.php
@@ -20,11 +20,22 @@ class Upload extends Admin_Controller
 {
     public $targetPath;
 
+    public $ctype_default = "application/octet-stream";
+    public $content_types = array(
+        'gif' => 'image/gif',
+        'jpg' => 'image/jpeg',
+        'jpeg' => 'image/jpeg',
+        'pdf' => 'application/pdf',
+        'png' => 'image/png',
+        'txt' => 'text/plain',
+        'xml' => 'application/xml',
+    );
+
     public function __construct()
     {
         parent::__construct();
         $this->load->model('upload/mdl_uploads');
-        $this->targetPath = getcwd() . '/uploads/customer_files';
+        $this->targetPath = UPLOADS_FOLDER . '/customer_files';
 
     }
 
@@ -38,20 +49,21 @@ class Upload extends Admin_Controller
             $targetFile = $this->targetPath . '/' . $url_key . '_' . $fileName;
             $file_exists = file_exists($targetFile);
 
-            if (!$file_exists) //If file does not exists then upload
-            {
+            if (!$file_exists) {
+                // If file does not exists then upload
                 $data = array(
                     'client_id' => $customerId,
                     'url_key' => $url_key,
                     'file_name_original' => $fileName,
                     'file_name_new' => $url_key . '_' . $fileName
                 );
+
                 $this->mdl_uploads->create($data);
 
                 move_uploaded_file($tempFile, $targetFile);
-            } else //If file exists then echo the error and set a http error response
-            {
-                echo trans('error_dublicate_file');;
+            } else {
+                // If file exists then echo the error and set a http error response
+                echo trans('error_duplicate_file');;
                 http_response_code(404);
             }
 
@@ -67,26 +79,28 @@ class Upload extends Admin_Controller
         $fileName = $_POST['name'];
 
         $this->mdl_uploads->delete($url_key, $fileName);
-        unlink($path . '/' . $url_key . '_' . $fileName);
 
+        unlink($path . '/' . $url_key . '_' . $fileName);
     }
 
     public function create_dir($path, $chmod = '0777')
     {
-        if (!(is_dir($path) OR is_link($path)))
+        if (!(is_dir($path) || is_link($path))) {
             return mkdir($path, $chmod);
-        else
+        } else {
             return false;
+        }
     }
 
     public function show_files($url_key, $customerId = null)
     {
-
         $result = array();
         $path = $this->targetPath;
 
         $files = scandir($path);
+
         if ($files !== false) {
+
             foreach ($files as $file) {
                 if ('.' != $file && '..' != $file && strpos($file, $url_key) !== false) {
                     $obj['name'] = substr($file, strpos($file, '_', 1) + 1);
@@ -96,11 +110,45 @@ class Upload extends Admin_Controller
                     $result[] = $obj;
                 }
             }
-        } else {
 
+        } else {
             return false;
         }
+
         echo json_encode($result);
+    }
+
+    /**
+     * Returns the corresponding file as a download and prevents execution of files
+     *
+     * @param string $filename
+     * @return resource
+     */
+    public function get_file($filename)
+    {
+        $file_path = UPLOADS_FOLDER . 'customer_files/' . $filename;
+
+        $path_parts = pathinfo($file_path);
+        $file_ext = $path_parts['extension'];
+        $file = @fopen($file_path, 'rb');
+
+        if ($file) {
+            $file_size = filesize($file_path);
+
+            $save_ctype = isset($this->content_types[$file_ext]);
+            $ctype = $save_ctype ? $this->content_types[$file_ext] : $this->ctype_default;
+
+            header("Expires: -1");
+            header("Cache-Control: public, must-revalidate, post-check=0, pre-check=0");
+            header("Content-Disposition: attachment; filename=\"$filename\"");
+            header("Content-Type: " . $ctype);
+            header("Content-Length: " . $file_size);
+
+            return $file;
+        }
+
+        show_404();
+        exit;
     }
 
 }
