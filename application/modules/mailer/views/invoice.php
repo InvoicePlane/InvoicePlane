6a34d67ae19405b39351210e7f764130e7bd4714
Denys Vitali <denys.vitali@coelis.ch>
2017-02-07 00:24:23 +0100

    Span can't contain divs

--- application/modules/mailer/views/invoice.php
+++ application/modules/mailer/views/invoice.php
@@ -205,12 +205,12 @@
                 <div class="col-lg-7"></div>
                 <div class="col-lg-5">
                     <!-- The global file processing state -->
-                    <span class="fileupload-process">
+                    <div class="fileupload-process">
                       <div id="total-progress" class="progress progress-striped active" role="progressbar"
                            aria-valuemin="0" aria-valuemax="100" aria-valuenow="0">
                           <div class="progress-bar progress-bar-success" style="width:0%;" data-dz-uploadprogress></div>
                       </div>
-                    </span>
+                    </div>
                 </div>
 
                 <div class="table table-striped" class="files" id="previews">
