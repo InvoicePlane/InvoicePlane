d30a4c0b2610272115d37fef865ae3708e2f7c27
Kovah <mail@kovah.de>
2017-02-08 22:26:48 +0100

    Updates code style of the config files

--- application/config/memcached.php
+++ application/config/memcached.php
@@ -11,9 +11,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config = array(
-	'default' => array(
-		'hostname' => '127.0.0.1',
-		'port'     => '11211',
-		'weight'   => '1',
-	),
+    'default' => array(
+        'hostname' => '127.0.0.1',
+        'port' => '11211',
+        'weight' => '1',
+    ),
 );
