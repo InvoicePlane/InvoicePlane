b852772c9beeed08157095c5a687037a11b35b8d
Kovah <mail@kovah.de>
2017-02-09 17:21:10 +0100

    Updates code style and PHPDocs for the custom values module

--- application/modules/custom_values/controllers/Custom_values.php
+++ application/modules/custom_values/controllers/Custom_values.php
@@ -1,23 +1,23 @@
 <?php
-
-if (!defined('BASEPATH'))
-    exit('No direct script access allowed');
+if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 /*
  * InvoicePlane
  *
- * A free and open source web based invoicing system
- *
- * @package		InvoicePlane
- * @author		Kovah (www.kovah.de)
- * @copyright	Copyright (c) 2012 - 2015 InvoicePlane.com
+ * @author		InvoicePlane Developers & Contributors
+ * @copyright	Copyright (c) 2012 - 2017 InvoicePlane.com
  * @license		https://invoiceplane.com/license.txt
  * @link		https://invoiceplane.com
- *
  */
 
+/**
+ * Class Custom_Values
+ */
 class Custom_Values extends Admin_Controller
 {
+    /**
+     * Custom_Values constructor.
+     */
     public function __construct()
     {
         parent::__construct();
@@ -25,6 +25,9 @@ class Custom_Values extends Admin_Controller
         $this->load->model('mdl_custom_values');
     }
 
+    /**
+     * @param int $page
+     */
     public function index($page = 0)
     {
         $this->mdl_custom_values->paginate(site_url('custom_values/index'), $page);
@@ -35,6 +38,9 @@ class Custom_Values extends Admin_Controller
         $this->layout->render();
     }
 
+    /**
+     * @param null $id
+     */
     public function field($id = null)
     {
         if ($this->input->post('btn_cancel')) {
@@ -55,53 +61,62 @@ class Custom_Values extends Admin_Controller
         $this->layout->render();
     }
 
-    public function edit($id = null){
-      $this->layout->set('id', $id);
+    /**
+     * @param null $id
+     */
+    public function edit($id = null)
+    {
+        $this->layout->set('id', $id);
 
-      $value = $this->mdl_custom_values->get_by_id($id)->row();
-      $fid = $value->custom_field_id;
+        $value = $this->mdl_custom_values->get_by_id($id)->row();
+        $fid = $value->custom_field_id;
 
-      if ($this->input->post('btn_cancel')) {
-          redirect('custom_values/field/'.$fid);
-      }
+        if ($this->input->post('btn_cancel')) {
+            redirect('custom_values/field/' . $fid);
+        }
 
-      if ($this->mdl_custom_values->run_validation()) {
-          $this->mdl_custom_values->save($id);
-          redirect('custom_values/field/'.$fid);
-      }
+        if ($this->mdl_custom_values->run_validation()) {
+            $this->mdl_custom_values->save($id);
+            redirect('custom_values/field/' . $fid);
+        }
 
-      $this->layout->set('fid', $fid);
-      $this->layout->set('value', $value);
-      $this->layout->buffer('content', 'custom_values/edit');
-      $this->layout->render();
+        $this->layout->set('fid', $fid);
+        $this->layout->set('value', $value);
+        $this->layout->buffer('content', 'custom_values/edit');
+        $this->layout->render();
     }
 
-    public function new($id = null){
+    /**
+     * @param null $id
+     */
+    public function create($id = null)
+    {
+        if (!$id) {
+            redirect('custom_values');
+        }
 
-      if(!$id)
-      {
-        redirect('custom_values');
-      }
+        $fid = $id;
 
-      $fid = $id;
+        if ($this->input->post('btn_cancel')) {
+            redirect('custom_values/field/' . $fid);
+        }
 
-      if ($this->input->post('btn_cancel')) {
-          redirect('custom_values/field/'.$fid);
-      }
+        if ($this->mdl_custom_values->run_validation()) {
+            $this->mdl_custom_values->save_custom($fid);
+            redirect('custom_values/field/' . $fid);
+        }
 
-      if ($this->mdl_custom_values->run_validation()) {
-          $this->mdl_custom_values->save_custom($fid);
-          redirect('custom_values/field/'.$fid);
-      }
-
-      $this->load->model('custom_fields/mdl_custom_fields');
-      $field = $this->mdl_custom_fields->get_by_id($id);
-      $this->layout->set('id', $id);
-      $this->layout->set('field', $field);
-      $this->layout->buffer('content', 'custom_values/new');
-      $this->layout->render();
+        $this->load->model('custom_fields/mdl_custom_fields');
+        $field = $this->mdl_custom_fields->get_by_id($id);
+        $this->layout->set('id', $id);
+        $this->layout->set('field', $field);
+        $this->layout->buffer('content', 'custom_values/new');
+        $this->layout->render();
     }
 
+    /**
+     * @param $id
+     */
     public function delete($id)
     {
         $this->mdl_custom_values->delete($id);
