81079146261897f17e6721abf27c8e75d5fafd61
Kovah <mail@kovah.de>
2017-02-09 17:15:46 +0100

    Updates code style and PHPDocs for the custom fields module

--- application/modules/custom_fields/models/Mdl_invoice_custom.php
+++ application/modules/custom_fields/models/Mdl_invoice_custom.php
@@ -1,27 +1,28 @@
 <?php
-
-if (!defined('BASEPATH')) {
-    exit('No direct script access allowed');
-}
+if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 /*
  * InvoicePlane
  *
- * A free and open source web based invoicing system
- *
- * @package		InvoicePlane
- * @author		Kovah (www.kovah.de)
- * @copyright	Copyright (c) 2012 - 2015 InvoicePlane.com
+ * @author		InvoicePlane Developers & Contributors
+ * @copyright	Copyright (c) 2012 - 2017 InvoicePlane.com
  * @license		https://invoiceplane.com/license.txt
  * @link		https://invoiceplane.com
- *
  */
 
+/**
+ * Class Mdl_Invoice_Custom
+ */
 class Mdl_Invoice_Custom extends Validator
 {
     public $table = 'ip_invoice_custom';
     public $primary_key = 'ip_invoice_custom.invoice_custom_id';
 
+    /**
+     * @param $invoice_id
+     * @param $db_array
+     * @return bool|string
+     */
     public function save_custom($invoice_id, $db_array)
     {
         $result = $this->validate($db_array);
@@ -34,9 +35,13 @@ class Mdl_Invoice_Custom extends Validator
             if ($invoice_custom->num_rows()) {
                 $invoice_custom_id = $invoice_custom->row()->invoice_custom_id;
             }
+
             parent::save($invoice_custom_id, $db_array);
+
             return true;
         }
+
         return $result;
     }
+
 }
