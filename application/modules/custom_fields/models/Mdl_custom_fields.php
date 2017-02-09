32c0e6d420bcad99db73bb0ab6a4b5cd5213f962
Kovah <mail@kovah.de>
2017-02-09 17:15:46 +0100

    Updates code style and PHPDocs for the custom fields module

--- application/modules/custom_fields/models/Mdl_custom_fields.php
+++ application/modules/custom_fields/models/Mdl_custom_fields.php
@@ -1,21 +1,18 @@
 <?php
-
-if (!defined('BASEPATH'))
-    exit('No direct script access allowed');
+if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 /*
  * InvoicePlane
  *
- * A free and open source web based invoicing system
- *
- * @package		InvoicePlane
- * @author		Kovah (www.kovah.de)
- * @copyright	Copyright (c) 2012 - 2015 InvoicePlane.com
+ * @author		InvoicePlane Developers & Contributors
+ * @copyright	Copyright (c) 2012 - 2017 InvoicePlane.com
  * @license		https://invoiceplane.com/license.txt
  * @link		https://invoiceplane.com
- *
  */
 
+/**
+ * Class Mdl_Custom_Fields
+ */
 class Mdl_Custom_Fields extends MY_Model
 {
     public $table = 'ip_custom_fields';
@@ -26,31 +23,30 @@ class Mdl_Custom_Fields extends MY_Model
         $this->db->select('SQL_CALC_FOUND_ROWS *', false);
     }
 
-    public function custom_tables()
+    /**
+     * @param $element
+     * @return string
+     */
+    public function get_nicename($element)
     {
-        return array(
-            'ip_client_custom' => 'client',
-            'ip_invoice_custom' => 'invoice',
-            'ip_payment_custom' => 'payment',
-            'ip_quote_custom' => 'quote',
-            'ip_user_custom' => 'user'
-        );
+        if (in_array($element, $this->custom_types())) {
+            return strtolower(str_replace(''-'', '', $element));
+        }
+        return 'fallback';
     }
 
+    /**
+     * @return array
+     */
     public function custom_types()
     {
         $this->load->module("custom_values/mdl_custom_values");
         return Mdl_Custom_Values::custom_types();
     }
 
-    public function get_nicename($element){
-      if(in_array($element, $this->custom_types()))
-      {
-        return strtolower(str_replace("-","",$element));
-      }
-      return "fallback";
-    }
-
+    /**
+     * @return array
+     */
     public function validation_rules()
     {
         return array(
@@ -72,52 +68,31 @@ class Mdl_Custom_Fields extends MY_Model
         );
     }
 
-    public function db_array()
+    /**
+     * @param $table
+     * @return $this
+     */
+    public function get_by_table($table)
     {
-        // Get the default db array
-        $db_array = parent::db_array();
-
-        // Get the array of custom tables
-        $custom_tables = $this->custom_tables();
-
-        // Check if the user wants to add 'id' as custom field
-        if (strtolower($db_array['custom_field_label']) == 'id') {
-            // Replace 'id' with 'field_id' to avoid problems with the primary key
-            $custom_field_label = 'field_id';
-        } else {
-            $custom_field_label = strtolower(str_replace(' ', '_', $db_array['custom_field_label']));
-        }
-
-        if(in_array($db_array['custom_field_type'], $this->custom_types()))
-        {
-          $type = $db_array['custom_field_type'];
-        }
-        else{
-          $type = $this->custom_types()[0];
-        }
-
-        // Create the name for the custom field column
-
-        $this->load->helper('diacritics');
-
-        $clean_name = preg_replace('/[^a-z0-9_\s]/', '', strtolower(diacritics_remove_diacritics($custom_field_label)));
-
-        $db_array['custom_field_column'] = $custom_tables[$db_array['custom_field_table']] . '_custom_' . $clean_name;
-        $db_array['custom_field_type'] = $type;
-        // Return the db array
-        return $db_array;
-    }
-
-    public function get_by_table($table){
         $this->where('custom_field_table', $table);
         return $this->get();
     }
 
-    public function get_by_column($column){
+    /**
+     * @param $column
+     * @return $this
+     */
+    public function get_by_column($column)
+    {
         $this->where('custom_field_column', $column);
         return $this->get();
     }
 
+    /**
+     * @param null $id
+     * @param null $db_array
+     * @return null
+     */
     public function save($id = null, $db_array = null)
     {
         if ($id) {
@@ -144,20 +119,62 @@ class Mdl_Custom_Fields extends MY_Model
         return $id;
     }
 
-    private function add_column($table_name, $column_name)
+    /**
+     * @return array
+     */
+    public function db_array()
     {
-        $this->load->dbforge();
+        // Get the default db array
+        $db_array = parent::db_array();
 
-        $column = array(
-            $column_name => array(
-                'type' => 'VARCHAR',
-                'constraint' => 2000
-            )
-        );
+        // Get the array of custom tables
+        $custom_tables = $this->custom_tables();
 
-        $this->dbforge->add_column($table_name, $column);
+        // Check if the user wants to add 'id' as custom field
+        if (strtolower($db_array['custom_field_label']) == 'id') {
+            // Replace 'id' with 'field_id' to avoid problems with the primary key
+            $custom_field_label = 'field_id';
+        } else {
+            $custom_field_label = strtolower(str_replace(' ', '_', $db_array['custom_field_label']));
+        }
+
+        if (in_array($db_array['custom_field_type'], $this->custom_types())) {
+            $type = $db_array['custom_field_type'];
+        } else {
+            $type = $this->custom_types()[0];
+        }
+
+        // Create the name for the custom field column
+        $this->load->helper('diacritics');
+
+        $clean_name = preg_replace('/[^a-z0-9_\s]/', '', strtolower(diacritics_remove_diacritics($custom_field_label)));
+
+        $db_array['custom_field_column'] = $custom_tables[$db_array['custom_field_table']] . '_custom_' . $clean_name;
+        $db_array['custom_field_type'] = $type;
+
+        // Return the db array
+        return $db_array;
     }
 
+    /**
+     * @return array
+     */
+    public function custom_tables()
+    {
+        return array(
+            'ip_client_custom' => 'client',
+            'ip_invoice_custom' => 'invoice',
+            'ip_payment_custom' => 'payment',
+            'ip_quote_custom' => 'quote',
+            'ip_user_custom' => 'user'
+        );
+    }
+
+    /**
+     * @param $table_name
+     * @param $old_column_name
+     * @param $new_column_name
+     */
     private function rename_column($table_name, $old_column_name, $new_column_name)
     {
         $this->load->dbforge();
@@ -173,6 +190,27 @@ class Mdl_Custom_Fields extends MY_Model
         $this->dbforge->modify_column($table_name, $column);
     }
 
+    /**
+     * @param $table_name
+     * @param $column_name
+     */
+    private function add_column($table_name, $column_name)
+    {
+        $this->load->dbforge();
+
+        $column = array(
+            $column_name => array(
+                'type' => 'VARCHAR',
+                'constraint' => 2000
+            )
+        );
+
+        $this->dbforge->add_column($table_name, $column);
+    }
+
+    /**
+     * @param $id
+     */
     public function delete($id)
     {
         $custom_field = $this->get_by_id($id);
@@ -185,6 +223,10 @@ class Mdl_Custom_Fields extends MY_Model
         parent::delete($id);
     }
 
+    /**
+     * @param $table
+     * @return $this
+     */
     public function by_table($table)
     {
         $this->filter_where('custom_field_table', $table);
--- application/modules/custom_fields/models/Mdl_user_custom.php
+++ application/modules/custom_fields/models/Mdl_user_custom.php
@@ -1,30 +1,32 @@
 <?php
-
-if (!defined('BASEPATH')) {
-    exit('No direct script access allowed');
-}
+if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 /*
  * InvoicePlane
  *
- * A free and open source web based invoicing system
- *
- * @package		InvoicePlane
- * @author		Kovah (www.kovah.de)
- * @copyright	Copyright (c) 2012 - 2015 InvoicePlane.com
+ * @author		InvoicePlane Developers & Contributors
+ * @copyright	Copyright (c) 2012 - 2017 InvoicePlane.com
  * @license		https://invoiceplane.com/license.txt
  * @link		https://invoiceplane.com
- *
  */
 
+/**
+ * Class Mdl_User_Custom
+ */
 class Mdl_User_Custom extends Validator
 {
     public $table = 'ip_user_custom';
     public $primary_key = 'ip_user_custom.user_custom_id';
 
+    /**
+     * @param $user_id
+     * @param $db_array
+     * @return bool|string
+     */
     public function save_custom($user_id, $db_array)
     {
         $result = $this->validate($db_array);
+
         if ($result === true) {
             $db_array = $this->_formdata;
             $user_custom_id = null;
@@ -36,8 +38,11 @@ class Mdl_User_Custom extends Validator
             }
 
             parent::save($user_custom_id, $db_array);
+
             return true;
         }
+
         return $result;
     }
+
 }
