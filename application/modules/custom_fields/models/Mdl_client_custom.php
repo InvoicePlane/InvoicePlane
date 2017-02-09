12628ecc76f31c5e1db8e4f241c7bcd6ef033523
Kovah <mail@kovah.de>
2017-02-09 17:15:46 +0100

    Updates code style and PHPDocs for the custom fields module

--- application/modules/custom_fields/models/Mdl_client_custom.php
+++ application/modules/custom_fields/models/Mdl_client_custom.php
@@ -1,27 +1,28 @@
 <?php
-
-if (!defined('BASEPATH')) {
-    exit('No direct script access allowed');
-}
+if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 /*
  * InvoicePlane
  *
- * A free and open source web based invoicing system
- *
- * @package		InvoicePlane
- * @author		Kovah (www.kovah.de)
- * @copyright	Copyright (c) 2012 - 2015 InvoicePlane.com
+ * @author		InvoicePlane Developers & Contributors
+ * @copyright	Copyright (c) 2012 - 2017 InvoicePlane.com
  * @license		https://invoiceplane.com/license.txt
- * @link		  https://invoiceplane.com
- *
+ * @link		https://invoiceplane.com
  */
 
+/**
+ * Class Mdl_Client_Custom
+ */
 class Mdl_Client_Custom extends Validator
 {
     public $table = 'ip_client_custom';
     public $primary_key = 'ip_client_custom.client_custom_id';
 
+    /**
+     * @param $client_id
+     * @param $db_array
+     * @return bool|string
+     */
     public function save_custom($client_id, $db_array)
     {
         $result = $this->validate($db_array);
@@ -36,47 +37,62 @@ class Mdl_Client_Custom extends Validator
         return $result;
     }
 
+    /**
+     * @param null $id
+     * @return void
+     */
+    public function prep_form($id = null)
+    {
+        if ($id) {
+            $values = $this->get_by_client($id)->row();
+            $this->load->helper('custom_values_helper');
+            $this->load->module('custom_fields/mdl_custom_fields', 'cf');
+
+            foreach ($values as $key => $value) {
+                $type = $this->get_field_type($key);
+                if ($type != null) {
+                    $nicename = $this->cf->get_nicename(
+                        $type
+                    );
+                    $formatted = call_user_func("format_" . $nicename, $value);
+                    $this->set_form_value($key, $formatted);
+                }
+            }
+
+            parent::prep_form($id);
+        }
+    }
+
+    /**
+     * @param $client_id
+     * @return $this
+     */
     public function get_by_client($client_id)
     {
         $this->where('client_id', $client_id);
         return $this->get();
     }
 
-    public function prep_form($id = null)
-    {
-        if ($id) {
-            $values = $this->get_by_client($id)->row();
-            $this->load->helper("custom_values_helper");
-            $this->load->module("custom_fields/mdl_custom_fields", "cf");
-            foreach ($values as $key => $value) {
-              //echo $key;
-              $type = $this->get_field_type($key);
-              if ($type != null) {
-                $nicename = $this->cf->get_nicename(
-                  $type
-                );
-                $formatted = call_user_func("format_".$nicename, $value);
-                $this->set_form_value($key, $formatted);
-              }
-            }
-            parent::prep_form($id);
-        }
-    }
-
+    /**
+     * @return array
+     */
     public function db_array()
     {
         $db_array = parent::db_array();
-        $this->load->module("custom_fields/mdl_custom_fields");
+        $this->load->module('custom_fields/mdl_custom_fields');
         $fields = $this->mdl_custom_fields->get_by_table($table)->result();
+
         foreach ($fields as $field) {
             if ($field->custom_field_type == "DATE") {
                 $db_array[$field->custom_field_column] = date_to_mysql(
-                  $db_array[$field->custom_field_column]
+                    $db_array[$field->custom_field_column]
                 );
             } elseif ($field->custom_field_type == "MULTIPLE-CHOICE") {
                 $db_array[$field->custom_field_column] = implode(",", $db_array[$field->custom_field_column]);
             }
         }
+
         return $db_array;
     }
+
 }
--- application/modules/custom_fields/models/Mdl_payment_custom.php
+++ application/modules/custom_fields/models/Mdl_payment_custom.php
@@ -1,30 +1,32 @@
 <?php
-
-if (!defined('BASEPATH')) {
-    exit('No direct script access allowed');
-}
+if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 /*
  * InvoicePlane
  *
- * A free and open source web based invoicing system
- *
- * @package		InvoicePlane
- * @author		Kovah (www.kovah.de)
- * @copyright	Copyright (c) 2012 - 2015 InvoicePlane.com
+ * @author		InvoicePlane Developers & Contributors
+ * @copyright	Copyright (c) 2012 - 2017 InvoicePlane.com
  * @license		https://invoiceplane.com/license.txt
  * @link		https://invoiceplane.com
- *
  */
 
+/**
+ * Class Mdl_Payment_Custom
+ */
 class Mdl_Payment_Custom extends Validator
 {
     public $table = 'ip_payment_custom';
     public $primary_key = 'ip_payment_custom.payment_custom_id';
 
+    /**
+     * @param $payment_id
+     * @param $db_array
+     * @return bool|string
+     */
     public function save_custom($payment_id, $db_array)
     {
         $result = $this->validate($db_array);
+
         if ($result === true) {
             $db_array = $this->_formdata;
             $payment_custom_id = null;
@@ -38,8 +40,11 @@ class Mdl_Payment_Custom extends Validator
             }
 
             parent::save($payment_custom_id, $db_array);
+
             return true;
         }
+
         return $result;
     }
+
 }
