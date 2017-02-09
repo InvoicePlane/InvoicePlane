e217e82f4e4f22f36d836ba2d23323a389086301
Kovah <mail@kovah.de>
2017-02-09 17:24:09 +0100

    Updates code style and PHPDocs for the email templates module

--- application/modules/email_templates/controllers/Email_templates.php
+++ application/modules/email_templates/controllers/Email_templates.php
@@ -1,23 +1,23 @@
 <?php
-
-if (!defined('BASEPATH'))
-    exit('No direct script access allowed');
+if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 /*
  * InvoicePlane
  *
- * A free and open source web based invoicing system
- *
- * @package		InvoicePlane
- * @author		Kovah (www.kovah.de)
- * @copyright	Copyright (c) 2012 - 2015 InvoicePlane.com
+ * @author		InvoicePlane Developers & Contributors
+ * @copyright	Copyright (c) 2012 - 2017 InvoicePlane.com
  * @license		https://invoiceplane.com/license.txt
  * @link		https://invoiceplane.com
- *
  */
 
+/**
+ * Class Email_Templates
+ */
 class Email_Templates extends Admin_Controller
 {
+    /**
+     * Email_Templates constructor.
+     */
     public function __construct()
     {
         parent::__construct();
@@ -25,6 +25,9 @@ class Email_Templates extends Admin_Controller
         $this->load->model('mdl_email_templates');
     }
 
+    /**
+     * @param int $page
+     */
     public function index($page = 0)
     {
         $this->mdl_email_templates->paginate(site_url('email_templates/index'), $page);
@@ -35,6 +38,9 @@ class Email_Templates extends Admin_Controller
         $this->layout->render();
     }
 
+    /**
+     * @param null $id
+     */
     public function form($id = null)
     {
         if ($this->input->post('btn_cancel')) {
@@ -76,6 +82,9 @@ class Email_Templates extends Admin_Controller
         $this->layout->render();
     }
 
+    /**
+     * @param $id
+     */
     public function delete($id)
     {
         $this->mdl_email_templates->delete($id);
