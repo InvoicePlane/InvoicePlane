b19dd0b9d2dceb10a30ed64943c70857d0112514
Kovah <mail@kovah.de>
2017-02-10 10:38:03 +0100

    Updates code style and PHPDocs for the projects module

--- application/modules/projects/controllers/Projects.php
+++ application/modules/projects/controllers/Projects.php
@@ -1,24 +1,23 @@
 <?php
-
-if (!defined('BASEPATH'))
-    exit('No direct script access allowed');
+if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 /*
  * InvoicePlane
- * 
- * A free and open source web based invoicing system
  *
- * @package		InvoicePlane
- * @author		Kovah (www.kovah.de)
- * @copyright	Copyright (c) 2012 - 2014 InvoicePlane.com
+ * @author		InvoicePlane Developers & Contributors
+ * @copyright	Copyright (c) 2012 - 2017 InvoicePlane.com
  * @license		https://invoiceplane.com/license.txt
  * @link		https://invoiceplane.com
- * 
  */
 
+/**
+ * Class Projects
+ */
 class Projects extends Admin_Controller
 {
-
+    /**
+     * Projects constructor.
+     */
     public function __construct()
     {
         parent::__construct();
@@ -26,6 +25,9 @@ class Projects extends Admin_Controller
         $this->load->model('mdl_projects');
     }
 
+    /**
+     * @param int $page
+     */
     public function index($page = 0)
     {
         $this->mdl_projects->paginate(site_url('projects/index'), $page);
@@ -36,6 +38,9 @@ class Projects extends Admin_Controller
         $this->layout->render();
     }
 
+    /**
+     * @param null $id
+     */
     public function form($id = null)
     {
         if ($this->input->post('btn_cancel')) {
@@ -65,6 +70,9 @@ class Projects extends Admin_Controller
         $this->layout->render();
     }
 
+    /**
+     * @param $id
+     */
     public function delete($id)
     {
         $this->mdl_projects->delete($id);
@@ -72,5 +80,3 @@ class Projects extends Admin_Controller
     }
 
 }
-
-?>
--- application/modules/projects/models/Mdl_projects.php
+++ application/modules/projects/models/Mdl_projects.php
@@ -1,24 +1,20 @@
 <?php
-
-if (!defined('BASEPATH'))
-    exit('No direct script access allowed');
+if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 /*
  * InvoicePlane
- * 
- * A free and open source web based invoicing system
  *
- * @package		InvoicePlane
- * @author		Kovah (www.kovah.de)
- * @copyright	Copyright (c) 2012 - 2014 InvoicePlane.com
+ * @author		InvoicePlane Developers & Contributors
+ * @copyright	Copyright (c) 2012 - 2017 InvoicePlane.com
  * @license		https://invoiceplane.com/license.txt
  * @link		https://invoiceplane.com
- * 
  */
 
+/**
+ * Class Mdl_Projects
+ */
 class Mdl_Projects extends Response_Model
 {
-
     public $table = 'ip_projects';
     public $primary_key = 'ip_projects.project_id';
 
@@ -38,6 +34,9 @@ class Mdl_Projects extends Response_Model
         $this->db->join('ip_clients', 'ip_clients.client_id = ip_projects.client_id', 'left');
     }
 
+    /**
+     * @return array
+     */
     public function validation_rules()
     {
         return array(
@@ -54,5 +53,3 @@ class Mdl_Projects extends Response_Model
     }
 
 }
-
-?>
