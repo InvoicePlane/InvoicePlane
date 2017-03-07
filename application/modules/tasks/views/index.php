8dddf5493a879f9a4dfacda88455072368b02ff9
Kovah <mail@kovah.de>
2017-03-07 08:05:33 +0100

    Various improvements for the projects / tasks modules (IP-162)

--- application/modules/tasks/views/index.php
+++ application/modules/tasks/views/index.php
@@ -35,11 +35,15 @@
             <?php foreach ($tasks as $task) { ?>
                 <tr>
                     <td><?php echo $task->task_name; ?></td>
-                    <td><?php echo $task_statuses["$task->task_status"]["label"]; ?></td>
                     <td>
-                    <span class="<?php if ($task->is_overdue) { ?>font-overdue<?php } ?>">
-                    <?php echo date_from_mysql($task->task_finish_date); ?>
-                    </span>
+                        <span class="label <?php echo $task_statuses["$task->task_status"]['class']; ?>">
+                            <?php echo $task_statuses["$task->task_status"]['label']; ?>
+                        </span>
+                    </td>
+                    <td>
+                        <span class="<?php if ($task->is_overdue) { ?>text-danger<?php } ?>">
+                            <?php echo date_from_mysql($task->task_finish_date); ?>
+                        </span>
                     </td>
                     <td><?php echo $task->project_name; ?></td>
                     <td><?php echo format_currency($task->task_price); ?></td>
@@ -48,10 +52,13 @@
                            title="<?php echo trans('edit'); ?>">
                             <i class="fa fa-edit fa-margin"></i>
                         </a>
-                        <a href="<?php echo site_url('tasks/delete/' . $task->task_id); ?>"
-                           title="<?php echo trans('delete'); ?>"
-                           onclick="return confirm('<?php if ($task->task_status == 4) echo trans('alert_task_delete') . ' '; echo trans('delete_record_warning'); ?>');">
-                           <i class="fa fa-trash-o fa-margin"></i></a>
+                        <?php if (!($task->task_status == 4 && $this->config->item('enable_invoice_deletion') !== true)) : ?>
+                            <a href="<?php echo site_url('tasks/delete/' . $task->task_id); ?>"
+                               title="<?php echo trans('delete'); ?>"
+                               onclick="return confirm('<?php if ($task->task_status == 4) echo trans('alert_task_delete') . ' ' . trans('delete_record_warning'); ?>');">
+                                <i class="fa fa-trash-o fa-margin"></i>
+                            </a>
+                        <?php endif; ?>
                     </td>
                 </tr>
             <?php } ?>
