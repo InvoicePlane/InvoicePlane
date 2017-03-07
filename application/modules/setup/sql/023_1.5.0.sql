03d5cd07619c7aee58ff812edd943fb9a25cbe7f
Kovah <mail@kovah.de>
2017-03-07 07:26:28 +0100

    Corrects database migrations for 1.5.0

--- application/modules/setup/sql/023_1.5.0.sql
+++ application/modules/setup/sql/023_1.5.0.sql
@@ -117,3 +117,18 @@ ALTER TABLE `ip_clients`
 # Insert default theme into database (IP-338)
 INSERT INTO `ip_settings` (setting_key, setting_value)
   VALUES ('system_theme', 'invoiceplane');
+
+# Feature IP-162
+# Allow invoice item to refer to a table `products` or to table `tasks`
+ALTER TABLE `ip_invoice_items`
+  ADD COLUMN `item_task_id` int(11) DEFAULT NULL
+  AFTER `item_date_added`;
+
+# Add default hourly rate for tasks
+INSERT INTO `ip_settings` (`setting_key`, `setting_value`)
+VALUES ('default_hourly_rate', '0.00');
+
+# Add tax rate to tasks
+ALTER TABLE `ip_tasks`
+  ADD COLUMN `tax_rate_id` int(11) NOT NULL
+  AFTER `task_status`;
