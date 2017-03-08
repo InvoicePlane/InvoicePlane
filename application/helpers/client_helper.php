e3e2a2cc77df0f010063bae0b1c5ec8ebd66ee53
Kovah <mail@kovah.de>
2017-03-08 14:41:02 +0100

    Various fixes for user and client handling

--- application/helpers/client_helper.php
+++ application/helpers/client_helper.php
@@ -1,18 +1,20 @@
 <?php
-  function format_client($client)
-  {
-      if ($client->client_surname != "") {
-          return $client->client_name." ".$client->client_surname;
-      }
-      return $client->client_name;
-  }
 
-  function format_gender($gender){
-    if($gender == 0){
-      return trans('gender_male');
+function format_client($client)
+{
+    if ($client->client_surname != "") {
+        return $client->client_name . " " . $client->client_surname;
     }
-    if($gender == 1){
-      return trans('gender_female');
+    return $client->client_name;
+}
+
+function format_gender($gender)
+{
+    if ($gender == 0) {
+        return trans('gender_male');
+    }
+    if ($gender == 1) {
+        return trans('gender_female');
     }
     return trans('gender_other');
-  }
+}
