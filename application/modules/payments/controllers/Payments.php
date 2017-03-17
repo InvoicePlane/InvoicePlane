e3757451ac90ac601ed38fdb8f2615bb40ef9738
Kovah <mail@kovah.de>
2017-03-17 12:27:20 +0100

    Payments can only be entered for invoices where balance != 0

--- application/modules/payments/controllers/Payments.php
+++ application/modules/payments/controllers/Payments.php
@@ -103,10 +103,14 @@ class Payments extends Admin_Controller
         $this->load->model('custom_fields/mdl_custom_fields');
         $this->load->model('custom_values/mdl_custom_values');
 
-        $open_invoices = $this->mdl_invoices->where('ip_invoice_amounts.invoice_balance >', 0)->get()->result();
+        $open_invoices = $this->mdl_invoices
+            ->where('ip_invoice_amounts.invoice_balance >', 0)
+            ->or_where('ip_invoice_amounts.invoice_balance <', 0)
+            ->get()->result();
 
         $custom_fields = $this->mdl_custom_fields->by_table('ip_payment_custom')->get()->result();
         $custom_values = [];
+
         foreach ($custom_fields as $custom_field) {
             if (in_array($custom_field->custom_field_type, $this->mdl_custom_values->custom_value_fields())) {
                 $values = $this->mdl_custom_values->get_by_fid($custom_field->custom_field_id)->result();
