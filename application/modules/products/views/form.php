81c1c70526bea1c363886367c3b5f0a5def4ac77
Kovah <mail@kovah.de>
2017-03-22 10:47:05 +0100

    Update the product form

--- application/modules/products/views/form.php
+++ application/modules/products/views/form.php
@@ -1,4 +1,4 @@
-<form method="post" class="form-horizontal">
+<form method="post">
 
     <input type="hidden" name="_ip_csrf" value="<?= $this->security->get_csrf_hash() ?>">
 
@@ -12,7 +12,7 @@
         <?php $this->layout->load_view('layout/alerts'); ?>
 
         <div class="row">
-            <div class="col-xs-12 col-md-7">
+            <div class="col-xs-12 col-md-6">
                 <fieldset>
                     <legend>
                         <?php if ($this->mdl_products->form_value('product_id')) : ?>
@@ -24,136 +24,139 @@
                     </legend>
 
                     <div class="form-group">
-                        <div class="col-xs-12 col-sm-3 col-lg-2 text-right text-left-xs">
-                            <label class="control-label"><?php echo trans('family'); ?>: </label>
-                        </div>
-                        <div class="col-xs-12 col-sm-8 col-lg-8">
-                            <select name="family_id" id="family_id" class="form-control simple-select">
-                                <option value="0"><?php echo trans('select_family'); ?></option>
-                                <?php foreach ($families as $family) { ?>
-                                    <option value="<?php echo $family->family_id; ?>"
-                                            <?php if ($this->mdl_products->form_value('family_id') == $family->family_id) {
-                                            ?>selected="selected"<?php } ?>
-                                    ><?php echo $family->family_name; ?></option>
-                                <?php } ?>
-                            </select>
-                        </div>
+                        <label class="control-label" for="family_id">
+                            <?php echo trans('family'); ?>
+                        </label>
+
+                        <select name="family_id" id="family_id" class="form-control simple-select">
+                            <option value="0"><?php echo trans('select_family'); ?></option>
+                            <?php foreach ($families as $family) { ?>
+                                <option value="<?php echo $family->family_id; ?>"
+                                        <?php if ($this->mdl_products->form_value('family_id') == $family->family_id) {
+                                        ?>selected="selected"<?php } ?>
+                                ><?php echo $family->family_name; ?></option>
+                            <?php } ?>
+                        </select>
                     </div>
 
                     <div class="form-group">
-                        <div class="col-xs-12 col-sm-3 col-lg-2 text-right text-left-xs">
-                            <label class="control-label"><?php echo trans('product_sku'); ?>: </label>
-                        </div>
-                        <div class="col-xs-12 col-sm-8 col-lg-8">
-                            <input type="text" name="product_sku" id="product_sku" class="form-control"
-                                   value="<?php echo $this->mdl_products->form_value('product_sku'); ?>">
-                        </div>
+                        <label class="control-label" id="product_sku">
+                            <?php echo trans('product_sku'); ?>
+                        </label>
+
+                        <input type="text" name="product_sku" id="product_sku" class="form-control"
+                               value="<?php echo $this->mdl_products->form_value('product_sku'); ?>">
                     </div>
 
                     <div class="form-group">
-                        <div class="col-xs-12 col-sm-3 col-lg-2 text-right text-left-xs">
-                            <label class="control-label"><?php echo trans('product_tariff'); ?>: </label>
-                        </div>
-                        <div class="col-xs-12 col-sm-8 col-lg-8">
-                            <input type="text" name="product_tariff" id="product_tariff" class="form-control"
-                                   value="<?php echo $this->mdl_products->form_value('product_tariff'); ?>">
-                        </div>
+                        <label class="control-label" for="product_name">
+                            <?php echo trans('product_name'); ?>
+                        </label>
+
+                        <input type="text" name="product_name" id="product_name" class="form-control" required
+                               value="<?php echo $this->mdl_products->form_value('product_name'); ?>">
                     </div>
 
                     <div class="form-group">
-                        <div class="col-xs-12 col-sm-3 col-lg-2 text-right text-left-xs">
-                            <label class="control-label"><?php echo trans('product_name'); ?>: </label>
-                        </div>
-                        <div class="col-xs-12 col-sm-8 col-lg-8">
-                            <input type="text" name="product_name" id="product_name" class="form-control"
-                                   value="<?php echo $this->mdl_products->form_value('product_name'); ?>">
-                        </div>
+                        <label class="control-label" for="product_description">
+                            <?php echo trans('product_description'); ?>
+                        </label>
+
+                        <textarea name="product_description" id="product_description" class="form-control"
+                                  rows="3"><?php echo $this->mdl_products->form_value('product_description'); ?></textarea>
                     </div>
 
                     <div class="form-group">
-                        <div class="col-xs-12 col-sm-3 col-lg-2 text-right text-left-xs">
-                            <label class="control-label"><?php echo trans('product_description'); ?>: </label>
-                        </div>
-                        <div class="col-xs-12 col-sm-8 col-lg-8">
-                            <textarea name="product_description" id="product_description" class="form-control"
-                                      rows="3"><?php echo $this->mdl_products->form_value('product_description'); ?></textarea>
-                        </div>
-                    </div>
+                        <label class="control-label" for="product_price">
+                            <?php echo trans('product_price'); ?>
+                        </label>
 
-                    <div class="form-group">
-                        <div class="col-xs-12 col-sm-3 col-lg-2 text-right text-left-xs">
-                            <label class="control-label"><?php echo trans('product_price'); ?>: </label>
-                        </div>
-                        <div class="col-xs-12 col-sm-8 col-lg-8">
+                        <div class="input-group has-feedback">
                             <input type="text" name="product_price" id="product_price" class="form-control"
                                    value="<?php echo format_amount($this->mdl_products->form_value('product_price')); ?>">
+                            <span class="input-group-addon"><?php echo get_setting('currency_symbol'); ?></span>
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <div class="col-xs-12 col-sm-3 col-lg-2 text-right text-left-xs">
-                            <label class="control-label"><?php echo trans('product_unit'); ?>: </label>
-                        </div>
-                        <div class="col-xs-12 col-sm-8 col-lg-8">
-                            <select name="unit_id" id="unit_id" class="form-control simple-select">
-                                <option value="0"><?php echo trans('select_unit'); ?></option>
-                                <?php foreach ($units as $unit) { ?>
-                                    <option value="<?php echo $unit->unit_id; ?>"
-                                            <?php if ($this->mdl_products->form_value('unit_id') == $unit->unit_id) {
-                                            ?>selected="selected"<?php } ?>
-                                    ><?php echo $unit->unit_name . '/' . $unit->unit_name_plrl; ?></option>
-                                <?php } ?>
-                            </select>
-                        </div>
+                        <label class="control-label" for="unit_id">
+                            <?php echo trans('product_unit'); ?>
+                        </label>
+
+                        <select name="unit_id" id="unit_id" class="form-control simple-select">
+                            <option value="0"><?php echo trans('select_unit'); ?></option>
+                            <?php foreach ($units as $unit) { ?>
+                                <option value="<?php echo $unit->unit_id; ?>"
+                                        <?php if ($this->mdl_products->form_value('unit_id') == $unit->unit_id) {
+                                        ?>selected="selected"<?php } ?>
+                                ><?php echo $unit->unit_name . '/' . $unit->unit_name_plrl; ?></option>
+                            <?php } ?>
+                        </select>
                     </div>
 
                     <div class="form-group">
-                        <div class="col-xs-12 col-sm-3 col-lg-2 text-right text-left-xs">
-                            <label class="control-label"><?php echo trans('tax_rate'); ?>: </label>
-                        </div>
-                        <div class="col-xs-12 col-sm-8 col-lg-8">
-                            <select name="tax_rate_id" id="tax_rate_id" class="form-control simple-select">
-                                <option value="0"><?php echo trans('none'); ?></option>
-                                <?php foreach ($tax_rates as $tax_rate) { ?>
-                                    <option value="<?php echo $tax_rate->tax_rate_id; ?>"
-                                            <?php if ($this->mdl_products->form_value('tax_rate_id') == $tax_rate->tax_rate_id) {
-                                            ?>selected="selected" <?php } ?>>
-                                        <?php echo $tax_rate->tax_rate_name
-                                            . ' (' . format_amount($tax_rate->tax_rate_percent) . '%)'; ?>
-                                    </option>
-                                <?php } ?>
-                            </select>
-                        </div>
+                        <label class="control-label" for="tax_rate_id">
+                            <?php echo trans('tax_rate'); ?>
+                        </label>
+
+                        <select name="tax_rate_id" id="tax_rate_id" class="form-control simple-select">
+                            <option value="0"><?php echo trans('none'); ?></option>
+                            <?php foreach ($tax_rates as $tax_rate) { ?>
+                                <option value="<?php echo $tax_rate->tax_rate_id; ?>"
+                                        <?php if ($this->mdl_products->form_value('tax_rate_id') == $tax_rate->tax_rate_id) {
+                                        ?>selected="selected" <?php } ?>>
+                                    <?php echo $tax_rate->tax_rate_name
+                                        . ' (' . format_amount($tax_rate->tax_rate_percent) . '%)'; ?>
+                                </option>
+                            <?php } ?>
+                        </select>
                     </div>
 
                 </fieldset>
             </div>
 
-            <div class="col-xs-12 col-md-5">
+            <div class="col-xs-12 col-md-6">
+
                 <fieldset>
                     <legend><?php echo trans('extra_information'); ?></legend>
 
                     <div class="form-group">
-                        <div class="col-xs-12 col-sm-3 col-lg-2 text-right text-left-xs">
-                            <label class="control-label"><?php echo trans('provider_name'); ?>: </label>
-                        </div>
-                        <div class="col-xs-12 col-sm-8 col-lg-8">
-                            <input type="text" name="provider_name" id="provider_name" class="form-control"
-                                   value="<?php echo $this->mdl_products->form_value('provider_name'); ?>">
-                        </div>
+                        <label class="control-label" for="provider_name">
+                            <?php echo trans('provider_name'); ?>
+                        </label>
+
+                        <input type="text" name="provider_name" id="provider_name" class="form-control"
+                               value="<?php echo $this->mdl_products->form_value('provider_name'); ?>">
                     </div>
 
                     <div class="form-group">
-                        <div class="col-xs-12 col-sm-3 col-lg-2 text-right text-left-xs">
-                            <label class="control-label"><?php echo trans('purchase_price'); ?>: </label>
-                        </div>
-                        <div class="col-xs-12 col-sm-8 col-lg-8">
+                        <label class="control-label" for="purchase_price">
+                            <?php echo trans('purchase_price'); ?>
+                        </label>
+
+                        <div class="input-group has-feedback">
                             <input type="text" name="purchase_price" id="purchase_price" class="form-control"
                                    value="<?php echo format_amount($this->mdl_products->form_value('purchase_price')); ?>">
+                            <span class="input-group-addon"><?php echo get_setting('currency_symbol'); ?></span>
                         </div>
                     </div>
 
                 </fieldset>
+
+                <fieldset>
+                    <legend><?php echo trans('invoice_sumex'); ?></legend>
+
+                    <div class="form-group">
+                        <label class="control-label" for="product_tariff">
+                            <?php echo trans('product_tariff'); ?>
+                        </label>
+
+                        <input type="text" name="product_tariff" id="product_tariff" class="form-control"
+                               value="<?php echo $this->mdl_products->form_value('product_tariff'); ?>">
+                    </div>
+
+                </fieldset>
+
             </div>
         </div>
 
