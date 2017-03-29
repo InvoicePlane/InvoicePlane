7939d95aeccb90e4bdc03cb637cfa71d488126f6
Kovah <mail@kovah.de>
2017-03-29 18:43:27 +0200

    IP-504 - Unify the interface: email templates model

--- assets/core/js/scripts.js
+++ assets/core/js/scripts.js
@@ -176,19 +176,9 @@ $(document).ready(function () {
     // Select2 for all select inputs
     $(".simple-select").select2();
 
-    // Handle click event for Email Template Tags insertion
-    // Example Usage
-    // <a href="#" class="text-tag" data-tag="{{{client_name}}}">Client Name</a>
-    var lastTaggableClicked;
-    $('.text-tag').bind('click', function () {
-        var templateTag = this.getAttribute("data-tag");
-        insert_at_caret(lastTaggableClicked.id, templateTag);
-        return false;
-    });
-
     // Keep track of the last "taggable" input/textarea
     $('.taggable').on('focus', function () {
-        lastTaggableClicked = this;
+        window.lastTaggableClicked = this;
     });
 
     // HTML tags to email templates textarea
