c571b804aace66196979cf0685f2e89d959433b8
Kovah <mail@kovah.de>
2017-04-11 10:45:51 +0200

    Makes error pages work standalone

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -1,39 +1,42 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-$CI =& get_instance();
-if (!isset($CI)) $CI = new CI_Controller();;
-$CI->load->helper('url');
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html style="display:table;width:100%;">
 <head>
     <meta charset="utf-8">
-    <title>404 Page Not Found</title>
-    <link rel="stylesheet"
-          href="<?php echo base_url('assets/invoiceplane/css/style.css'); ?>">
-    <link rel="stylesheet" href="<?php echo base_url('assets/core/css/custom.css'); ?>">
+    <title>InvoicePlane - <?php echo $heading; ?></title>
+    <style>
+        html,
+        html * {
+            box-sizing: border-box;
+        }
+
+        body {
+            font-family: sans-serif;
+            background: #B94A48;
+            color: #fff;
+            height: 100vh;
+            display: table-cell;
+            vertical-align: middle;
+            text-align: center;
+            padding: 2vh 2vw;
+        }
+
+        h4 {
+            font-size: 30px;
+            text-align: center;
+            width: 100%;
+        }
+
+        p {
+            font-size: 16px;
+            width: 100%;
+        }
+    </style>
 </head>
-<body class="has-error">
-
-<div class="container text-center">
-    <img src="<?php echo base_url('assets/core/img/logo_400x200.png') ?>"/>
-
-    <div class="row">
-        <div class="col-xs-12 col-md-8 col-md-offset-2">
-
-            <br>
-            <div class="panel panel-danger">
-                <div class="panel-heading">
-                    <?php echo $heading; ?>
-                </div>
-                <div class="panel-body text-danger">
-                    <?php echo $message; ?>
-                </div>
-            </div>
-
-        </div>
-    </div>
-</div>
-
+<body>
+<h4><?php echo $heading; ?></h4>
+<p><?php echo $message; ?></p>
 </body>
 </html>
