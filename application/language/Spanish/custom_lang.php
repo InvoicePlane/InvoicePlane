8b0521d0bea6b6f541832035dc1de70712dcd408
Daniel Fernández <13066282+daniel89fg@users.noreply.github.com>
2017-09-18 13:38:22 +0200

    Add files via upload

---
+++ application/language/Spanish/custom_lang.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * CUSTOM LANGUAGE STRINGS
+ *
+ * Add your custom Language Strings here with the following scheme:
+ *
+ * 'language_string_key' => 'Actual content of the Language String',
+ *
+ * Please notice that 'language_string_key' should only contain alphanumeric
+ * characters and underscores.
+ * If you are not sure take a look at the ip_lang.php file or visit the
+ * community forums.
+ */
+
+$lang = array(
+	'mascotas'	=> 'Mascotas',
+	'ver_mascotas'	=> 'Ver mascotas',
+	'anadir_mascota'	=> 'Añadir mascota',
+	'raza'	=> 'Raza',
+	'enfermedades'	=> 'Enfermedades',
+	'chip'	=> 'Chip',
+	'ubicacion'	=> 'Ubicación',
+	'mascotas_form'	=> 'Formulario de mascotas',
+	'ver_mascota'	=> 'Ver mascota',
+	'informacion'	=> 'Información',
+	'fecha_creacion'	=> 'Fecha de creación',
+);
