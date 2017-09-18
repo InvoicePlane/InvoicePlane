c8e1a6a843cf457ab9d0672f0bff556ebba8cb5b
Daniel Fernández <13066282+daniel89fg@users.noreply.github.com>
2017-09-18 14:49:41 +0200

    tab fix

--- application/modules/user_clients/models/Mdl_user_clients.php
+++ application/modules/user_clients/models/Mdl_user_clients.php
@@ -86,7 +86,7 @@ class Mdl_User_Clients extends MY_Model
         }
     }
     
-    public function get_users_all_clients()
+    public function GetUsersAllClients()
     {
         $this->load->model('users/mdl_users');
         $users = $this->mdl_users->where('user_all_clients', 1)->get()->result();
