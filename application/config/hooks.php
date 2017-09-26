5499200f5b9218836fd027036e80e4f1456522bb
Daniel Fernández <13066282+daniel89fg@users.noreply.github.com>
2017-09-26 13:21:54 +0200

    Edit hook
    El hook debe llamar a la función despues de cargar el constructor. Si lo llamamos antes no a cargado el core.
    
    ---
    
    The hook should call the function after loading the constructor. If we call before the core is not loaded.

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -12,7 +12,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$hook['pre_controller'] = array(
+$hook['post_controller_constructor'] = array(
     'class' => 'SetTimezoneClass',
     'function' => 'setTimezone',
     'filename' => 'SetTimezoneClass.php',
