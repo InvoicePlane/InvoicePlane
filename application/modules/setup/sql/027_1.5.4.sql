53c858cf667ebb840587e19013d175ffb1a1ed5d
Daniel Fernández <13066282+daniel89fg@users.noreply.github.com>
2017-09-26 09:38:20 +0200

    News tables warehouses
    Se han creado dos tablas nuevas para almacenar los almacenes y las entradas y salidas de los productos. Así como se añadierón dos nuevas lineas en la tabla de productos para enlazarlo con el almacen y ponerle stock.
    
    ---
    
    Two new tables have been created to store the warehouses and the inputs and outputs of the products. As well as adding two new lines in the table of products to link it with the store and put it stock.

--- application/modules/setup/sql/027_1.5.4.sql
+++ application/modules/setup/sql/027_1.5.4.sql
@@ -7,3 +7,29 @@ COLLATE utf8_general_ci NOT NULL;
 ALTER TABLE `ip_invoice_items`
   ADD `item_is_recurring` TINYINT(1)
   AFTER `item_order`;
+
+CREATE TABLE `ip_warehouses` (
+  `warehouse_id`   INT(11)  NOT NULL AUTO_INCREMENT,
+  `warehouse_name` LONGTEXT NOT NULL,
+  `warehouse_location` LONGTEXT NOT NULL,
+  PRIMARY KEY (`warehouse_id`)
+)
+  ENGINE = MyISAM
+  DEFAULT CHARSET = utf8;
+
+CREATE TABLE `ip_warehouse_products` (
+  `war_pro_id`   INT(11)  NOT NULL AUTO_INCREMENT,  
+  `warehouse_id`   INT(11)  NOT NULL,
+  `product_id`   INT(11)  NOT NULL,
+  `product_qty`   INT(11)  NOT NULL,
+  `war_pro_type` INT(1) NOT NULL,
+  `war_pro_date` DATETIME NOT NULL,
+  `user_id` INT(1) NOT NULL,
+  PRIMARY KEY (`war_pro_id`)
+)
+  ENGINE = MyISAM
+  DEFAULT CHARSET = utf8;
+
+ALTER TABLE ip_products
+  ADD COLUMN product_qty INT(11) NULL,
+  ADD COLUMN warehouse_id INT(11) NULL;
