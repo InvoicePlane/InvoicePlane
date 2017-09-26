0bcb446bc850df5d4d976681de2ba096104893c8
Daniel Fernández <13066282+daniel89fg@users.noreply.github.com>
2017-09-26 09:36:47 +0200

    Only number javascript
    Se crea una nueva función que permite ingresar sólo números en los campos input.
    
    ---
    
    A new function is created that allows entering only numbers in the input fields.

--- assets/core/js/scripts.js
+++ assets/core/js/scripts.js
@@ -251,4 +251,11 @@ $(document).ready(function () {
             }
         });
     }
+    
+    //NUMEROS ENTEROS
+    $(document).on('keypress', '.numeric', function(e){
+        if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+            return false;
+        }
+    });
 });
