5f72916f593ef794318bc79a3fa6e90d1988536b
Maxime Grimler <kalimerre@gmail.com>
2017-11-13 10:59:27 +0100

    [IP-620] - Space in invoice name will be downloaded as corrupted

--- application/modules/invoices/controllers/Invoices.php
+++ application/modules/invoices/controllers/Invoices.php
@@ -107,6 +107,7 @@ class Invoices extends Admin_Controller
      */
     public function download($invoice)
     {
+        $invoice = urldecode($invoice);
         header('Content-type: application/pdf');
         header('Content-Disposition: attachment; filename="' . urldecode($invoice) . '"');
         readfile('./uploads/archive/' . urldecode($invoice));
