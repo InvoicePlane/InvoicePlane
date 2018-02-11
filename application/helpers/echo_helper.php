9077a15b8712aae0c812e4c9491bce8cfdaa3aeb
Kovah <mail@kovah.de>
2018-02-11 12:30:13 +0100

    Append version to asset paths to prevent browser cache issues (IP-641) Also makes current version available in the settings variables

--- application/helpers/echo_helper.php
+++ application/helpers/echo_helper.php
@@ -12,7 +12,9 @@ if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 /**
  * Shorthand for htmlspecialchars()
+ *
  * @param $output
+ *
  * @return string
  */
 function htmlsc($output)
@@ -24,6 +26,8 @@ function htmlsc($output)
  * Echo something with escaped HTML special chars
  *
  * @param mixed $output
+ *
+ * @return void
  */
 function _htmlsc($output)
 {
@@ -34,6 +38,8 @@ function _htmlsc($output)
  * Echo something with escaped HTML entities
  *
  * @param mixed $output
+ *
+ * @return void
  */
 function _htmle($output)
 {
@@ -46,6 +52,8 @@ function _htmle($output)
  * @param string $line
  * @param string $id
  * @param null|string $default
+ *
+ * @return void
  */
 function _trans($line, $id = '', $default = null)
 {
@@ -58,16 +66,49 @@ function _trans($line, $id = '', $default = null)
  * @param $str
  * @param string $type
  * @param bool $popup
+ *
+ * @return void
  */
-function _auto_link($str, $type = 'both', $popup = FALSE) {
+function _auto_link($str, $type = 'both', $popup = false)
+{
     echo auto_link(htmlsc($str), $type, $popup);
 }
 
 /**
  * Output the standard CSRF protection field
+ *
+ * @return void
  */
-function _csrf_field() {
+function _csrf_field()
+{
     $CI = &get_instance();
     echo '<input type="hidden" name="' . $CI->config->item('csrf_token_name');
     echo '" value="' . $CI->security->get_csrf_hash() . '">';
+}
+
+/**
+ * Returns the correct URL for a asset within the theme directory
+ * Also appends the current version to the asset to prevent browser caching issues
+ *
+ * @param string $asset
+ *
+ * @return void
+ */
+function _theme_asset($asset)
+{
+    echo base_url() . 'assets/' . get_setting('system_theme', 'invoiceplane');
+    echo '/' . $asset . '?v=' . get_setting('current_version');
+}
+
+/**
+ * Returns the correct URL for a asset within the core directory
+ * Also appends the current version to the asset to prevent browser caching issues
+ *
+ * @param string $asset
+ *
+ * @return void
+ */
+function _core_asset($asset)
+{
+    echo base_url() . 'assets/core/' . $asset . '?v=' . get_setting('current_version');
 }
