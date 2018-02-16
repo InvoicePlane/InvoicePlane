f0505e65f22803490e5c3dd6f4f1d449f8a9c66a
Kovah <mail@kovah.de>
2018-02-16 16:12:55 +0100

    Update payment processing and implement payment logs in frontend (IP-650)

--- application/language/english/gateway_lang.php
+++ application/language/english/gateway_lang.php
@@ -12,8 +12,10 @@ $lang = array(
     'online_payment_creditcard_hint'     => 'If you want to pay via credit card please enter the information below.<br/>The credit card information are not stored on our servers and will be transferred to the online payment gateway using a secure connection.',
     'enable_online_payments'             => 'Enable Online Payments',
     'payment_provider'                   => 'Payment Provider',
+    'provider_response'                  => 'Provider Response',
     'add_payment_provider'               => 'Add a Payment Provider',
     'transaction_reference'              => 'Transaction Reference',
+    'transaction_successful'             => 'Transaction successful',
     'payment_description'                => 'Payment for Invoice %s',
 
     // Credit card strings
