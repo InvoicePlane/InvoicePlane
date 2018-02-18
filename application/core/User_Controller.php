ddd4bc9fbff7bf1839f6d438d5352730db0065ce
Kovah <mail@kovah.de>
2018-02-18 11:28:47 +0100

    Hotfix the user controller Login was not possible anymore after code corrections because of the strict type check

--- application/core/User_Controller.php
+++ application/core/User_Controller.php
@@ -28,7 +28,7 @@ class User_Controller extends Base_Controller
     {
         parent::__construct();
 
-        if ($this->session->userdata($required_key) !== $required_val) {
+        if ($this->session->userdata($required_key) != $required_val) {
             redirect('sessions/login');
         }
     }
