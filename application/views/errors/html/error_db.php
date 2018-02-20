40a5036723f35a8679d883207ace8bf7e4324ebc
Kovah <mail@kovah.de>
2018-02-20 14:00:26 +0100

    Update the error views with the IP styling

--- application/views/errors/html/error_db.php
+++ application/views/errors/html/error_db.php
@@ -1,65 +1,30 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html>
+<html style="display:table;width:100%;">
 <head>
     <meta charset="utf-8">
-    <title>Database Error</title>
-    <style type="text/css">
-
-        ::selection { background-color: #E13300; color: white; }
-
-        ::-moz-selection { background-color: #E13300; color: white; }
-
+    <title>InvoicePlane - <?php echo $heading; ?></title>
+    <style>
         body {
-            background-color: #fff;
-            margin: 40px;
-            font: 13px/20px normal Helvetica, Arial, sans-serif;
-            color: #4F5155;
+            font-family: sans-serif;
+            font-size: 16px;
+            background: #B94A48;
+            color: #fff;
+            padding: 2vw;
         }
 
-        a {
-            color: #003399;
-            background-color: transparent;
-            font-weight: normal;
-        }
-
-        h1 {
-            color: #444;
-            background-color: transparent;
-            border-bottom: 1px solid #D0D0D0;
-            font-size: 19px;
-            font-weight: normal;
-            margin: 0 0 14px 0;
-            padding: 14px 15px 10px 15px;
-        }
-
-        code {
-            font-family: Consolas, Monaco, Courier New, Courier, monospace;
-            font-size: 12px;
-            background-color: #f9f9f9;
-            border: 1px solid #D0D0D0;
-            color: #002166;
-            display: block;
-            margin: 14px 0 14px 0;
-            padding: 12px 10px 12px 10px;
-        }
-
-        #container {
-            margin: 10px;
-            border: 1px solid #D0D0D0;
-            box-shadow: 0 0 8px #D0D0D0;
+        h4 {
+            font-size: 200%;
+            padding-bottom: 5px;
+            border-bottom: 2px solid #B14240;
         }
 
         p {
-            margin: 12px 15px 12px 15px;
+            font-size: 100%;
         }
     </style>
 </head>
 <body>
-<div id="container">
-    <h1><?php echo $heading; ?></h1>
-    <?php echo $message; ?>
-</div>
+<h4><?php echo $heading; ?></h4>
+<p><?php echo $message; ?></p>
 </body>
 </html>
