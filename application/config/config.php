229605b4e4a50ed9e79f030bbb874c764cb7184a
Kovah <mail@kovah.de>
2018-02-20 01:38:21 +0100

    Update the config and import the Modular Extension

--- application/config/config.php
+++ application/config/config.php
@@ -114,7 +114,7 @@ $config['enable_hooks'] = false;
 | https://codeigniter.com/user_guide/general/creating_libraries.html
 |
 */
-$config['subclass_prefix'] = 'MY_';
+$config['subclass_prefix'] = 'IP_';
 
 /*
 |--------------------------------------------------------------------------
---
+++ application/core/IP_Loader.php
@@ -0,0 +1,18 @@
+<?php defined('BASEPATH') || exit('No direct script access allowed');
+
+// Load the HMVC Router
+require_once(dirname(__FILE__) . '/MX/Loader.php');
+
+/**
+ * Class IP_Loader
+ *
+ * @package      InvoicePlane
+ * @author       InvoicePlane Developers & Contributors
+ * @copyright    Copyright (c) 2012 - 2018, InvoicePlane (https://invoiceplane.com/)
+ * @license      http://opensource.org/licenses/MIT	MIT License
+ * @link         https://invoiceplane.com
+ */
+class IP_Loader extends MX_Loader
+{
+
+}
