b857ec01ee7dab748b6124a1613bd634d8c79825
Kovah <mail@kovah.de>
2018-05-13 22:50:59 +0200

    Fix uppercase PHP keywords

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 $this->load->helper('url');
 ?><!DOCTYPE html>
 <html lang="en">
