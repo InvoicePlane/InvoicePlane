b60adc0fae046f521d9eb8273dd4401f15dce8f0
Kovah <mail@kovah.de>
2018-05-29 21:06:41 +0200

    Set correct default skin in SkinComposer

--- app/Composers/SkinComposer.php
+++ app/Composers/SkinComposer.php
@@ -18,7 +18,7 @@ class SkinComposer
 {
     public function compose($view)
     {
-        $skin = (config('fi.skin') ?: 'skin-invoiceplane.css');
+        $skin = (config('fi.skin') ?: 'skin-invoiceplane.min.css');
         $view->with('skin', $skin);
         $view->with('skinClass', str_replace('.min.css', '', $skin));
     }
