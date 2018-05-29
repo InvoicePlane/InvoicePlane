e5a563327285bdb3f314ef61fac872bfe962711f
Kovah <mail@kovah.de>
2018-05-29 17:39:15 +0200

    Fix skin helper for settings module

--- app/Support/Skins.php
+++ app/Support/Skins.php
@@ -18,21 +18,18 @@ class Skins
 {
     public static function lists()
     {
-        $skins = Directory::listAssocContents(public_path('assets/dist/css/skins'));
+        $skins = Directory::listAssocContents(public_path('assets/dist/adminlte/css/skins'));
 
         unset($skins['_all-skins.css'], $skins['_all-skins.min.css']);
 
-        foreach ($skins as $skin)
-        {
-            if (!strstr($skin, '.min.css'))
-            {
+        foreach ($skins as $skin) {
+            if (!strstr($skin, '.min.css')) {
                 unset($skins[$skin]);
                 continue;
             }
 
             $skins[$skin] = str_replace('skin-', '', $skins[$skin]);
             $skins[$skin] = str_replace('.min.css', '', $skins[$skin]);
-
         }
 
         return $skins;
