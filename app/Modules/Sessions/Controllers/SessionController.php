7cd9b50d1646644ce323e12f2400a8ad3d4bc156
Kovah <mail@kovah.de>
2018-05-29 11:58:12 +0200

    Fix session module and update font sources in app.scss

--- app/Modules/Sessions/Controllers/SessionController.php
+++ app/Modules/Sessions/Controllers/SessionController.php
@@ -24,20 +24,18 @@ class SessionController extends Controller
         deleteTempFiles();
         deleteViewCache();
 
-        return view('sessions.login');
+        return view('sessions.login')->with('skin', config('fi.skin'));
     }
 
     public function attempt(SessionRequest $request)
     {
         $rememberMe = ($request->input('remember_me')) ? true : false;
 
-        if (!auth()->attempt(['email' => $request->input('email'), 'password' => $request->input('password')], $rememberMe))
-        {
+        if (!auth()->attempt(['email' => $request->input('email'), 'password' => $request->input('password')], $rememberMe)) {
             return redirect()->route('session.login')->with('error', trans('fi.invalid_credentials'));
         }
 
-        if (!auth()->user()->client_id)
-        {
+        if (!auth()->user()->client_id) {
             return redirect()->route('dashboard.index');
         }
 
--- app/Modules/Sessions/Views/sessions/login.blade.php
+++ app/Modules/Sessions/Views/sessions/login.blade.php
@@ -5,15 +5,9 @@
     <title>{{ trans('fi.welcome') }}</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 
-    <link href="{{ asset('favicon.png') }}" rel="icon" type="image/png">
-    <link href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css"/>
-    <link href="{{ asset('assets/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css"/>
-    <link href="{{ asset('assets/dist/css/AdminLTE.min.css') }}" rel="stylesheet" type="text/css"/>
-    <link href="{{ asset('assets/style.css') }}" rel="stylesheet" type="text/css"/>
+    @include('layouts._head')
 
-    @if (file_exists(base_path('custom/custom.css')))
-        <link href="{{ asset('custom/custom.css') }}" rel="stylesheet" type="text/css"/>
-    @endif
+    @include('layouts._js_global')
 
 </head>
 <body class="login-page">
@@ -23,11 +17,11 @@
         {!! Form::open() !!}
         <div class="form-group has-feedback">
             <input type="email" name="email" id="email" class="form-control" placeholder="{{ trans('fi.email') }}">
-            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+            <span class="fa fa-envelope form-control-feedback"></span>
         </div>
         <div class="form-group has-feedback">
             <input type="password" name="password" class="form-control" placeholder="{{ trans('fi.password') }}">
-            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+            <span class="fa fa-lock form-control-feedback"></span>
         </div>
         <div class="row">
             <div class="col-xs-8">
@@ -46,15 +40,6 @@
 
     </div>
 </div>
-
-<script src="{{ asset('assets/plugins/jQuery/jQuery.min.js') }}"></script>
-<script src="{{ asset('assets/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
-
-<script type="text/javascript">
-    $(function () {
-        $('#email').focus();
-    });
-</script>
 
 </body>
 </html>
