7f381ab93c14a688cd70607757bcad97ccfcc8a9
Kovah <mail@kovah.de>
2018-06-13 18:19:32 +0200

    Correct the addon provider to correctly abort while running tests

--- app/Providers/AddonServiceProvider.php
+++ app/Providers/AddonServiceProvider.php
@@ -10,52 +10,55 @@ class AddonServiceProvider extends ServiceProvider
 {
     public function boot(Request $request)
     {
-        if ($request->segment(1) !== 'setup' and (!app()->runningInConsole() or $this->app->environment('testing'))) {
-            config(['fi.menus.navigation' => []]);
-            config(['fi.menus.system' => []]);
-            config(['fi.menus.reports' => []]);
+        if ($request->segment(1) == 'setup' || app()->runningInConsole() || $this->app->environment() == 'testing') {
+            // Abort if the app is currently curring in the CLI, is running tests or the setup
+            return;
+        }
 
-            // Get the enabled addons.
-            $addons = Addon::where('enabled', 1)->orderBy('name')->get();
+        config(['fi.menus.navigation' => []]);
+        config(['fi.menus.system' => []]);
+        config(['fi.menus.reports' => []]);
 
-            // For each enabled addon, load the appropriate things.
-            foreach ($addons as $addon) {
-                if (isset($addon->navigation_menu) and $addon->navigation_menu) {
-                    config(['fi.menus.navigation.' . $addon->id => $addon->navigation_menu]);
-                }
+        // Get the enabled addons.
+        $addons = Addon::where('enabled', 1)->orderBy('name')->get();
 
-                if (isset($addon->navigation_reports) and $addon->navigation_reports) {
-                    config(['fi.menus.reports.' . $addon->id => $addon->navigation_reports]);
-                }
+        // For each enabled addon, load the appropriate things.
+        foreach ($addons as $addon) {
+            if (isset($addon->navigation_menu) and $addon->navigation_menu) {
+                config(['fi.menus.navigation.' . $addon->id => $addon->navigation_menu]);
+            }
 
-                if (isset($addon->system_menu) and $addon->system_menu) {
-                    config(['fi.menus.system.' . $addon->id => $addon->system_menu]);
-                }
+            if (isset($addon->navigation_reports) and $addon->navigation_reports) {
+                config(['fi.menus.reports.' . $addon->id => $addon->navigation_reports]);
+            }
 
-                // Scan addon directories for routes, views and language files.
-                $routesPath = addon_path($addon->path . '/routes.php');
-                $viewsPath = addon_path($addon->path . '/Views');
-                $langPath = addon_path($addon->path . '/Lang');
+            if (isset($addon->system_menu) and $addon->system_menu) {
+                config(['fi.menus.system.' . $addon->id => $addon->system_menu]);
+            }
 
-                if (file_exists($routesPath)) {
-                    require $routesPath;
-                }
+            // Scan addon directories for routes, views and language files.
+            $routesPath = addon_path($addon->path . '/routes.php');
+            $viewsPath = addon_path($addon->path . '/Views');
+            $langPath = addon_path($addon->path . '/Lang');
 
-                if (file_exists($viewsPath)) {
-                    $this->app->view->addLocation($viewsPath);
-                }
+            if (file_exists($routesPath)) {
+                require $routesPath;
+            }
 
-                if (file_exists($langPath)) {
-                    $this->loadTranslationsFrom($langPath, 'addon');
+            if (file_exists($viewsPath)) {
+                $this->app->view->addLocation($viewsPath);
+            }
 
-                    $this->loadTranslationsFrom($langPath, $addon->path);
-                }
+            if (file_exists($langPath)) {
+                $this->loadTranslationsFrom($langPath, 'addon');
 
-                $providerPath = addon_path($addon->path . '/AddonServiceProvider.php');
+                $this->loadTranslationsFrom($langPath, $addon->path);
+            }
 
-                if (file_exists($providerPath)) {
-                    $this->app->register('Addons\\' . $addon->path . '\AddonServiceProvider');
-                }
+            $providerPath = addon_path($addon->path . '/AddonServiceProvider.php');
+
+            if (file_exists($providerPath)) {
+                $this->app->register('Addons\\' . $addon->path . '\AddonServiceProvider');
             }
         }
     }
