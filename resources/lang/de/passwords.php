4da915509ffded424b621d942f355cf28d95ad9f
Dennis Ploeger <dennis.ploeger@kps.com>
2018-07-13 19:03:22 +0200

    German translation completed

--- resources/lang/de/passwords.php
+++ resources/lang/de/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'password' => 'Passwörter müssen mindestens sechs Zeichen haben und der Bestätigung entsprechen.',
+    'reset' => 'Dein Passwort wurde zurückgesetzt!',
+    'sent' => 'Wir haben Ihnen einen Link zur Passwortzurücksetzung geschickt!',
+    'token' => 'Dieser Schlüssel zur Passwortzurücksetzung ist ungültig.',
+    'user' => "Wir konnten keinen Benutzer mit der E-Mailadresse finden.",
 
 ];
