6ad2eee10c401e0d42cad0d1f9fb709bcd23441e
Kovah <mail@kovah.de>
2018-08-09 17:18:18 +0200

    Fix styling of the dashboard

--- app/Modules/Dashboard/Views/dashboard/index.blade.php
+++ app/Modules/Dashboard/Views/dashboard/index.blade.php
@@ -5,15 +5,17 @@
     @include('layouts._alerts')
 
     <section class="content-header">
-        <h1>@lang('ip.dashboard')</h1>
+        <h2 class="mb-4">@lang('ip.dashboard')</h2>
     </section>
 
-    @foreach ($widgets as $widget)
-        @if (config('ip.widgetEnabled' . $widget))
-            <div class="col-sm-12 col-md-{{ config('ip.widgetColumnWidth' . $widget, 6) }}">
-                @include($widget . 'Widget')
-            </div>
-        @endif
-    @endforeach
+    <div class="row dashboard-widgets">
+        @foreach ($widgets as $widget)
+            @if (config('ip.widgetEnabled' . $widget))
+                <div class="dashboard-widget col-12 col-md-{{ config('ip.widgetColumnWidth' . $widget, 6) }}">
+                    @include($widget . 'Widget')
+                </div>
+            @endif
+        @endforeach
+    </div>
 
 @stop
