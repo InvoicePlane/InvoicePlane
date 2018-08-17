908cb6f85c3472c99b22e8df91acb2594c0d9301
steve-ks <steve.hegenbart@kingstarter.de>
2018-08-17 16:09:49 +0200

    Update postgres sql upgrade files to actual postgres syntax

--- application/modules/setup/sql/postgres/030_1.5.7.sql
+++ application/modules/setup/sql/postgres/030_1.5.7.sql
@@ -1,3 +1,2 @@
-ALTER TABLE `ip_merchant_responses`
-  ADD `merchant_response_successful` TINYINT(1) DEFAULT 1
-  AFTER `invoice_id`;
+ALTER TABLE ip_merchant_responses
+  ADD COLUMN merchant_response_successful SMALLINT DEFAULT 1;
