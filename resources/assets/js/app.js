07282a2defaa660721cc5c9c925b45f1ab8b2fee
Kovah <mail@kovah.de>
2018-08-20 22:38:24 +0200

    Fix layout and z-index issues

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -10,14 +10,23 @@
  * Based on FusionInvoice by Jesse Terry (FusionInvoice, LLC)
  */
 
-$(document).ready(function(){
+$(document).ready(function () {
 
   // Enable tooltips
   $('[data-toggle="tooltip"]').tooltip();
 
   // Sidebar toggling
-  $(document).on('click', '.sidebar-toggle', function(e){
+  $(document).on('click', '.sidebar-toggle', function (e) {
     e.preventDefault();
     $('body').toggleClass('sidebar-visible');
+  });
+
+  // Fix for dropdowns inside table-responsive elements
+  $(document).on('show.bs.dropdown', '.table-responsive', function () {
+    $('.table-responsive').css('overflow', 'inherit');
+  });
+
+  $(document).on('hide.bs.dropdown', '.table-responsive', function () {
+    $('.table-responsive').css('overflow', 'auto');
   });
 });
