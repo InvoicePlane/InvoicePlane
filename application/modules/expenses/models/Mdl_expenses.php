951868c8587e95b1d7655b5a71279a3ecd831987
chrispytorius <47629214+chrispytorius@users.noreply.github.com>
2019-02-14 21:47:05 +1100

    Add files via upload

---
+++ application/modules/expenses/models/Mdl_expenses.php
@@ -0,0 +1,148 @@
+<?php
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+/*
+ * InvoicePlane
+ * 
+ * A free and open source web based invoicing system
+ *
+ * @package		InvoicePlane
+ * @author		Aeolun (www.serial-experiments.com)
+ * @copyright	Copyright (c) 2012 - 2016 InvoicePlane.com
+ * @license		https://invoiceplane.com/license.txt
+ * @link		https://invoiceplane.com
+ * 
+ */
+class Mdl_Expenses extends Response_Model
+{
+    public $table = 'ip_expenses';
+    public $primary_key = 'ip_expenses.expense_id';
+    public $validation_rules = 'validation_rules';
+    public function default_select()
+    {
+        $this->db->select("
+            SQL_CALC_FOUND_ROWS ip_expense_custom.*,
+            ip_payment_methods.*,
+            ip_clients.client_name,
+            ip_clients.client_id,
+            ip_tax_rates.tax_rate_name,
+            ip_tax_rates.tax_rate_percent,
+            ip_expenses.*", FALSE);
+    }
+    public function default_order_by()
+    {
+        $this->db->order_by('ip_expenses.expense_date DESC');
+    }
+    public function default_join()
+    {
+        $this->db->join('ip_payment_methods', 'ip_payment_methods.payment_method_id = ip_expenses.payment_method_id', 'left');
+        $this->db->join('ip_expense_custom', 'ip_expense_custom.expense_id = ip_expenses.expense_id', 'left');
+        $this->db->join('ip_clients', 'ip_clients.client_id = ip_expenses.client_id', 'left');
+        $this->db->join('ip_tax_rates', 'ip_tax_rates.tax_rate_id = ip_expenses.tax_rate_id', 'left');
+    }
+    public function validation_rules()
+    {
+        return array(
+            'expense_date' => array(
+                'field' => 'expense_date',
+                'label' => lang('date'),
+                'rules' => 'required'
+            ),
+            'expense_amount' => array(
+                'field' => 'expense_amount',
+                'label' => lang('payment'),
+                'rules' => 'required'
+            ),
+            'tax_rate_id' => array(
+                'field' => 'tax_rate_id',
+                'label' => lang('tax_rate')
+            ),
+            'payment_method_id' => array(
+                'field' => 'payment_method_id',
+                'label' => lang('payment_method')
+            ),
+            'expense_note' => array(
+                'field' => 'expense_note',
+                'label' => lang('note')
+            ),
+            'client_id' => array(
+                'field' => 'client_id',
+                'label' => lang('client')
+            )
+        );
+    }
+    public function save($id = NULL, $db_array = NULL)
+    {
+        $db_array = ($db_array) ? $db_array : $this->db_array($id);
+        // Save the payment
+        $id = parent::save($id, $db_array);
+        return $id;
+    }
+    public function delete($id = NULL)
+    {
+        // Delete the payment
+		// Get the invoice id before deleting payment
+     
+
+        // Delete the payment
+        parent::delete($id);
+    }
+	
+	public function get_extension($file) 
+	{
+	 $extension = end(explode(".", $file));
+	 return $extension ? $extension : false;
+	}
+	
+    public function db_array($id = NULL)
+    {
+        $db_array = parent::db_array();
+        $db_array['expense_date'] = date_to_mysql($db_array['expense_date']);
+        $db_array['expense_amount'] = standardize_amount($db_array['expense_amount']);
+		
+		
+		$targetDir = "uploads/";
+		$fileName = basename($_FILES["expense_file"]["name"]);
+		$targetFilePath = $targetDir . $fileName;
+		$fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
+		
+		
+		if(!empty($_FILES["expense_file"]["name"]))
+		{
+			// Allow certain file formats
+			$allowTypes = array('jpg','png','jpeg','gif','pdf');
+			
+			$db_array['expense_file'] = base64_encode(file_get_contents($_FILES["expense_file"]["tmp_name"]));
+			$db_array['expense_file_name'] = $fileName;
+			
+		}
+		else
+		{
+		    if($id != NULL)
+		    {
+			    unset ($db_array['expense_file']);
+			    unset($db_array['expense_file_name']);
+		    }
+		    else
+			{
+			    $db_array['expense_file'] = "";
+			    $db_array['expense_file_name'] = "";
+			}
+		}
+		
+
+		
+		
+        return $db_array;
+    }
+    public function prep_form($id = NULL)
+    {
+        if (!parent::prep_form($id)) {
+            return FALSE;
+        }
+        if (!$id) {
+            parent::set_form_value('expense_date', date('Y-m-d'));
+        }
+        return TRUE;
+    }
+}
---
+++ application/modules/expenses/views/delete.php
@@ -0,0 +1,152 @@
+<?php 
+$attributes = array('class' => 'form-horizontal');
+echo form_open('',$attributes);
+//<form method="post" class="form-horizontal" >
+?>
+
+
+    <?php if ($expense_id) { ?>
+        <input type="hidden" name="expense_id" value="<?php echo $expense_id; ?>">
+        <div id="headerbar">
+    	
+        <h1>Edit Expense</h1>
+         <?php
+         $this->layout->load_view('layout/header_buttons'); 
+        ?>
+       
+        
+    	</div>
+    <?php }
+		else
+		{
+			?>
+            <div id="headerbar">
+    	
+            <h1>Add Expense</h1>
+            <?php
+			 $this->layout->load_view('layout/header_buttons'); 
+			?>
+            
+            </div>
+            <?php
+		}
+	 ?>
+
+    
+
+    <div id="content">
+
+        <?php $this->layout->load_view('layout/alerts'); ?>
+
+        <div class="form-group">
+            <div class="col-xs-12 col-sm-2 text-right text-left-xs">
+                <label for="client_id" class="control-label"><?php echo lang('client'); ?></label>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <select name="client_id" id="client_id" class="form-control">
+                        <option value=""></option>
+                        <?php foreach ($clients as $client) { ?>
+                            <option value="<?php echo $client->client_id; ?>"
+                                    <?php if ($this->mdl_expenses->form_value('client_id') == $client->client_id) { ?>selected="selected"<?php } ?>><?php echo $client->client_name; ?></option>
+                        <?php } ?>
+                </select>
+            </div>
+        </div>
+
+        <div class="form-group has-feedback">
+            <div class="col-xs-12 col-sm-2 text-right text-left-xs">
+                <label for="expense_date" class="control-label"><?php echo lang('date'); ?></label>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <div class="input-group">
+                    <input name="expense_date" id="expense_date"
+                           class="form-control datepicker"
+                           value="<?php echo date_from_mysql($this->mdl_expenses->form_value('expense_date')); ?>">
+                  <span class="input-group-addon">
+                      <i class="fa fa-calendar fa-fw"></i>
+                  </span>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="col-xs-12 col-sm-2 text-right text-left-xs">
+                <label for="expense_amount" class="control-label"><?php echo lang('amount'); ?></label>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <input type="text" name="expense_amount" id="expense_amount" class="form-control"
+                       value="<?php echo format_amount($this->mdl_expenses->form_value('expense_amount')); ?>">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="col-xs-12 col-sm-2 text-right text-left-xs">
+                <label for="tax_rate_id" class="control-label"><?php echo lang('tax_rate'); ?></label>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <select name="tax_rate_id" id="tax_rate_id" class="form-control">
+                    <option value=""></option>
+                    <?php foreach ($tax_rates as $rate) { ?>
+                        <option value="<?php echo $rate->tax_rate_id; ?>"
+                                <?php if ($this->mdl_expenses->form_value('tax_rate_id') == $rate->tax_rate_id) { ?>selected="selected"<?php } ?>><?php echo $rate->tax_rate_name; ?></option>
+                    <?php } ?>
+                </select>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="col-xs-12 col-sm-2 text-right text-left-xs">
+                <label for="payment_method_id" class="control-label">
+                    <?php echo lang('payment_method'); ?>
+                </label>
+            </div>
+            <div class="col-xs-12 col-sm-6 payment-method-wrapper">
+
+                <?php
+                // Add a hidden input field if a payment method was set to pass the disabled attribute
+                if ($this->mdl_expenses->form_value('payment_method_id')) { ?>
+                    <input type="hidden" name="payment_method_id" class="hidden"
+                           value="<?php echo $this->mdl_expenses->form_value('payment_method_id'); ?>">
+                <?php } ?>
+
+                <select id="payment_method_id" name="payment_method_id" class="form-control"
+                    <?php echo($this->mdl_expenses->form_value('payment_method_id') ? 'disabled="disabled"' : ''); ?>>
+
+                    <?php foreach ($payment_methods as $payment_method) { ?>
+                        <option value="<?php echo $payment_method->payment_method_id; ?>"
+                                <?php if ($this->mdl_expenses->form_value('payment_method_id') == $payment_method->payment_method_id) { ?>selected="selected"<?php } ?>>
+                            <?php echo $payment_method->payment_method_name; ?>
+                        </option>
+                    <?php } ?>
+                </select>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="col-xs-12 col-sm-2 text-right text-left-xs">
+                <label for="expense_note" class="control-label"><?php echo lang('note'); ?></label>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <textarea name="expense_note"
+                          class="form-control"><?php echo $this->mdl_expenses->form_value('expense_note'); ?></textarea>
+            </div>
+
+        </div>
+
+        <?php foreach ($custom_fields as $custom_field) { ?>
+            <div class="form-group">
+                <div class="col-xs-12 col-sm-2 text-right text-left-xs">
+                    <label><?php echo $custom_field->custom_field_label; ?>: </label>
+                </div>
+                <div class="col-xs-12 col-sm-6">
+                    <input type="text" name="custom[<?php echo $custom_field->custom_field_column; ?>]"
+                           id="<?php echo $custom_field->custom_field_column; ?>"
+                           class="form-control"
+                           value="<?php echo form_prep($this->mdl_expenses->form_value('custom[' . $custom_field->custom_field_column . ']')); ?>">
+                </div>
+            </div>
+        <?php } ?>
+		 
+    </div>
+
+<?php echo form_close();?>
