404024082c7334f94ab525ca32e7943824b0ac71
Andy Binder <AndyBinder@gmx.de>
2019-07-03 12:30:31 +0200

    Setup create and alter SQL Tables for Car Module

--- application/modules/setup/sql/032_1.5.9.sql
+++ application/modules/setup/sql/032_1.5.9.sql
@@ -1,1 +1,45 @@
 # Added for versioning
+
+# Car module
+CREATE TABLE `ip_car_custom` (
+	`car_custom_id` INT(11) NOT NULL AUTO_INCREMENT,
+	`car_id` INT(11) NOT NULL,
+	PRIMARY KEY (`car_custom_id`),
+	INDEX `car_id` (`car_id`)
+)
+AUTO_INCREMENT=1;
+
+CREATE TABLE `ip_cars` (
+	`car_id` INT(11) NOT NULL AUTO_INCREMENT,
+	`car_client_id` INT(11),
+	`car_vehicle` TEXT,
+	`car_builddate` DATE,
+	`car_licenseplate` TEXT,
+	`car_kmstand` INT,
+	`car_auhu` TEXT,
+	`car_date_created` DATE,
+	`car_date_modified` DATE,
+	`car_chassnr` TEXT,
+	`car_note` TEXT,
+	`car_active` INT(1) DEFAULT '1',
+	PRIMARY KEY (`car_id`),
+	INDEX `car_id` (`car_id`)
+);
+
+ALTER TABLE `ip_invoices` ADD COLUMN (
+        `car_id` INT(11) NOT NULL,
+        `car_vehicle` TEXT,
+        `car_licenseplate` TEXT,
+        `car_kmstand` INT,
+        `car_auhu` TEXT,
+        `car_chassnr` TEXT
+);
+
+ALTER TABLE `ip_quotes` ADD COLUMN (
+        `car_id` INT(11) NOT NULL,
+        `car_vehicle` TEXT,
+        `car_licenseplate` TEXT,
+        `car_kmstand` INT,
+        `car_auhu` TEXT,
+        `car_chassnr` TEXT
+);
