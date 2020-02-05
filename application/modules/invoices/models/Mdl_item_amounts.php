770615f65bcaa2fd7c7f51148070a39cbc067006
luca <pellizzari954@gmail.com>
2020-02-05 12:35:39 +0100

    working before-tax and after-tax calculation based on switch, todo check wrong invoice total

--- application/modules/invoices/models/Mdl_item_amounts.php
+++ application/modules/invoices/models/Mdl_item_amounts.php
@@ -29,15 +29,43 @@ class Mdl_Item_Amounts extends CI_Model
         $this->load->model('invoices/mdl_items');
         $item = $this->mdl_items->get_by_id($item_id);
 
-        $item_subtotal = $item->item_quantity * $item->item_price;
-        // new tax total based on discounted amount
-        $item_discount_total = $item->item_discount_amount * $item->item_quantity;	        //$item_tax_total = $item_subtotal * ($item->item_tax_rate_percent / 100);
-        $item_total = $item_subtotal + $item_tax_total - $item_discount_total;	        $item_discount_total = ($item->item_discount_amount * $item->item_quantity) + ((($item->item_price / 100) * $item->item_discount_percent) * $item->item_quantity);
-        // calculating before tax: ((subtotal - discount) + ((subtotal - discount) / 100 * tax_percent))
-        // todo: give in the future option to select discount on net price / gross price
-        $item_discounted = $item_subtotal - $item_discount_total;
-        $item_tax_total = $item_discounted * ($item->item_tax_rate_percent / 100); 
-        $item_total = $item_discounted + $item_tax_total;
+        $item_total = 0;
+        $item_subtotal = 0;
+        $item_tax_total = 0;
+        $item_discount_total = 0;
+
+        // If the setting is set to 0 or 'apply_before_item_tax', 
+        // we calculate the item tax on the net amount.
+        // total: ((subtotal - discount) + ((subtotal - discount) / 100 * tax_percent))
+        if(get_setting('default_invoice_tax_rate_placement') === "0") {
+
+            $item_subtotal = $item->item_quantity * $item->item_price;
+            $item_discount_total = ($item->item_discount_amount * $item->item_quantity) + ((($item->item_price / 100) * $item->item_discount_percent) * $item->item_quantity);
+            $item_discounted = $item_subtotal - $item_discount_total;
+            $item_tax_total = $item_discounted * ($item->item_tax_rate_percent / 100); 
+            $item_total = $item_discounted + $item_tax_total;
+        
+        // If the setting is set to 1 or 'apply_after_item_tax',
+        // we calculate an offset 'x' to discount, to equal final price - 'real discount'
+        // total: (subtotal + tax_amount) - discount = (price - x) + ((price - x) / 100 * tax_perc)
+        } else {
+
+            $temp_subtotal = $item->item_quantity * $item->item_price;
+            $temp_taxed = $temp_subtotal * ($item->item_tax_rate_percent / 100); 
+            $temp_total = $temp_subtotal + $temp_taxed;
+            $temp_discount = ($item->item_discount_amount * $item->item_quantity) + ((($temp_total / 100) * $item->item_discount_percent) * $item->item_quantity);
+
+            // let k be (price - x), where x is the offset amount
+            $k_value = (100 / (100 + $item->item_tax_rate_percent)) * ($temp_subtotal + $temp_taxed - $temp_discount);
+
+            // reverse the formula and get the offset value
+            $x_value = $temp_subtotal - $k_value;
+
+            $item_subtotal = ($temp_subtotal - $x_value);
+            $item_tax_total = (($temp_subtotal - $x_value) / 100 * $item->item_tax_rate_percent);
+            $item_total = $item_subtotal + $item_tax_total;
+            $item_discount_total = $temp_discount;
+        }
 
         $db_array = array(
             'item_id' => $item_id,
