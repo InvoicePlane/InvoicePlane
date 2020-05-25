4a1a192ac1327929afb1973d4ad0e344ca266408
dpoper <dpoper@users.noreply.github.com>
2020-05-25 14:11:23 +0200

    Eigene Vorlagen für Rechnung und Angebote

---
+++ application/language/German/custom_lang.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * CUSTOM LANGUAGE STRINGS
+ *
+ * Add your custom Language Strings here with the following scheme:
+ *
+ * 'language_string_key' => 'Actual content of the Language String',
+ *
+ * Please notice that 'language_string_key' should only contain alphanumeric
+ * characters and underscores.
+ * If you are not sure take a look at the ip_lang.php file or visit the
+ * community forums.
+ */
+
+$lang = array(
+    'attachments_invoice'                          => 'Rechnungsanlagen',
+	'ban'                                          => 'Buchungskonto',
+    'cus_id'                                       => 'Kundennummer',
+    'download_invoice_pdf'                         => 'Rechnung als PDF herunterladen',
+    'vat_id'                                       => 'UST-ID',
+    'vat_id_short'                                 => 'UST-ID',
+	'vat_id_user'                                  => 'Meine UST-ID',
+	'vat_id_client'                                => 'Ihre UST-ID'
+);
