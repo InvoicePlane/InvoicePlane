d2882148be10d0241eaabcf022084aaeef0f162a
naui95 <naui95@users.noreply.github.com>
2022-06-11 21:16:17 +0200

    fixed code formatting

--- application/modules/guest/views/gateway/stripe.php
+++ application/modules/guest/views/gateway/stripe.php
@@ -1,13 +1,7 @@
 <form id="payment-form">
-  <div id="card-element">
-    <!-- Elements will create input elements here -->
-  </div>
-
-  <!-- We'll put the error messages in this element -->
+  <div id="card-element"></div>
   <div id="card-errors" role="alert"></div>
-
   <br></br>
-
   <a id="getToken" class="btn btn-success btn-lg">Pay now</a>
 </form>
 
