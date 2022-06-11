3dc56dbd36fb874c5be38cae4663cde9a884384f
naui95 <naui95@users.noreply.github.com>
2022-06-11 21:22:38 +0200

    fixed indenting and other minor bugs

--- application/modules/guest/views/gateway/stripe.php
+++ application/modules/guest/views/gateway/stripe.php
@@ -1,8 +1,8 @@
 <form id="payment-form">
-  <div id="card-element"></div>
-  <div id="card-errors" role="alert"></div>
-  <br></br>
-  <a id="getToken" class="btn btn-success btn-lg">Pay now</a>
+    <div id="card-element"></div>
+    <div id="card-errors" role="alert"></div>
+    <br></br>
+    <a id="getToken" class="btn btn-success btn-lg">Pay now</a>
 </form>
 
 <script src="/application/modules/guest/views/gateway/stripe.js"></script>
