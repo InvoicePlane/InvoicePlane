7418d00abe4eaa0a1fb65cdc002075547ad5e0a6
naui95 <naui95@hotmail.com>
2022-12-03 07:05:42 +0100

    fixes issue #886 the length of the field by varchar(100) is enough (since login table uses the same length) and provides additional security

--- application/modules/setup/sql/035_1.5.12.sql
+++ application/modules/setup/sql/035_1.5.12.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `ip_login_log` (
-    `login_name` varchar(255),
+    `login_name` varchar(100),
     `log_count` int DEFAULT 0,
     `log_create_timestamp` datetime,
     PRIMARY KEY (login_name)
