6770909e17f8da3e35ac479a6aa882aa5e7b15ae
vespino <6991325+vespino@users.noreply.github.com>
2022-12-08 11:37:02 +0100

    Update invoice_helper.php
    In version 1.6.0 the logo isn't shown on the PDF invoice. When changing line 39 the logo returns.

--- application/helpers/invoice_helper.php
+++ application/helpers/invoice_helper.php
@@ -36,7 +36,7 @@ function invoice_logo_pdf()
     $CI = &get_instance();
 
     if ($CI->mdl_settings->setting('invoice_logo')) {
-        return '<img src="file://' . getcwd() . '/uploads/' . $CI->mdl_settings->setting('invoice_logo') . '" id="invoice-logo">';
+        return '<img src="' . base_url() . 'uploads/' . $CI->mdl_settings->setting('invoice_logo') . '" id="invoice-logo">';
     }
 
     return '';
