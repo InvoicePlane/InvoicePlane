285105b38734d2bfd38bf2c6617386ef3549096c
VeRony <30659226+Verony-makesIT@users.noreply.github.com>
2023-02-21 21:17:29 +0100

    Fix #841 Copy quote (modal) UI feedback
    Bug:  When the data is retrieved from the custom_fields db and put into an array and if there is no data in the db, the array cannot be populated so the result is that there is no "response" at all.
    Fix: extracted the working snippet from mdl_invoice and modified it in mdl_quotes. Even when there is no data/array there is a response.

--- application/modules/quotes/models/Mdl_quotes.php
+++ application/modules/quotes/models/Mdl_quotes.php
@@ -228,13 +228,13 @@ class Mdl_Quotes extends Response_Model
 
         // Copy the custom fields
         $this->load->model('custom_fields/mdl_quote_custom');
-        $db_array = $this->mdl_quote_custom->where('quote_id', $source_id)->get()->row_array() ?? [];
+        $custom_fields = $this->mdl_quote_custom->where('quote_id', $source_id)->get()->result() ?? [];
 
-        if (count($db_array) > 2) {
-            unset($db_array['quote_custom_id']);
-            $db_array['quote_id'] = $target_id;
-            $this->mdl_quote_custom->save_custom($target_id, $db_array);
+        $form_data = array();
+        foreach ($custom_fields as $field) {
+            $form_data[$field->quote_custom_fieldid] = $field->quote_custom_fieldvalue;
         }
+        $this->mdl_quote_custom->save_custom($target_id, $form_data);
     }
 
     /**
