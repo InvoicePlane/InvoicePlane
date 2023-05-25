fd23201cffb4a4c4f6ac3a5f80f155c4355c00a9
Soft Bucket IO <softbucket.io@gmail.com>
2023-05-25 12:09:32 +0300

    add client currency code migration

--- application/modules/setup/sql/036_1.6.sql
+++ application/modules/setup/sql/036_1.6.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `ip_invoices_recurring` CHANGE `recur_end_date` `recur_end_date` DATE NULL;
 ALTER TABLE `ip_invoices_recurring` CHANGE `recur_next_date` `recur_next_date` DATE NULL;
+ALTER TABLE `ip_clients` ADD COLUMN `client_currency_code` VARCHAR(3) NULL;
