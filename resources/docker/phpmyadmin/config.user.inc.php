8ab92c5a4421ee6a4663f4ff2a07d5ab24ec5632
Niels Drost <nielsdrost7@gmail.com>
2023-07-08 16:53:24 +0200

    Change host in phpMyAdmin config file #956

--- resources/docker/phpmyadmin/config.user.inc.php
+++ resources/docker/phpmyadmin/config.user.inc.php
@@ -27,7 +27,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['host'] = 'db';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
