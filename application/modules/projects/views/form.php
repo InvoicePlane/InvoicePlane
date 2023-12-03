7f6cd2b54a61a273e563e521ba30f23e4402e9b2
Kevin Joudrier <kev.joudrier@gmail.com>
2023-12-03 23:58:30 +0100

    Revert "normalization"
    This reverts commit 197b14819003c37a8d04ccaab06214c6b8cbe604.

--- application/modules/projects/views/form.php
+++ application/modules/projects/views/form.php
@@ -1,6 +1,6 @@
 <?php
-    $default_client_name = $this->mdl_projects->form_value('client_name', true);
-    $default_client_surname = $this->mdl_projects->form_value('client_surname', true);
+    $defaultClientName = $this->mdl_projects->form_value('client_name', true);
+    $defaultClientSurname = $this->mdl_projects->form_value('client_surname', true);
 ?>
 
 <script>
@@ -29,9 +29,9 @@
         <div class="form-group">
             <label for="client_id"><?php _trans('client'); ?></label>
             <select name="client_id" id="client_id" class="client-id-select form-control" autofocus="autofocus">
-                <?php if(null !== $default_client_name): ?>
-                    <option value="<?php echo $default_client_name; ?>">
-                        <?php echo $default_client_name . ' ' . $default_client_surname; ?>
+                <?php if(null !== $defaultClientName): ?>
+                    <option value="<?php echo $defaultClientName; ?>">
+                        <?php echo $defaultClientName . ' ' . $defaultClientSurname; ?>
                     </option>
                 <?php endif; ?>
             </select>
