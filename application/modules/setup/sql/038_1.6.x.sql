bbf6d10a57b60245dfc09d56b59d77ae96905568
Ordissimo <thierry@ordissimo.com>
2024-01-28 15:13:43 +0100

    Missing sql addition

--- application/modules/setup/sql/038_1.6.x.sql
+++ application/modules/setup/sql/038_1.6.x.sql
@@ -3,3 +3,11 @@ CREATE TABLE `ip_services` (
   service_name varchar(255) NOT NULL,
   PRIMARY KEY (service_id, service_name)
 )
+
+ALTER TABLE `ip_invoices`
+  ADD `service_id` int(11) DEFAULT 0
+  AFTER `creditinvoice_parent_id`;
+
+ALTER TABLE `ip_quotes`
+  ADD `service_id` int(11) DEFAULT 0
+  AFTER `notes`;
