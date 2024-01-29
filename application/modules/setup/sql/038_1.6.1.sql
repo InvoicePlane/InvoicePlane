969a81f395248a6917964a29cf37cb9fe03322d4
novafirst <privremeno@yahoo.com>
2024-01-29 18:44:42 -0500

    Update 038_1.6.1.sql
    Added client_number to ip_clients table
    
    Added to ip_invoices table:
    invoice_quote_number,
    invoice_work_order,
    invoice_agreement

--- application/modules/setup/sql/038_1.6.1.sql
+++ application/modules/setup/sql/038_1.6.1.sql
@@ -1,1 +1,4 @@
 ALTER TABLE `ip_clients` ADD client_number VARCHAR(30) NULL DEFAULT NULL AFTER `client_id`;
+ALTER TABLE `ip_invoices` ADD invoice_quote_number INT AFTER `invoice_id`;
+ALTER TABLE `ip_invoices` ADD invoice_work_order VARCHAR(30) NULL DEFAULT NULL AFTER `invoice_quote_number`;
+ALTER TABLE `ip_invoices` ADD invoice_agreement VARCHAR(30) NULL DEFAULT NULL AFTER `invoice_work_order`;
