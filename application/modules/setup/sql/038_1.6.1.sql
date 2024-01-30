4ba79308f3264bca66248db3905d85ccc779b53c
novafirst <privremeno@yahoo.com>
2024-01-29 21:45:44 -0500

    Update 038_1.6.1.sql
    Alter ip_quotes table to add quote_work_order and quote_agreement

--- application/modules/setup/sql/038_1.6.1.sql
+++ application/modules/setup/sql/038_1.6.1.sql
@@ -2,3 +2,5 @@ ALTER TABLE `ip_clients` ADD client_number VARCHAR(30) NULL DEFAULT NULL AFTER `
 ALTER TABLE `ip_invoices` ADD invoice_quote_number INT AFTER `invoice_id`;
 ALTER TABLE `ip_invoices` ADD invoice_work_order VARCHAR(30) NULL DEFAULT NULL AFTER `invoice_quote_number`;
 ALTER TABLE `ip_invoices` ADD invoice_agreement VARCHAR(30) NULL DEFAULT NULL AFTER `invoice_work_order`;
+ALTER TABLE `ip_quotes` ADD quote_work_order VARCHAR(30) NULL DEFAULT NULL AFTER `quote_id`;
+ALTER TABLE `ip_quotes` ADD quote_agreement VARCHAR(30) NULL DEFAULT NULL AFTER `quote_work_order`;
