4c6f3e3f101587c5d526e7357cf8172c71419762
Ordissimo <thierry@ordissimo.com>
2024-02-01 22:02:28 +0100

    Fix syntax

--- application/modules/setup/sql/038_1.6.x.sql
+++ application/modules/setup/sql/038_1.6.x.sql
@@ -2,7 +2,7 @@ CREATE TABLE `ip_services` (
   service_id int(11) NOT NULL,
   service_name varchar(255) NOT NULL,
   PRIMARY KEY (service_id, service_name)
-)
+);
 
 ALTER TABLE `ip_invoices`
   ADD `service_id` int(11) DEFAULT 0
