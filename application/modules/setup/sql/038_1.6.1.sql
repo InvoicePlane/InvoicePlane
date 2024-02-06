121b232bdb56ccd199e795882ddc67c0a7a69baf
Kosta <privremeno@yahoo.com>
2024-02-06 18:04:23 -0500

    Update 038_1.6.1.sql

--- application/modules/setup/sql/038_1.6.1.sql
+++ application/modules/setup/sql/038_1.6.1.sql
@@ -1,6 +1,6 @@
-ALTER TABLE `ip_clients` ADD client_number VARCHAR(30) NULL DEFAULT NULL AFTER `client_id`;
-ALTER TABLE `ip_invoices` ADD invoice_quote_number INT AFTER `invoice_id`;
-ALTER TABLE `ip_invoices` ADD invoice_work_order VARCHAR(30) NULL DEFAULT NULL AFTER `invoice_quote_number`;
-ALTER TABLE `ip_invoices` ADD invoice_agreement VARCHAR(30) NULL DEFAULT NULL AFTER `invoice_work_order`;
-ALTER TABLE `ip_quotes` ADD quote_work_order VARCHAR(30) NULL DEFAULT NULL AFTER `quote_id`;
-ALTER TABLE `ip_quotes` ADD quote_agreement VARCHAR(30) NULL DEFAULT NULL AFTER `quote_work_order`;
+ALTER TABLE `ip_clients` ADD client_number VARCHAR(100) NULL DEFAULT NULL AFTER `client_id`;
+ALTER TABLE `ip_invoices` ADD invoice_quote_number VARCHAR(100) NULL DEFAULT NULL AFTER `invoice_id`;
+ALTER TABLE `ip_invoices` ADD invoice_work_order VARCHAR(100) NULL DEFAULT NULL AFTER `invoice_quote_number`;
+ALTER TABLE `ip_invoices` ADD invoice_agreement VARCHAR(100) NULL DEFAULT NULL AFTER `invoice_work_order`;
+ALTER TABLE `ip_quotes` ADD quote_work_order VARCHAR(100) NULL DEFAULT NULL AFTER `quote_id`;
+ALTER TABLE `ip_quotes` ADD quote_agreement VARCHAR(100) NULL DEFAULT NULL AFTER `quote_work_order`;
