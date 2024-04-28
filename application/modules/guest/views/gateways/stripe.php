7cc8de194384eeec3d0f1fe513dd7f875977d71e
naui95 <nahuel.guidotti@outlook.com>
2024-04-28 13:07:30 +0200

    adapted stripe page to use embedded checkout

--- application/modules/guest/views/gateways/stripe.php
+++ application/modules/guest/views/gateways/stripe.php
@@ -1,69 +1,30 @@
 
-<form id="payment-form">
-    <div id="card-element"></div>
-    <div id="card-errors" role="alert"></div>
-    <br></br>
-    <a id="getToken" class="btn btn-success btn-lg">Pay now</a>
-</form>
+<div id="stripe-checkout"></div>
 
 <script>
     var stripe;
     $.getScript("https://js.stripe.com/v3/").done(()=>{
        stripe = Stripe('<?php echo($stripe_api_key); ?>');
-       loadStripe();
+
+       loadStripe().then(()=>{
+        $("#fullpage-loader").fadeOut(200);
+       });
     });
 
-    function loadStripe() {
-        var elements = stripe.elements();
+    async function loadStripe() {
+        const fetchClientSecret = async () => {
+            const response = await fetch('<?php echo site_url('guest/gateways/stripe/create_checkout_session/'.$invoice_url_key); ?>',{
+                method: 'GET'
+            })
+            .then((response) => response.json())
 
-        var style = {
-            base: {
-            color: "#32325d",
-            fontFamily: 'Arial, sans-serif',
-            fontSmoothing: "antialiased",
-            fontSize: "16px",
-            textDecoration:"underline",
-            "::placeholder": {
-                color: "#32325d"
-            },
-            },
-            invalid: {
-            fontFamily: 'Arial, sans-serif',
-            color: "#fa755a",
-            iconColor: "#fa755a"
-            }
-        };
-
-    var card = elements.create("card",{style: style});
-
-    card.mount("#card-element");
-
-    card.on('ready',()=>{$("#fullpage-loader").fadeOut(200)});
-
-    card.on('change', function(event) {
-
-        var displayError = document.getElementById('card-errors');
-
-        if (event.error) {
-            displayError.textContent = event.error.message;
-        } else {
-            displayError.textContent = '';
+            return await response.clientSecret;
         }
-    });
 
-    $('#getToken').click(()=>{
-        $("#fullpage-loader").fadeIn(200)
-        stripe.createToken(card).then((result)=>{
-            if(result.error)
-            {
-                $("#fullpage-loader").fadeOut(200)
-            }
-            if(result.token)
-            {
-                $('input[name="creditcard_number"]').val(result.token.id)
-                $("#payment-information-form").submit();
-            }
-        })
-    });
-}
+        const checkout = await stripe.initEmbeddedCheckout({
+            fetchClientSecret
+        });
+
+        checkout.mount('#stripe-checkout');
+    }
 </script>
