3f1815d4209e00498bbd48af7e946362f6179a00
Niels Drost <nielsdrost7@gmail.com>
2024-06-16 11:39:50 +0200

    #1046: Fixed some formatting

--- application/modules/guest/controllers/gateways/Paypal.php
+++ application/modules/guest/controllers/gateways/Paypal.php
@@ -3,23 +3,24 @@ if (!defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
 
-class Paypal extends Base_Controller {
-
+class Paypal extends Base_Controller
+{
     public function __construct()
     {
         parent::__construct();
         $this->_create_client();
     }
 
-    protected function _create_client() : void {
+    protected function _create_client(): void
+    {
         $this->load->library('crypt');
 
         //load the REST API consumer library
-        $this->load->library('gateways/PaypalLib',[
+        $this->load->library('gateways/PaypalLib', [
             'client_id' => get_setting('gateway_paypal_clientId'),
             'client_secret' => $this->crypt->decode(get_setting('gateway_paypal_clientSecret')),
             'demo' => get_setting('gateway_paypal_testMode') == 1 ? true : false
-        ],'lib_paypal');
+        ], 'lib_paypal');
     }
 
     /**
@@ -29,19 +30,20 @@ class Paypal extends Base_Controller {
      * @param string $invoice_url_key
      * @return json the PayPal object to be loaded in the JS SDK script
      */
-    public function paypal_create_order($invoice_url_key) {
+    public function paypal_create_order($invoice_url_key)
+    {
         // Check if the invoice exists and is billable
         $this->load->model('invoices/mdl_invoices');
 
         $invoice = $this->mdl_invoices->where('ip_invoices.invoice_url_key', $invoice_url_key)
-        ->get()->row();
+            ->get()->row();
 
         // Check if the invoice is payable
         if ($invoice->invoice_balance <= 0) {
             $this->session->set_userdata('alert_error', lang('invoice_already_paid'));
-            redirect(site_url('guest/view/invoice/'.$invoice->invoice_url_key));
+            redirect(site_url('guest/view/invoice/' . $invoice->invoice_url_key));
         }
-        
+
         //create the order
         $paypal_client = $this->lib_paypal->createOrder([
             'invoice_id' => $invoice->invoice_id,
@@ -50,7 +52,7 @@ class Paypal extends Base_Controller {
             'custom_id' => $invoice_url_key
         ]);
 
-       return $this->output->set_output($paypal_client); //TODO: make proper response
+        return $this->output->set_output($paypal_client); //TODO: make proper response
     }
 
     /**
@@ -60,12 +62,12 @@ class Paypal extends Base_Controller {
      * @param string $order_id
      * @return void
      */
-    public function paypal_capture_payment($order_id) {
-        
+    public function paypal_capture_payment($order_id)
+    {
         $paypal_response = $this->lib_paypal->captureOrder($order_id);
 
         //handle the payment
-        if($paypal_response['status']) {
+        if ($paypal_response['status']) {
             $paypal_object = json_decode($paypal_response['response']->getBody());
 
             $invoice_id = $paypal_object->purchase_units[0]->payments->captures[0]->invoice_id;
@@ -73,8 +75,8 @@ class Paypal extends Base_Controller {
 
             //record the payment
             $this->load->model('payments/mdl_payments');
-    
-            $this->mdl_payments->save(null,[
+
+            $this->mdl_payments->save(null, [
                 'invoice_id' => $invoice_id,
                 'payment_date' => date('Y-m-d'),
                 'payment_amount' => $amount,
@@ -83,9 +85,9 @@ class Paypal extends Base_Controller {
             ]);
 
             $invoice = $this->mdl_invoices->where('ip_invoices.invoice_id', $invoice_id)
-            ->get()->row();
+                ->get()->row();
 
-            $this->session->set_flashdata('alert_success', sprintf(trans('online_payment_payment_successful'),$invoice->invoice_number));
+            $this->session->set_flashdata('alert_success', sprintf(trans('online_payment_payment_successful'), $invoice->invoice_number));
             $this->session->keep_flashdata('alert_success');
 
             $this->db->insert('ip_merchant_responses', [
@@ -94,11 +96,9 @@ class Paypal extends Base_Controller {
                 'merchant_response_date' => date('Y-m-d'),
                 'merchant_response_driver' => 'paypal',
                 'merchant_response' => $paypal_object->status,
-                'merchant_response_reference' =>'Resource ID:' . $paypal_object->id,
+                'merchant_response_reference' => 'Resource ID:' . $paypal_object->id,
             ]);
-        }
-        else
-        {
+        } else {
             $response_error = json_decode($paypal_response['error']->getResponse()->getBody());
 
             //get the order details to have the invoice id from paypal
@@ -111,12 +111,12 @@ class Paypal extends Base_Controller {
                 'merchant_response_date' => date('Y-m-d'),
                 'merchant_response_driver' => 'paypal',
                 'merchant_response' => "name: " . $response_error->name . "; details: " . $response_error->details[0]->description,
-                'merchant_response_reference' =>'Resource ID:' . $order_id,
+                'merchant_response_reference' => 'Resource ID:' . $order_id,
             ]);
 
             //set error message to be flashed
             $this->session->set_flashdata('alert_error',
-            trans('online_payment_payment_failed') . '<br/>' . $response_error->details[0]->description);
+                trans('online_payment_payment_failed') . '<br/>' . $response_error->details[0]->description);
             $this->session->keep_flashdata('alert_error');
         }
     }
--- application/modules/guest/views/gateways/paypal.php
+++ application/modules/guest/views/gateways/paypal.php
@@ -1,37 +1,36 @@
-
 <div class="container">
     <div id="paypal-buttons" class="col-xs-12 col-md-8 col-md-offset-2"></div>
 </div>
 <script>
-$.ajax({
-    url:"https://www.paypal.com/sdk/js?client-id=<?php echo $paypal_client_id; ?>&currency=<?php echo $currency; ?>",
-    dataType:"script",
-    cache:true,
-    success: () => {
-        initPayPal();
-    }
-});
-
-function initPayPal() {
-    paypal.Buttons({
-        createOrder() {
-            return fetch('<?php echo site_url('guest/gateways/paypal/paypal_create_order/'.$invoice_url_key); ?>',
-            {
-                method: "GET"
-            })
-            .then((response) => response.json())
-            .then((order) => order.id)
-        },
-        onApprove: function (data) {
-            return fetch('<?php echo site_url('guest/gateways/paypal/paypal_capture_payment/');?>'+data.orderID,{
-                method: 'GET'
-            })
-            .then((response) => window.location.replace('<?php echo site_url('guest/view/invoice/'.$invoice_url_key); ?>'));
-        },
-        onError: function (error) {
-            console.log(error)
-            window.location.replace('<?php echo site_url('guest/payment_information/form/'.$invoice_url_key .'/paypal'); ?>')
+    $.ajax({
+        url: "https://www.paypal.com/sdk/js?client-id=<?php echo $paypal_client_id; ?>&currency=<?php echo $currency; ?>",
+        dataType: "script",
+        cache: true,
+        success: () => {
+            initPayPal();
         }
-    }).render('#paypal-buttons');
-}
+    });
+
+    function initPayPal() {
+        paypal.Buttons({
+            createOrder() {
+                return fetch('<?php echo site_url('guest/gateways/paypal/paypal_create_order/' . $invoice_url_key); ?>',
+                    {
+                        method: "GET"
+                    })
+                    .then((response) => response.json())
+                    .then((order) => order.id)
+            },
+            onApprove: function (data) {
+                return fetch('<?php echo site_url('guest/gateways/paypal/paypal_capture_payment/');?>' + data.orderID, {
+                    method: 'GET'
+                })
+                    .then((response) => window.location.replace('<?php echo site_url('guest/view/invoice/' . $invoice_url_key); ?>'));
+            },
+            onError: function (error) {
+                console.log(error)
+                window.location.replace('<?php echo site_url('guest/payment_information/form/' . $invoice_url_key . '/paypal'); ?>')
+            }
+        }).render('#paypal-buttons');
+    }
 </script>
