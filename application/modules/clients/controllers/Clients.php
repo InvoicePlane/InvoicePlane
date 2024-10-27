cf5f55e8373e9dbdc64b7df96e1efe85d2ce2a6d
pumpi <sf@pumpi-online.de>
2024-10-27 11:07:33 +0000

    refactor: rename tab variable to activeTab

--- application/modules/clients/controllers/Clients.php
+++ application/modules/clients/controllers/Clients.php
@@ -183,7 +183,7 @@ class Clients extends Admin_Controller
     /**
      * @param int $client_id
      */
-    public function view($client_id, $tab = 'detail', $page = 0)
+    public function view($client_id, $activeTab = 'detail', $page = 0)
     {
         $this->load->model('clients/mdl_client_notes');
         $this->load->model('invoices/mdl_invoices');
@@ -221,7 +221,7 @@ class Clients extends Admin_Controller
                 'custom_fields'    => $custom_fields,
                 'quote_statuses'   => $this->mdl_quotes->statuses(),
                 'invoice_statuses' => $this->mdl_invoices->statuses(),
-                'tab'              => $tab,
+                'activeTab'        => $activeTab,
             ]
         );
 
