5e96598afe11705d0e48456fd26219c9009e6122
naui95 <naui95@hotmail.com>
2024-11-09 22:18:39 +0100

    added db changes

--- application/modules/setup/sql/038_1.6.2.sql
+++ application/modules/setup/sql/038_1.6.2.sql
@@ -1,2 +1,3 @@
 ALTER table `ip_invoice_items` MODIFY `item_quantity` DECIMAL(20,8) NULL;
 ALTER table `ip_quote_items` MODIFY `item_quantity` DECIMAL(20,8) NULL;
+INSERT INTO `ip_settings` (`setting_key`,`setting_value`) VALUES ('default_item_decimals',2);
