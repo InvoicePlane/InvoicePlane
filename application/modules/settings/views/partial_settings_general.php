b4a5c8145a5447270461853e50d8031e3bf0232b
naui95 <naui95@hotmail.com>
2024-11-09 22:19:02 +0100

    created setting

--- application/modules/settings/views/partial_settings_general.php
+++ application/modules/settings/views/partial_settings_general.php
@@ -218,6 +218,27 @@
                             </select>
                         </div>
                     </div>
+
+                    <div class="col-xs-12 col-md-6">
+                        <div class="form-group">
+                            <label for="settings[default_item_decimals]">
+                                <?php _trans('default_item_decimals'); ?>
+                            </label>
+                            <?php $current_default_item_decimals = get_setting('default_item_decimals'); ?>
+                            <select name="settings[default_item_decimals]" id="settings[default_item_decimals]"
+                                class="form-control simple-select"
+                                data-minimum-results-for-search="Infinity">
+                                <option value="1" <?php check_select($current_default_item_decimals, '1'); ?>>1</option>
+                                <option value="2" <?php check_select($current_default_item_decimals, '2'); ?>>2</option>
+                                <option value="3" <?php check_select($current_default_item_decimals, '3'); ?>>3</option>
+                                <option value="4" <?php check_select($current_default_item_decimals, '4'); ?>>4</option>
+                                <option value="5" <?php check_select($current_default_item_decimals, '5'); ?>>5</option>
+                                <option value="6" <?php check_select($current_default_item_decimals, '6'); ?>>6</option>
+                                <option value="7" <?php check_select($current_default_item_decimals, '7'); ?>>7</option>
+                                <option value="8" <?php check_select($current_default_item_decimals, '8'); ?>>8</option>
+                            </select>
+                        </div>
+                    </div>
                 </div>
 
             </div>
