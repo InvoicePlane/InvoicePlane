08a12d2c80da46d6e3d430b2ec2495049618ba45
Niels Drost <nielsdrost7@gmail.com>
2024-11-15 11:50:35 +0100

    1063: Set back Cryptor the way it was and then allowed Dynamic Properties again

--- application/libraries/Cryptor.php
+++ application/libraries/Cryptor.php
@@ -35,38 +35,34 @@ if (! defined('BASEPATH')) {
  */
 class Cryptor
 {
-    public const FORMAT_RAW = 0;
-
-    public const FORMAT_B64 = 1;
-
-    public const FORMAT_HEX = 2;
-
     private $cipher_algo;
-
     private $hash_algo;
-
     private $iv_num_bytes;
-
     private $format;
+
+    const FORMAT_RAW = 0;
+    const FORMAT_B64 = 1;
+    const FORMAT_HEX = 2;
 
     /**
      * Construct a Cryptor, using aes256 encryption, sha256 key hashing and base64 encoding.
-     *
-     * @param string $cipher_algo the cipher algorithm
-     * @param string $hash_algo   key hashing algorithm
-     * @param [type] $fmt         Format of the encrypted data
+     * @param string $cipher_algo The cipher algorithm.
+     * @param string $hash_algo   Key hashing algorithm.
+     * @param [type] $fmt         Format of the encrypted data.
      */
-    public function __construct($cipher_algo = 'aes-256-ctr', $hash_algo = 'sha256', $fmt = self::FORMAT_B64)
+    public function __construct($cipher_algo = 'aes-256-ctr', $hash_algo = 'sha256', $fmt = Cryptor::FORMAT_B64)
     {
         $this->cipher_algo = $cipher_algo;
         $this->hash_algo = $hash_algo;
         $this->format = $fmt;
 
-        if ( ! in_array($cipher_algo, openssl_get_cipher_methods(true))) {
+        if (!in_array($cipher_algo, openssl_get_cipher_methods(true)))
+        {
             throw new \Exception("Cryptor:: - unknown cipher algo {$cipher_algo}");
         }
 
-        if ( ! in_array($hash_algo, openssl_get_md_methods(true))) {
+        if (!in_array($hash_algo, openssl_get_md_methods(true)))
+        {
             throw new \Exception("Cryptor:: - unknown hash algo {$hash_algo}");
         }
 
@@ -74,66 +70,34 @@ class Cryptor
     }
 
     /**
-     * Static convenience method for encrypting.
-     *
-     * @param string $in  string to encrypt
-     * @param string $key encryption key
-     * @param int    $fmt Optional override for the output encoding. One of FORMAT_RAW, FORMAT_B64 or FORMAT_HEX.
-     *
-     * @return string the encrypted string
-     */
-    public static function Encrypt($in, $key, $fmt = null)
-    {
-        $c = new self();
-
-        return $c->encryptString($in, $key, $fmt);
-    }
-
-    /**
-     * Static convenience method for decrypting.
-     *
-     * @param string $in  string to decrypt
-     * @param string $key decryption key
-     * @param int    $fmt Optional override for the input encoding. One of FORMAT_RAW, FORMAT_B64 or FORMAT_HEX.
-     *
-     * @return string the decrypted string
-     */
-    public static function Decrypt($in, $key, $fmt = null)
-    {
-        $c = new self();
-
-        return $c->decryptString($in, $key, $fmt);
-    }
-
-    /**
      * Encrypt a string.
-     *
-     * @param string $in  string to encrypt
-     * @param string $key encryption key
-     * @param int    $fmt Optional override for the output encoding. One of FORMAT_RAW, FORMAT_B64 or FORMAT_HEX.
-     *
-     * @return string the encrypted string
+     * @param  string $in  String to encrypt.
+     * @param  string $key Encryption key.
+     * @param  int $fmt Optional override for the output encoding. One of FORMAT_RAW, FORMAT_B64 or FORMAT_HEX.
+     * @return string      The encrypted string.
      */
     public function encryptString($in, $key, $fmt = null)
     {
-        if ($fmt === null) {
+        if ($fmt === null)
+        {
             $fmt = $this->format;
         }
 
         // Build an initialisation vector
         $iv = openssl_random_pseudo_bytes($this->iv_num_bytes, $isStrongCrypto);
-        if ( ! $isStrongCrypto) {
-            throw new \Exception('Cryptor::encryptString() - Not a strong key');
+        if (!$isStrongCrypto) {
+            throw new \Exception("Cryptor::encryptString() - Not a strong key");
         }
 
         // Hash the key
         $keyhash = openssl_digest($key, $this->hash_algo, true);
 
         // and encrypt
-        $opts = OPENSSL_RAW_DATA;
+        $opts =  OPENSSL_RAW_DATA;
         $encrypted = openssl_encrypt($in, $this->cipher_algo, $keyhash, $opts, $iv);
 
-        if ($encrypted === false) {
+        if ($encrypted === false)
+        {
             throw new \Exception('Cryptor::encryptString() - Encryption failed: ' . openssl_error_string());
         }
 
@@ -141,9 +105,12 @@ class Cryptor
         $res = $iv . $encrypted;
 
         // and format the result if required.
-        if ($fmt == self::FORMAT_B64) {
+        if ($fmt == Cryptor::FORMAT_B64)
+        {
             $res = base64_encode($res);
-        } elseif ($fmt == self::FORMAT_HEX) {
+        }
+        else if ($fmt == Cryptor::FORMAT_HEX)
+        {
             $res = unpack('H*', $res)[1];
         }
 
@@ -152,37 +119,40 @@ class Cryptor
 
     /**
      * Decrypt a string.
-     *
-     * @param string $in  string to decrypt
-     * @param string $key decryption key
-     * @param int    $fmt Optional override for the input encoding. One of FORMAT_RAW, FORMAT_B64 or FORMAT_HEX.
-     *
-     * @return string the decrypted string
+     * @param  string $in  String to decrypt.
+     * @param  string $key Decryption key.
+     * @param  int $fmt Optional override for the input encoding. One of FORMAT_RAW, FORMAT_B64 or FORMAT_HEX.
+     * @return string      The decrypted string.
      */
     public function decryptString($in, $key, $fmt = null)
     {
-        if ($fmt === null) {
+        if ($fmt === null)
+        {
             $fmt = $this->format;
         }
 
         $raw = $in;
 
         // Restore the encrypted data if encoded
-        if ($fmt == self::FORMAT_B64) {
+        if ($fmt == Cryptor::FORMAT_B64)
+        {
             $raw = base64_decode($in);
-        } elseif ($fmt == self::FORMAT_HEX) {
+        }
+        else if ($fmt == Cryptor::FORMAT_HEX)
+        {
             $raw = pack('H*', $in);
         }
 
         // and do an integrity check on the size.
-        if (mb_strlen($raw) < $this->iv_num_bytes) {
+        if (strlen($raw) < $this->iv_num_bytes)
+        {
             throw new \Exception('Cryptor::decryptString() - ' .
-                'data length ' . mb_strlen($raw) . " is less than iv length {$this->iv_num_bytes}");
+                'data length ' . strlen($raw) . " is less than iv length {$this->iv_num_bytes}");
         }
 
         // Extract the initialisation vector and encrypted data
-        $iv = mb_substr($raw, 0, $this->iv_num_bytes);
-        $raw = mb_substr($raw, $this->iv_num_bytes);
+        $iv = substr($raw, 0, $this->iv_num_bytes);
+        $raw = substr($raw, $this->iv_num_bytes);
 
         // Hash the key
         $keyhash = openssl_digest($key, $this->hash_algo, true);
@@ -191,10 +161,37 @@ class Cryptor
         $opts = OPENSSL_RAW_DATA;
         $res = openssl_decrypt($raw, $this->cipher_algo, $keyhash, $opts, $iv);
 
-        if ($res === false) {
+        if ($res === false)
+        {
             throw new \Exception('Cryptor::decryptString - decryption failed: ' . openssl_error_string());
         }
 
         return $res;
+    }
+
+    /**
+     * Static convenience method for encrypting.
+     * @param  string $in  String to encrypt.
+     * @param  string $key Encryption key.
+     * @param  int $fmt Optional override for the output encoding. One of FORMAT_RAW, FORMAT_B64 or FORMAT_HEX.
+     * @return string      The encrypted string.
+     */
+    public static function Encrypt($in, $key, $fmt = null)
+    {
+        $c = new Cryptor();
+        return $c->encryptString($in, $key, $fmt);
+    }
+
+    /**
+     * Static convenience method for decrypting.
+     * @param  string $in  String to decrypt.
+     * @param  string $key Decryption key.
+     * @param  int $fmt Optional override for the input encoding. One of FORMAT_RAW, FORMAT_B64 or FORMAT_HEX.
+     * @return string      The decrypted string.
+     */
+    public static function Decrypt($in, $key, $fmt = null)
+    {
+        $c = new Cryptor();
+        return $c->decryptString($in, $key, $fmt);
     }
 }
