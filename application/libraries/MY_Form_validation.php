ba2e91117142171787106c343709ecb1f6ea3ff2
Niels Drost <nielsdrost7@gmail.com>
2024-11-15 11:53:35 +0100

    1063: Set back MY_Form_validation the way it was and then allowed Dynamic Properties again

--- application/libraries/MY_Form_validation.php
+++ application/libraries/MY_Form_validation.php
@@ -22,21 +22,20 @@ class MY_Form_validation extends CI_Form_validation
 
     public function is_unique($str, $field)
     {
-        if (mb_substr_count($field, '.') == 3) {
+        if (substr_count($field, '.') == 3) {
             list($table, $field, $id_field, $id_val) = explode('.', $field);
             $query = $this->CI->db->limit(1)->where($field, $str)->where($id_field . ' != ', $id_val)->get($table);
         } else {
             list($table, $field) = explode('.', $field);
-            $query = $this->CI->db->limit(1)->get_where($table, [$field => $str]);
+            $query = $this->CI->db->limit(1)->get_where($table, array($field => $str));
         }
 
         return $query->num_rows() === 0;
     }
 
-    public function run($module = '', $group = '')
+    function run($module = '', $group = '')
     {
-        (is_object($module)) && $this->CI = &$module;
-
+        (is_object($module)) AND $this->CI = &$module;
         return parent::run($group);
     }
 }
