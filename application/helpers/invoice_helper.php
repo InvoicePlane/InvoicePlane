8f4f84da6baafd328c535e224878e0c42276357a
Georg Klimm <gklimm@gmail.com>
2024-11-21 14:25:38 +0100

    test

--- application/helpers/invoice_helper.php
+++ application/helpers/invoice_helper.php
@@ -4,7 +4,7 @@ if (!defined('BASEPATH')) exit('No direct script access allowed');
 /*
  * InvoicePlane
  *
- * @author		InvoicePlane Developers & Contributors
+ * @author		InvoicePlane Developers & Contributors 
  * @copyright	Copyright (c) 2012 - 2018 InvoicePlane.com
  * @license		https://invoiceplane.com/license.txt
  * @link		https://invoiceplane.com
