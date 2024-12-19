8d8b600184142d98f98e90b470bc65e9cf65470e
Thomas Ingles <thomas@sudwebdesign.fr>
2024-12-19 20:42:03 +0100

    Fix #1146 after posting a Payments Form when amount > inv. total
    https://github.com/InvoicePlane/InvoicePlane/issues/1146
    
    Fix number_format(): Arg #1 ($num) must be of type float, string given
    modules/payments/views/form.php Function: format_amount

--- application/modules/payments/models/Mdl_payments.php
+++ application/modules/payments/models/Mdl_payments.php
@@ -28,7 +28,7 @@ class Mdl_Payments extends Response_Model
             ip_invoice_amounts.*,
             ip_clients.client_name,
             ip_clients.client_surname,
-        	  ip_clients.client_id,
+            ip_clients.client_id,
             ip_invoices.invoice_number,
             ip_invoices.invoice_date_created,
             ip_payments.*", false);
