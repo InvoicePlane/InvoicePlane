e2c4ff5fa8552b796d6625f3c77a481b0136a647
Verony <30659226+Verony-makesIT@users.noreply.github.com>
2024-12-27 09:16:33 +0100

    Small XmlConfig changes
    Deleted "ZugferdV20" example
    Added XMLname variable for CII invoices

--- application/helpers/XMLconfigs/Ublexamv20.php
+++ application/helpers/XMLconfigs/Ublexamv20.php
@@ -5,10 +5,12 @@
 
 //  'full-name'   => 'UBL example v2.0',  -> UBL version name visible in the clients drop-down menu
 //  'countrycode' => 'EX',                -> associated countrycode (if available in your native language country list)
-//  'embedXML'    => false,               -> for 'Zugferd' (and similar = Xml embedded in Pdf) set to true
+//  'embedXML'    => false,               -> for 'ZUGFeRD' (and similar = Xml embedded in Pdf) set to true
+//  'XMLname'     => 'factur-x.xml',      -> name of the Xml file (if embedded in a CII Pdf)
 
 $xml_setting = array( 
     'full-name'   => 'UBL example v2.0',
     'countrycode' => 'EX',
     'embedXML'    => false,
+    'XMLname'     => false,
 );
--- application/helpers/XMLconfigs/Zugferdv10.php
+++ application/helpers/XMLconfigs/Zugferdv10.php
@@ -5,13 +5,12 @@
 
 //  'full-name'   => 'UBL example v2.0',  -> UBL version name visible in the clients drop-down menu
 //  'countrycode' => 'EX',                -> associated countrycode (if available in your native language country list)
-//  'embedXML'    => false,               -> for 'Zugferd' (and similar = Xml embedded in Pdf) set to true
-//  'XMLname'     => 'factur-x.xml',      -> name of the Xml file (if embedded in Pdf)
+//  'embedXML'    => false,               -> for 'ZUGFeRD' (and similar = Xml embedded in Pdf) set to true
+//  'XMLname'     => 'factur-x.xml',      -> name of the Xml file (if embedded in a CII Pdf)
 
 $xml_setting = array( 
   'full-name'   => 'ZUGFeRD v1.0',
   'countrycode' => 'DE',
   'embedXML'    => true,
   'XMLname'     => 'ZUGFeRD-invoice.xml',
-  // 'XMLname'     => 'factur-x.xml',
 );
