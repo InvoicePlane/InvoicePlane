f8cc148252ad2bdf1da05e0da6e709ac4620bbce
AeroBytes <31496522+AeroBytesNL@users.noreply.github.com>
2024-12-28 18:24:38 +0100

    bug-fix-#1147-error-on-database-migration (#1159)
    * Added required input check on full page loaded to 'fix' #1130
    
    * Refactored upgrade_tables function in Mdl_setup.php
    
    * Refactored execute_contents in Mdl_setup.php to remove nested ifs
    
    * Added error ignoring on database upgrade to fix #1147
    
    * Removed comment and changed AND operator to OR in Mdl_setup.php
    
    * Refactored upgrade_tables function in Mdl_setup.php
    
    * Refactored execute_contents in Mdl_setup.php to remove nested ifs
    
    * Added error ignoring on database upgrade to fix #1147
    
    * Removed comment and changed AND operator to OR in Mdl_setup.php
    
    * Removed scripts.js from conflicting branche
    
    * Removed scripts.js from conflicting branche
    
    * Update Mdl_setup.php
    
    * Added displaying DB error and moved to negative comparison in Mdl_setup.php
    
    * Removed tab and comments in Mdl_setup.php
    
    ---------
    
    Co-authored-by: Niels Drost <47660417+nielsdrost7@users.noreply.github.com>

--- application/modules/setup/models/Mdl_setup.php
+++ application/modules/setup/models/Mdl_setup.php
@@ -48,11 +48,16 @@ class Mdl_Setup extends CI_Model
         $commands = explode(';', $contents);
 
         foreach ($commands as $command) {
-            if (trim($command)) {
-                if (!$this->db->query(trim($command) . ';')) {
-                    $this->errors[] = $this->db->_error_message();
-                }
+            if (!trim($command)) {
+                continue;
             }
+
+            $this->db->query(trim($command) . ';');
+			
+			$error = $this->db->error();
+			if ($error['code'] !== 1050 || $error['code'] !== 1060) {
+				$this->errors[] = $this->db->_error_message();
+			}
         }
     }
 
@@ -157,28 +162,28 @@ class Mdl_Setup extends CI_Model
 
         // Loop through the files and take appropriate action
         foreach ($sql_files as $sql_file) {
-            if (substr($sql_file, -4) == '.sql') {
-                // $this->db->select('COUNT(*) AS update_applied');
-                $this->db->where('version_file', $sql_file);
-                // $update_applied = $this->db->get('ip_versions')->row()->update_applied;
-                $update_applied = $this->db->get('ip_versions');
+			if (substr($sql_file, -4) !== '.sql') {
+				continue;
+			}
+			
+			$this->db->where('version_file', $sql_file);
+			$update_applied = $this->db->get('ip_versions');
 
-                // if (!$update_applied)
-                if (!$update_applied->num_rows()) {
-                    $file_contents = file_get_contents(APPPATH . 'modules/setup/sql/' . $sql_file);
+			if ($update_applied->num_rows()) {
+				continue;
+			}
+			
+			$file_contents = file_get_contents(APPPATH . 'modules/setup/sql/' . $sql_file);
+			$this->execute_contents($file_contents);
+			$this->save_version($sql_file);
 
-                    $this->execute_contents($file_contents);
+			$upgrade_method = 'upgrade_' . str_replace('.', '_', substr($sql_file, 0, -4));
 
-                    $this->save_version($sql_file);
-
-                    // Check for any required upgrade methods
-                    $upgrade_method = 'upgrade_' . str_replace('.', '_', substr($sql_file, 0, -4));
-
-                    if (method_exists($this, $upgrade_method)) {
-                        $this->$upgrade_method();
-                    }
-                }
-            }
+			if (!method_exists($this, $upgrade_method)) {
+				continue;
+			}
+			
+			$this->$upgrade_method();
         }
 
         if ($this->errors) {
