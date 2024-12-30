660813cd6327baac74cb73ce83a2652c605070c0
Niels Drost <47660417+nielsdrost7@users.noreply.github.com>
2024-12-30 11:09:39 +0100

    fix problem

--- application/modules/email_templates/views/template-tags-invoices.php
+++ application/modules/email_templates/views/template-tags-invoices.php
@@ -45,8 +45,8 @@
         </optgroup>
 
         <optgroup label="<?php _trans('custom_fields'); ?>">
-            if(isset($custom_fields['ip_invoice_custom'])) {
-                <?php foreach ($custom_fields['ip_invoice_custom'] as $custom) { ?>
+            <?php if(isset($custom_fields['ip_invoice_custom'])) {
+                foreach ($custom_fields['ip_invoice_custom'] as $custom) { ?>
                     <option value="{{{<?php echo 'ip_cf_' . $custom->custom_field_id; ?>}}}">
                         <?php echo $custom->custom_field_label . ' (ID ' . $custom->custom_field_id . ')'; ?>
                     </option>
