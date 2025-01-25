d04a3eaa1d4d47aa9fdc4eea25085d7dc6b5a94f
Thomas Ingles <thomas@sudwebdesign.fr>
2025-01-25 18:25:55 +0100

    partial_itemlist_table[quot|invoic]e_discount access lint fix

--- application/modules/quotes/views/partial_itemlist_table_quote_discount.php
+++ application/modules/quotes/views/partial_itemlist_table_quote_discount.php
@@ -3,6 +3,7 @@
                 <td class="clearfix">
                     <div class="discount-field">
                         <div class="input-group input-group-sm">
+                            <label for="quote_discount_amount" class="input-group-addon ig-addon-aligned"><?php _trans('discount'); ?></label>
                             <input id="quote_discount_amount" name="quote_discount_amount"
                                    class="discount-option form-control input-sm amount"
                                    value="<?php echo format_amount($quote->quote_discount_amount != 0 ? $quote->quote_discount_amount : ''); ?>">
@@ -11,6 +12,7 @@
                     </div>
                     <div class="discount-field">
                         <div class="input-group input-group-sm">
+                            <label for="quote_discount_percent" class="input-group-addon ig-addon-aligned"><?php _trans('discount'); ?> %</label>
                             <input id="quote_discount_percent" name="quote_discount_percent"
                                    value="<?php echo format_amount($quote->quote_discount_percent != 0 ? $quote->quote_discount_percent : ''); ?>"
                                    class="discount-option form-control input-sm amount">
