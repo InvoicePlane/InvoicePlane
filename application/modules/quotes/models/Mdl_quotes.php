f2ef2298f15c8bce51118c14e3e70ad76b8fa7f5
Thomas Ingles <thomas@sudwebdesign.fr>
2025-01-31 16:31:06 +0100

    sort quotes by date instead of id
    see #1218

--- application/modules/quotes/models/Mdl_quotes.php
+++ application/modules/quotes/models/Mdl_quotes.php
@@ -78,7 +78,7 @@ class Mdl_Quotes extends Response_Model
 
     public function default_order_by()
     {
-        $this->db->order_by('ip_quotes.quote_id DESC');
+        $this->db->order_by('ip_quotes.quote_date_created DESC');
     }
 
     public function default_join()
