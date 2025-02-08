3822013d380cb3f8802793b8d674a3ad796f38e2
Thomas Ingles <thomas@sudwebdesign.fr>
2025-02-08 10:21:04 +0100

    wip item discount from invoices
    wip need to correct me

--- application/modules/layout/views/partial/itemlist_responsive_item_discount_show.php
+++ application/modules/layout/views/partial/itemlist_responsive_item_discount_show.php
@@ -4,9 +4,19 @@ $item_value = isset($item->item_discount) ? format_currency($item->item_discount
 ?>
                                 <div class="row mb-1">
                                     <div class="col-xs-9 col-sm-8">
-                                        <?php _trans('discount'); ?>:
+                                        <?php _trans('item'); ?> <?php _trans('discount'); ?>:
                                     </div>
                                     <div class="col-xs-3 col-sm-4">
                                         <?php echo $item_value; ?>
                                     </div>
                                 </div>
+<?php if ( ! $legacy_calculation) : ?>
+                                <div class="row mb-1">
+                                    <div class="col-xs-9 col-sm-8">
+                                        <?php _trans('invoice'); ?> <?php _trans('discount'); ?>:
+                                    </div>
+                                    <div class="col-xs-3 col-sm-4">
+                                        <?php echo format_currency($item->item_subtotal - ($item->item_total - $item->item_tax_total + $item->item_discount)); ?>
+                                    </div>
+                                </div>
+<?php endif ?>
--- application/modules/layout/views/partial/itemlist_table_item_discount_show.php
+++ application/modules/layout/views/partial/itemlist_table_item_discount_show.php
@@ -5,4 +5,7 @@ $item_value = isset($item->item_discount) ? format_currency($item->item_discount
                 <td class="td-amount td-vert-middle">
                     <span><?php _trans('discount'); ?></span><br/>
                     <span name="item_discount_total" class="amount"><?php echo $item_value; ?></span>
+<?php if ( ! $legacy_calculation) : ?>
+                    <span name="item_discount_total" class="amount"><?php echo format_currency($item->item_subtotal - ($item->item_total - $item->item_tax_total + $item->item_discount)); ?></span>
+<?php endif ?>
                 </td>
