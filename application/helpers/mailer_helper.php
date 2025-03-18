912c49c588fed22920ff5160dbc705fa3d7141e5
Thomas Ingles <thomas@sudwebdesign.fr>
2025-03-19 00:54:14 +0100

    Fix send e-mail addr by template {{{}}} blocked by brwsr & server
    Like {{{user_email}}} in from/cc/bcc input rework
    Check valid e-mail only after parse_template()
    But before phpmailer (to see error translated)
    
    Meaculpa & the original flow restored (but improved)

--- application/helpers/mailer_helper.php
+++ application/helpers/mailer_helper.php
@@ -7,10 +7,10 @@ if ( ! defined('BASEPATH')) {
 /*
  * InvoicePlane
  *
- * @author		InvoicePlane Developers & Contributors
- * @copyright	Copyright (c) 2012 - 2018 InvoicePlane.com
- * @license		https://invoiceplane.com/license.txt
- * @link		https://invoiceplane.com
+ * @author      InvoicePlane Developers & Contributors
+ * @copyright   Copyright (c) 2012 - 2018 InvoicePlane.com
+ * @license     https://invoiceplane.com/license.txt
+ * @link        https://invoiceplane.com
  */
 
 /**
@@ -53,12 +53,14 @@ function email_invoice(
     $bcc = null,
     $attachments = null
 ) {
-    $CI = &get_instance();
+    $CI = & get_instance();
 
-    $CI->load->helper('mailer/phpmailer');
-    $CI->load->helper('template');
-    $CI->load->helper('invoice');
-    $CI->load->helper('pdf');
+    $CI->load->helper([
+        'mailer/phpmailer',
+        'template',
+        'invoice',
+        'pdf',
+    ]);
 
     $db_invoice = $CI->mdl_invoices->where('ip_invoices.invoice_id', $invoice_id)->get()->row();
 
@@ -73,6 +75,21 @@ function email_invoice(
     $cc      = parse_template($db_invoice, $cc);
     $bcc     = parse_template($db_invoice, $bcc);
     $from    = [parse_template($db_invoice, $from[0]), parse_template($db_invoice, $from[1])];
+
+    // Check parsed emails before phpmail - since v1.6.3
+    if (! filter_var($to, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = 'to_email';
+    }
+    if (! filter_var($from[0], FILTER_VALIDATE_EMAIL)) {
+        $errors[] = 'from_email';
+    }
+    if ($cc && ! filter_var($cc, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = 'cc_email';
+    }
+    if ($bcc && ! filter_var($bcc, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = 'bcc_email';
+    }
+    check_mail_errors($errors, 'mailer/invoice/' . $invoice_id);
 
     $message = (empty($message) ? ' ' : $message);
 
@@ -105,11 +122,13 @@ function email_quote(
     $bcc = null,
     $attachments = null
 ) {
-    $CI = &get_instance();
+    $CI = & get_instance();
 
-    $CI->load->helper('mailer/phpmailer');
-    $CI->load->helper('template');
-    $CI->load->helper('pdf');
+    $CI->load->helper([
+        'mailer/phpmailer',
+        'template',
+        'pdf',
+    ]);
 
     $quote = generate_quote_pdf($quote_id, false, $quote_template);
 
@@ -120,6 +139,21 @@ function email_quote(
     $cc      = parse_template($db_quote, $cc);
     $bcc     = parse_template($db_quote, $bcc);
     $from    = [parse_template($db_quote, $from[0]), parse_template($db_quote, $from[1])];
+
+    // Check parsed emails before phpmail - since v1.6.3
+    if (! filter_var($to, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = 'to_email';
+    }
+    if (! filter_var($from[0], FILTER_VALIDATE_EMAIL)) {
+        $errors[] = 'from_email';
+    }
+    if ($cc && ! filter_var($cc, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = 'cc_email';
+    }
+    if ($bcc && ! filter_var($bcc, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = 'bcc_email';
+    }
+    check_mail_errors($errors, 'mailer/quote/' . $quote_id);
 
     $message = (empty($message) ? ' ' : $message);
 
@@ -143,7 +177,7 @@ function email_quote_status($quote_id, $status)
         return false;
     }
 
-    $CI = &get_instance();
+    $CI = & get_instance();
     $CI->load->helper('mailer/phpmailer');
 
     $quote    = $CI->mdl_quotes->where('ip_quotes.quote_id', $quote_id)->get()->row();
@@ -166,4 +200,23 @@ function email_quote_status($quote_id, $status)
     );
 
     return phpmail_send($user_email, $user_email, $subject, $body);
+}
+
+/**
+ * @param []  $errors
+ * @param str $redirect
+ */
+function check_mail_errors($errors = [], $redirect = '')
+{
+    if ($errors)
+    {
+        $CI = & get_instance();
+        foreach($errors as $i => $e)
+        {
+            $errors[$i] = strtr(trans('form_validation_valid_email'), ['{field}' => trans($e)]);
+        }
+        $CI->session->set_flashdata('alert_error', implode('<br>', $errors));
+        $redirect = empty($redirect) ? (empty($_SERVER['HTTP_REFERER']) ? '' : $_SERVER['HTTP_REFERER']) : $redirect;
+        redirect($redirect);
+    }
 }
