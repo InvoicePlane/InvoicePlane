f4fcb611420d5d50710356f1edc5d73af911b5dd
Thomas Ingles <thomas@sudwebdesign.fr>
2025-04-16 16:00:18 +0200

    Rector up to DeadCodeLevel(2)
    Applied rules:
     * RecastingRemovalRector

--- rector.php
+++ rector.php
@@ -21,6 +21,6 @@ return RectorConfig::configure()
     // uncomment to reach your current PHP version
     // ->withPhpSets()
     ->withTypeCoverageLevel(0)
-    ->withDeadCodeLevel(1)
+    ->withDeadCodeLevel(2)
     ->withCodeQualityLevel(70) // 0 to 70
     ->withCodingStyleLevel(12);
