0bdb85666914f63b17cca76991a166ab93f6081d
Niels Drost <nielsdrost7@gmail.com>
2025-08-06 12:40:45 +0200

    ## Version 1.6.3 (final)
    ### What's Changed (since v1.6.2)
    
    #### Major Features & E-Invoicing
    
    * [[IP-1268](#1268)] **E-invoicing infrastructure update** by @sudwebdesign
    * [[IP-1272](#1272)] **E-invoicing enhancements** by @sudwebdesign
    * [[IP-1247](#1247)] Processing e-invoices flow (and some bugfixes) for version 1.6.3 by @nielsdrost7
    * [[IP-1282](#1282)] feat: Legacy calculation setup step by @pumpi
    * [[IP-1281](#1281)] fix: Client overview shows wrong e-Invoicing state by @pumpi
    
    #### Changed / Improvements
    
    * [[IP-1277](#1277)] Replace node-sass with sass by @onny
    * [[IP-1261](#1261)] Guest Payment stripe flow & online_payment lang improved by @nielsdrost7
    * [[IP-1178](#1178)] Add custom_fields in controllers/Settings by @sudwebdesign
    * [[IP-1206](#1206)] Add report: Invoices per client by @mheiduk
    * [[IP-1228](#1228)] Improve number_helper & standardize_amount (fix european format) by @sudwebdesign
    * [[IP-1229](#1229)] Remove unattended standardize_amount in payments view form by @sudwebdesign
    * [[IP-1241](#1241)] Add pagination to invoice and quote templates by @onny
    * [[IP-1219](#1219)] Sort invoices by date instead of id by @MrKrisKrisu
    * [[IP-1222](#1222)] Sort quotes by date instead of id by @sudwebdesign
    
    #### Fixed
    
    * [[IP-1179](#1179)] Fix #fullpage-loader (Spinner) never showed after save by @sudwebdesign
    * [[IP-1174](#1174)] Removed '.pdf' from Invoices.php downloads to fix issue by @HeapReaper
    * [[IP-1175](#1175)] Check invoice balance before rendering QR code in web view by @idressos
    * [[IP-1183](#1183)] Fix summary client delete button go to 404 (link2form) by @sudwebdesign
    * [[IP-1185](#1185)] Fix styling in clients table header by @tstoeter
    * [[IP-1186](#1186)] Style2class for amounts & balances (th & tr) by @sudwebdesign
    * [[IP-1197](#1197)] Send email show blank page (php>=8.2) by @sudwebdesign
    * [[IP-1199](#1199)] Add invoice_status case in template_helper by @sudwebdesign
    * [[IP-1201](#1201)] Fix SMTP password wrong after saving settings by @sudwebdesign
    * [[IP-1204](#1204)] Update template_helper.php to fix email template with custom single choice field by @LOK-Soft
    * [[IP-1251](#1251)] fix: amount of the credit transfer cannot be smaller than 0.01 Euro by @mheiduk
    * [[IP-1278](#1278)] Fix: Styling issues by @pumpi
    * [[IP-1283](#1283)] fix: Client detail view exception by
    
    **Full Changelog**: https://github.com/InvoicePlane/InvoicePlane/compare/v1.6.2...v1.6.3

--- application/language/english/ip_lang.php
+++ application/language/english/ip_lang.php
@@ -809,13 +809,6 @@ $lang = [
     'professor' => 'professor',
     'custom'    => 'custom',
 
-    // Clients/Enums/ClientTitleEnum
-    'mr'                                           => 'mr',
-    'mrs'                                          => 'mrs',
-    'doctor'                                       => 'doctor',
-    'professor'                                    => 'professor',
-    'custom'                                       => 'custom',
-
     // Errors
     'validator_fail' => 'Unable to process field %s: %s',
 
