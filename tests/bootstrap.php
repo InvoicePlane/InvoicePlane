b805f73384dfc0467658236b812e93654fe7fbe1
Niels Drost <nielsdrost7@gmail.com>
2026-04-16 15:35:50 +0200

    v172: WIP

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,40 +1,43 @@
 <?php
 
 /**
- * PHPUnit Bootstrap File
- * 
+ * PHPUnit Bootstrap File.
+ *
  * This file is loaded before running tests to set up the testing environment.
  */
 
 // Define constants that CodeIgniter expects
-if (!defined('BASEPATH')) {
+if ( ! defined('BASEPATH')) {
     define('BASEPATH', __DIR__ . '/../vendor/pocketarc/codeigniter/system/');
 }
 
-if (!defined('APPPATH')) {
+if ( ! defined('APPPATH')) {
     define('APPPATH', __DIR__ . '/../application/');
 }
 
-if (!defined('ENVIRONMENT')) {
+if ( ! defined('ENVIRONMENT')) {
     define('ENVIRONMENT', 'testing');
 }
 
 // Mock log_message function for tests
-if (!function_exists('log_message')) {
-    function log_message($level, $message) {
+if ( ! function_exists('log_message')) {
+    function log_message($level, $message)
+    {
         // Silently ignore during tests
         return true;
     }
 }
 
 // Mock html_escape function for tests
-if (!function_exists('html_escape')) {
-    function html_escape($var, $double_encode = true) {
+if ( ! function_exists('html_escape')) {
+    function html_escape($var, $double_encode = true)
+    {
         if (is_array($var)) {
-            return array_map(function($v) use ($double_encode) {
+            return array_map(function ($v) use ($double_encode) {
                 return html_escape($v, $double_encode);
             }, $var);
         }
+
         return htmlspecialchars($var, ENT_QUOTES, 'UTF-8', $double_encode);
     }
 }
