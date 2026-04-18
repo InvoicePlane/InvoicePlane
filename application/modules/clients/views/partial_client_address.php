fbeb320e2fab80745b8c03b0e9d4fe50646c6cd2
Jannomag <jannomag@gmx.de>
2026-04-18 08:58:04 +0200

    Reorder address components based on country rules

--- application/modules/clients/views/partial_client_address.php
+++ application/modules/clients/views/partial_client_address.php
@@ -5,10 +5,30 @@
     <?php echo $client->client_address_2 ? htmlsc($client->client_address_2) . '<br>' : ''; ?>
 </span>
 <span class="client-adress-town-line">
-    <?php echo $client->client_city ? htmlsc($client->client_city) . ' ' : ''; ?>
-    <?php echo $client->client_state ? htmlsc($client->client_state) . ' ' : ''; ?>
-    <?php echo $client->client_zip ? htmlsc($client->client_zip) : ''; ?>
+    <?php 
+    // list of countries with ZIP + City order
+    $zipBeforeCityCountries = ['AT', 'BE', 'CH', 'CZ', 'DE', 'DK', 'ES', 'FI', 'FR', 'IT', 'LU', 'NL', 'NO', 'PL', 'SE', 'SK'];
+    
+    // check default country, if empty use system default
+    $country_to_check = $client->client_country ? $client->client_country : get_setting('default_country');
+    // if the client country or (if empty) the (system) default country matches one of the "zipBeforeCityCountries" change the order to zip+city
+    if (in_array($country_to_check, $zipBeforeCityCountries)): ?>
+        <?php echo $client->client_zip ? htmlsc($client->client_zip) . ' ' : ''; ?>
+        <?php echo $client->client_city ? htmlsc($client->client_city) . ' ' : ''; ?>
+        <?php echo $client->client_state ? htmlsc($client->client_state) : ''; ?>
+    <?php else: ?>
+        <?php echo $client->client_city ? htmlsc($client->client_city) . ' ' : ''; ?>
+        <?php echo $client->client_state ? htmlsc($client->client_state) . ' ' : ''; ?>
+        <?php echo $client->client_zip ? htmlsc($client->client_zip) : ''; ?>
+    <?php endif; ?>
 </span>
 <span class="client-adress-country-line">
-    <?php echo $client->client_country ? '<br>' . get_country_name(trans('cldr'), $client->client_country) : ''; ?>
+    // get the default country code from the system settings and if the client country matches, hide the country name in the address
+    <?php 
+    $default_country = get_setting('default_country');
+
+    if ($client->client_country && $client->client_country !== $default_country) {
+        echo '<br>' . get_country_name(trans('cldr'), $client->client_country);
+    } 
+    ?>
 </span>
