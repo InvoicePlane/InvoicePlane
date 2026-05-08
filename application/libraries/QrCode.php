4060fed62c8bc728e9eca06e22e808d1b4d488c0
Jannomag <jannomag@gmx.de>
2026-05-08 20:04:41 +0200

    Implement recipient determination logic in QrCode.php
    Added logic to determine recipient based on priority from QR code settings and invoice details.

--- application/libraries/QrCode.php
+++ application/libraries/QrCode.php
@@ -30,6 +30,19 @@ class QrCode
         $CI->load->helper('template');
 
         $this->invoice         = $params['invoice'];
+
+        // Determine recipient with the following priority:
+        // First priority: Get recipient from qr code settings
+        $recipient = $CI->mdl_settings->setting('qr_code_recipient');
+        // Second priority: If still empty, get recipient from invoice's user_company
+        if (empty($recipient)) {
+            $recipient = $this->invoice->user_company;
+        }
+        // Third priority: If still empty, get recipient from invoice's user_name
+        if (empty($recipient)) {
+            $recipient = $this->invoice->user_name;
+        }
+        $this->recipient = $recipient;
         $this->recipient       = $this->invoice->user_company ?: $CI->mdl_settings->setting('qr_code_recipient');
         $this->iban            = $this->invoice->user_iban ?: $CI->mdl_settings->setting('qr_code_iban');
         $this->bic             = $this->invoice->user_bic ?: $CI->mdl_settings->setting('qr_code_bic');
