eddf80d267c037ea17a121edae68fb160c95dd95
Niels Drost <47660417+nielsdrost7@users.noreply.github.com>
2026-06-08 04:21:10 +0200

    Feature/1569 treat client birthdate (#1572)
    * Fix invalid client birthdate display
    
    * Fix: Remove accidental ghost folder with leading space from .github/actions
    
    ---------
    
    Co-authored-by: Muhammad Musa <admin@excellence.edu.pk>

--- application/modules/setup/sql/043_1.7.2.sql
+++ application/modules/setup/sql/043_1.7.2.sql
@@ -1,1 +1,8 @@
 # Added for versioning
+-- Treat empty client birthdates as NULL.
+UPDATE `ip_clients`
+SET `client_birthdate` = NULL
+WHERE `client_birthdate` = '0000-00-00';
+
+ALTER TABLE `ip_clients`
+MODIFY `client_birthdate` DATE NULL DEFAULT NULL;
