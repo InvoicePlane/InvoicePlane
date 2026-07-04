6e1b701805226aa2e2a1d0e2244f3c9e449dfaef
Thomas Ingles <thomas@sudwebdesign.fr>
2026-07-04 17:17:46 +0200

    integrations settings: Title++, incoming+events → headerbar (right)

--- application/modules/integrations/views/settings.php
+++ application/modules/integrations/views/settings.php
@@ -1,58 +1,51 @@
 <div id="headerbar">
-    <h1 class="headerbar-title"><?php _trans('einvoice'); ?></h1>
+    <h1 class="headerbar-title"><?php _trans('einvoice'); ?> <?php _trans('providers'); ?></h1>
+    <div class="headerbar-item pull-right">
+        <a href="<?php echo site_url('integrations/incoming'); ?>" class="btn btn-sm btn-default">
+            <i class="fa fa-inbox"></i> <?php _trans('incoming_invoices'); ?>
+        </a>
+
+        <a href="<?php echo site_url('integrations/events'); ?>" class="btn btn-sm btn-default">
+            <i class="fa fa-history"></i> <?php _trans('einvoice_events'); ?>
+        </a>
+    </div>
 </div>
 
 <div id="content" class="table-content">
     <?php $this->layout->load_view('layout/alerts'); ?>
 
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <?php _trans('providers'); ?>
-        </div>
-
-        <div class="panel-body">
-            <a href="<?php echo site_url('integrations/incoming'); ?>" class="btn btn-sm btn-default">
-                <i class="fa fa-inbox"></i> <?php _trans('incoming_invoices'); ?>
-            </a>
-
-            <a href="<?php echo site_url('integrations/events'); ?>" class="btn btn-sm btn-default">
-                <i class="fa fa-history"></i> <?php _trans('einvoice_events'); ?>
-            </a>
-        </div>
-
-        <div class="table-responsive">
-            <table class="table table-striped">
-                <thead>
+    <div class="table-responsive">
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th><?php _trans('provider'); ?></th>
+                <th><?php _trans('label'); ?></th>
+                <th><?php _trans('status'); ?></th>
+                <th><?php _trans('actions'); ?></th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($providers as $provider) : ?>
                 <tr>
-                    <th><?php _trans('provider'); ?></th>
-                    <th><?php _trans('label'); ?></th>
-                    <th><?php _trans('status'); ?></th>
-                    <th><?php _trans('actions'); ?></th>
+                    <td><?php _htmlsc($provider['merchant_type']); ?></td>
+                    <td><?php _htmlsc($provider['label']); ?></td>
+                    <td>
+                        <?php echo (int) $provider['enabled'] === 1 ? trans('enabled') : trans('disabled'); ?>
+                    </td>
+                    <td>
+                       <a href="<?php echo site_url('integrations/settings/edit/' . $provider['id']); ?>"
+                          class="btn btn-sm btn-default">
+                          <?php _trans('edit'); ?>
+                       </a>
+                       <a href="<?php echo site_url('integrations/sync/' . $provider['id']); ?>"
+                          class="btn btn-sm btn-primary">
+                          <i class="fa fa-refresh"></i>
+                          <?php _trans('sync'); ?>
+                       </a>
+                    </td>
                 </tr>
-                </thead>
-                <tbody>
-                <?php foreach ($providers as $provider) : ?>
-                    <tr>
-                        <td><?php _htmlsc($provider['merchant_type']); ?></td>
-                        <td><?php _htmlsc($provider['label']); ?></td>
-                        <td>
-                            <?php echo (int) $provider['enabled'] === 1 ? trans('enabled') : trans('disabled'); ?>
-                        </td>
-                        <td>
-                           <a href="<?php echo site_url('integrations/settings/edit/' . $provider['id']); ?>"
-                              class="btn btn-sm btn-default">
-                              <?php _trans('edit'); ?>
-                           </a>
-                           <a href="<?php echo site_url('integrations/sync/' . $provider['id']); ?>"
-                              class="btn btn-sm btn-primary">
-                              <i class="fa fa-refresh"></i>
-                              <?php _trans('sync'); ?>
-                           </a>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-                </tbody>
-            </table>
-        </div>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
     </div>
 </div>
