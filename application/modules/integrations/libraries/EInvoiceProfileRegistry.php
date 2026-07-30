c99221486ced40e16f91b607fa6dd09220daeca2
Ordissimo <thierry@ordissimo.com>
2026-07-30 20:55:30 +0200

    feat(einvoicing): generate Peppol BIS Billing UBL

--- application/modules/integrations/libraries/EInvoiceProfileRegistry.php
+++ application/modules/integrations/libraries/EInvoiceProfileRegistry.php
@@ -266,4 +266,41 @@ final class EInvoiceProfileRegistry
     {
         return $this->profiles;
     }
+
+    public static function builtIn(): self
+    {
+        return new self([
+            new EInvoiceProfile(
+                'UblPeppolV21',
+                'Peppol BIS Billing 3.0 (UBL 2.1)',
+                'BE',
+                'Ublv24',
+                'peppol-bis-billing-3',
+                'ubl',
+                'application/xml',
+                'xml',
+                false,
+                '',
+                [
+                    'CustomizationID' => 'urn:cen.eu:en16931:2017#compliant#urn:fdc:peppol.eu:2017:poacc:billing:3.0',
+                    'ProfileID'       => 'urn:fdc:peppol.eu:2017:poacc:billing:01:1.0',
+                    'EndpointID'      => [
+                        'user'   => 'einvoice_identifier',
+                        'client' => 'peppol_id',
+                    ],
+                    'user_eas_code'       => '0088',
+                    'client_eas_code'     => '0088',
+                    'PartyIdentification' => true,
+                    'PartyLegalEntity'    => [
+                        'CompanyID' => 'vat_id',
+                        'SchemeID'  => false,
+                    ],
+                ],
+                'urn:cen.eu:en16931:2017#compliant#urn:fdc:peppol.eu:2017:poacc:billing:3.0',
+                'urn:fdc:peppol.eu:2017:poacc:billing:01:1.0',
+                PeppolDocumentType::BillingInvoice->value,
+                ['letspeppol']
+            ),
+        ]);
+    }
 }
