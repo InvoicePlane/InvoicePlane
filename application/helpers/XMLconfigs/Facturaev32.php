58b250eba89106fc95ca5a1f41a9082bb07c4761
Ordissimo <thierry@ordissimo.com>
2026-07-30 21:02:36 +0200

    feat(einvoicing): add Spanish Facturae profile

---
+++ application/helpers/XMLconfigs/Facturaev32.php
@@ -0,0 +1,13 @@
+<?php
+
+defined('BASEPATH') || exit('No direct script access allowed');
+
+$xml_setting = [
+    'full-name'   => 'Facturae 3.2.1',
+    'countrycode' => 'ES',
+    'embedXML'    => false,
+    'XMLname'     => '',
+    'options'     => [
+        'series' => 'IP',
+    ],
+];
