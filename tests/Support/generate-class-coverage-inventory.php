a58058ffe55f1441ff5b7dcd62a5edc2608e06ac
Niels Drost <nielsdrost7@gmail.com>
2026-08-04 19:51:51 +0200

    test: add class coverage inventory

---
+++ tests/Support/generate-class-coverage-inventory.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/ClassCoverageInventory.php';
+
+$repoRoot = dirname(__DIR__, 2);
+$target   = $argv[1] ?? $repoRoot . '/tests/Support/class-coverage-inventory.md';
+
+$inventory = ClassCoverageInventory::build($repoRoot);
+$markdown  = ClassCoverageInventory::toMarkdown($inventory);
+
+if (file_put_contents($target, $markdown) === false) {
+    fwrite(STDERR, 'Unable to write class coverage inventory: ' . $target . PHP_EOL);
+    exit(1);
+}
+
+fwrite(STDOUT, sprintf(
+    "Wrote %s (%d classes)%s",
+    $target,
+    count($inventory),
+    PHP_EOL
+));
